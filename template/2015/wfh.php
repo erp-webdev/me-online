81a39690446ac60ee95a4eff29022a74eab57628
megaworld-laptop <megaworld-laptop@ISM-Laptop14.mphi.com>
2020-05-08 18:48:26 +0800

    revision 1.1

--- template/2015/wfh.php
+++ template/2015/wfh.php
@@ -119,7 +119,8 @@ $(function() {
 																		</td>
 																		<td class="centertalign"><input style="width: 50%" id="wfh_totalworkedhours<?php echo $key; ?>" type="number" name="wfh_totalworkedhours[<?php echo $key; ?>]" attribute="<?php echo $key; ?>" class="wfh_totalworkedhours"></td>
 																		<td class="centertalign">
-																			<textarea rows="1" style="width: 100%; height: 50%;" name="wfh_activity[<?php echo $key; ?>]" id="wfh_activity<?php echo $key ?>" class="txtbox"></textarea>
+																			<textarea rows="1" style="display: none" name="wfh_activity[<?php echo $key; ?>]" id="wfh_activity<?php echo $key ?>" class="txtbox"></textarea>
+																			<input id="wfhshow" type="button" name="wfhshow" value="Show/Hide Activities" class="btn margintop10" style="background-color: #3EC2FB;" />
 																		</td>
 																	</tr>
 
