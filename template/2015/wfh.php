6bf154adc875b9f2bbaa8595095ec4d11451b2e0
megaworld-laptop <megaworld-laptop@ISM-Laptop14.mphi.com>
2020-05-08 20:12:12 +0800

    test5

--- template/2015/wfh.php
+++ template/2015/wfh.php
@@ -187,10 +187,20 @@ $(function() {
 		$('.wfhshow').click(function() {
 			arrayid = $(this).attr('attribute1');
 
-			var new_row = "<tr><td>&nbsp;</td><td><input type='text' name='TimeIn["+arrayid+"]'></td><td><input type='text' name='TimeOut["+arrayid+"]'></td><td><input type='text' name='Activities["+arrayid+"]'></td><td><input class='btn wfhremove' onClick='(function(){ alert('testers');return false; })();return false;' attribute1='"+arrayid+"' type='button' name='wfhshow' value='Remove' style='background-color: #EA171F;' /></td></tr>";
+			var new_row = "<tr><td>&nbsp;</td><td><input type='text' name='TimeIn["+arrayid+"]'></td><td><input type='text' name='TimeOut["+arrayid+"]'></td><td><input type='text' name='Activities["+arrayid+"]'></td><td><input class='btn wfhremove' onClick='wfhRemove()' attribute1='"+arrayid+"' type='button' name='wfhshow' value='Remove' style='background-color: #EA171F;' /></td></tr>";
 
 			$('#wfhtbody'+arrayid).show();
 			$('#wfhtbody'+arrayid).after(new_row);
+
+
+		});
+
+		function wfhRemove() {
+
+			arrayid = $(this).attr('attribute1');
+			alert('clicked!'+arrayid);
+
+			$(this).parents("tr").remove();
 
 
 		});
