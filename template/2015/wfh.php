03cc2d0e2fdceeaec9c810a2dbf45b0f80091f69
Kevs <kevinayag@users.noreply.github.com>
2020-05-10 23:01:36 +0800

    update

--- template/2015/wfh.php
+++ template/2015/wfh.php
@@ -29,7 +29,7 @@
 										<table class="tdataform" border="0" cellspacing="0">
 											<tr>
 												<td width="15%"><b>DTR Date: </b></td>
-												<td width="85%"><input id="wfh_froml" type="text" name="wfh_from" value="<?php echo $wfh_fdate; ?>" class="txtbox datepickwh" readonly /> - <input id="wfh_tol" type="text" name="wfh_to" value="<?php echo $wfh_todate; ?>" class="txtbox datepickwh" readonly /></td>
+												<td width="85%"><input id="wfh_from_" type="text" name="wfh_from" value="<?php echo $wfh_fdate; ?>" class="txtbox datepickwh" readonly ng-model="wfh_from"/> - <input id="wfh_to_" type="text" name="wfh_to" value="<?php echo $wfh_todate; ?>" class="txtbox datepickwh" readonly ng-model="wfh_to"/></td>
 											</tr>
 											<tr>
 												<td width="100%" colspan="3">
@@ -43,104 +43,67 @@
 																<th width="60px">Total Worked Hours</th>
 																<th width="">Activities</th>
 															</tr>
+															<script>
+																$(function() {
 
-															<?php
+																	$("#mdtr_absent<?php echo $key; ?>").change(function() {
 
-																$key = 1;
+																		arrayid = $(this).attr('attribute');
 
-																$shiftsched = $mainsql->get_schedshift($profile_idnum);
-																$shiftlist = $mainsql->get_shift();
-																$wfh_from_original = $wfh_from;
-																while($wfh_from <= $wfh_today) {
-
-																	$udate = date("U", $wfh_from);
-																	$dates = date("Y-m-d", $wfh_from);
-																	$days = date("D m/d/y", $wfh_from);
-																	$numdays = intval(date("N", $wfh_from));
-																	$dtr_data = $mainsql->get_dtr_bydate($profile_idnum, date("m/d/Y", $udate));
-
-																	$shiftsched2 = $mainsql->get_schedshiftdtr($profile_idnum, $dates);
-																	$sft = $mainsql->get_shift($shiftsched2[0]['ShiftID']);
-
-																	$dtimein = trim($dtr_data[0]['TimeIN']);
-																	$dtimeout = trim($dtr_data[0]['TimeOut']);
-
-																	$timein = date('h:ia', strtotime($sft[0]['TimeIN']));
-																	$timeout = date('h:ia', strtotime($sft[0]['TimeOUT']));
-
-																	$datein = strtotime($dates.' '.$timein);
-																	$dateout = strtotime($dates.' '.$timeout);
-
-																	$totaltime = $dateout - $datein;
-
-																	?>
-
-<script>
-$(function() {
-
-	$("#mdtr_absent<?php echo $key; ?>").change(function() {
-
-		arrayid = $(this).attr('attribute');
-
-		if($("#wfh_disable" + arrayid).val() == 0){
-			$("#wfh_disable" + arrayid).val(1);
-		}else{
-			$("#wfh_disable" + arrayid).val(0);
-		}
+																		if($("#wfh_disable" + arrayid).val() == 0){
+																			$("#wfh_disable" + arrayid).val(1);
+																		}else{
+																			$("#wfh_disable" + arrayid).val(0);
+																		}
 
 
 
-		if($("#wfh_totalworkedhours" + arrayid).prop("disabled")){
-			$("#wfh_totalworkedhours" + arrayid).prop("disabled", false);
-		}else{
-			$("#wfh_totalworkedhours" + arrayid).prop("disabled", true);
-		}
+																		if($("#wfh_totalworkedhours" + arrayid).prop("disabled")){
+																			$("#wfh_totalworkedhours" + arrayid).prop("disabled", false);
+																		}else{
+																			$("#wfh_totalworkedhours" + arrayid).prop("disabled", true);
+																		}
 
-		if($("#wfh_activity" + arrayid).prop("disabled")){
-			$("#wfh_activity" + arrayid).prop("disabled", false);
-		}else{
-			$("#wfh_activity" + arrayid).prop("disabled", true);
-		}
+																		if($("#wfh_activity" + arrayid).prop("disabled")){
+																			$("#wfh_activity" + arrayid).prop("disabled", false);
+																		}else{
+																			$("#wfh_activity" + arrayid).prop("disabled", true);
+																		}
 
-	});
+																	});
 
-});
-
-
-
-</script>
-
-
-
-																	<tr id="tr<?php echo $key; ?>">
-																		<td class="centertalign"><?php echo $key; ?></td>
-																		<td class="centertalign"><input type="hidden" name="wfh_disable[<?php echo $key; ?>]" id="wfh_disable<?php echo $key; ?>" value=0><input id="mdtr_absent<?php echo $key; ?>" type="checkbox" name="mdtr_absent[<?php echo $key; ?>]" attribute="<?php echo $key; ?>" class="mdtr_absent"></td>
-																		<td class="centertalign">
-																			<?php echo $days; ?>
-																			<input id="wfh_dayin<?php echo $key; ?>" type="hidden" name="wfh_dayin[<?php echo $key; ?>]" value="<?php echo $dates; ?>" class="wfh_dayin<?php echo $key; ?>" />
-																		</td>
-																		<td class="centertalign"><input style="width: 100%" id="wfh_totalworkedhours<?php echo $key; ?>" type="number" name="wfh_totalworkedhours[<?php echo $key; ?>]" attribute="<?php echo $key; ?>" class="wfh_totalworkedhours"></td>
-																		<td class="centertalign" >
-																			<textarea rows="1" style="display:none" name="wfh_activity[<?php echo $key; ?>]" id="wfh_activity<?php echo $key; ?>" class="txtbox"></textarea>
-																			<table>
-																				<tr ng-repeat="activity in wfh_activity<?php echo $key; ?>">
-																					<td style="border-bottom: 0px; margin: 0; padding: 0" >
-																						<input id="asd{{ $index }}" type="text" class="txtbox width80" ng-model="wfh_activity<?php echo $key; ?>[$index].time">
-																					</td>
-																					<td style="border-bottom: 0px; margin: 0; padding: 0" width="150px"><textarea class="txtarea" name="" id="" cols="30" rows="1" ng-model="wfh_activity<?php echo $key; ?>[$index].act" ng-click="check()"></textarea></td>
-																					<td style="border-bottom: 0px; margin: 0; padding: 0; text-align:left" width="120px"><button style="" type="button" class="smlbtn" ng-show="$index+1 == wfh_activity<?php echo $key; ?>.length" ng-click="addItem('wfh_activity<?php echo $key; ?>')">Add</button><button style="" type="button" class="redbtn " ng-show="wfh_activity<?php echo $key; ?>.length > 1" ng-click="delItem('wfh_activity<?php echo $key; ?>', $index)">Del</button></td>
-																				</tr>
-																			</table>
-																		</td>
-																	</tr>
-
-																	<?php
-																	$key++;
-
-																	$wfh_from = strtotime("+1 day", $wfh_from);
-																}
-
-															?>
+																});
+															</script>
+															<tr ng-repeat="wfh_day in wfh_days" id="tr{{ $index+1 }}">
+																<td class="centertalign">{{ $index+1 }}</td>
+																<td class="centertalign">
+																	<input type="hidden" name="wfh_disable[{{ $index+1 }}]" id="wfh_disable{{ $index+1 }}" value=0><input id="mdtr_absent{{ $index+1 }}" type="checkbox" name="mdtr_absent[{{ $index+1 }}]" attribute="{{ $index+1 }}" class="mdtr_absent">
+																</td>
+																<td class="centertalign">
+																	{{ $wfh_day | date: "E MM/dd/yy"}}
+																	<input id="wfh_dayin{{ $index+1 }}" type="hidden" name="wfh_dayin[{{ $index+1 }}]" value="{{ $wfh_day | date: 'y-MM-dd'}}" class="wfh_dayin{{ $index+1 }}" />
+																</td>
+																<td class="centertalign">
+																	<input style="width: 100%" id="wfh_totalworkedhours{{ $index+1 }}" type="number" name="wfh_totalworkedhours[{{ $index+1 }}]" attribute="{{ $index+1 }}" class="wfh_totalworkedhours">
+																</td>
+																<td class="centertalign" >
+																	<textarea rows="1" style="display:none" name="wfh_activity[{{ $index+1 }}]" id="wfh_activity{{ $index+1 }}" class="txtbox"></textarea>
+																	<table>
+																		<tr ng-repeat="activity in wfh_activity{{ $index+1 }}">
+																			<td style="border-bottom: 0px; margin: 0; padding: 0" >
+																				<input id="asd{{ $index }}" type="text" class="txtbox width80" ng-model="wfh_activity{{ $index+1 }}[$index].time">
+																			</td>
+																			<td style="border-bottom: 0px; margin: 0; padding: 0" width="150px">
+																				<textarea class="txtarea" name="" id="" cols="30" rows="1" ng-model="wfh_activity{{ $index+1 }}[$index].act" ng-click="check()"></textarea>
+																			</td>
+																			<td style="border-bottom: 0px; margin: 0; padding: 0; text-align:left" width="120px">
+																				<button style="" type="button" class="smlbtn" ng-show="$index+1 == wfh_activity{{ $index+1 }}.length" ng-click="addItem('wfh_activity{{ $index+1 }}')">Add</button>
+																				<button style="" type="button" class="redbtn " ng-show="wfh_activity{{ $index+1 }}.length > 1" ng-click="delItem('wfh_activity{{ $index+1 }}', $index)">Del</button>
+																			</td>
+																		</tr>
+																	</table>
+																</td>
+															</tr>
 														</table>
 														
 													</div>
@@ -189,36 +152,51 @@ $(function() {
 	
 <script>
 
-														$(document).ready(function () {
-															var wfh_app = angular.module('WFHApp', []);
-															wfh_app.controller('WFHController', function WFHController($scope){
-																
-																$scope.item = {time : '', act: ''};
-																
-																<?php $key = 1; $wfh_from = $wfh_from_original; while($wfh_from <= $wfh_today) { ?>
+	$(document).ready(function () {
+		var wfh_app = angular.module('WFHApp', []);
+		wfh_app.controller('WFHController', function WFHController($scope){
+			// Dates
+			$scope.wfh_from = Date.now();
+			$scope.wfh_to = Date.now();
+			$scope.wfh_days = [];
+			$scope.watchGroup(['wfh_from', 'wfh_to'], function(newVal, oldVal, scope){
+				$scope.wfh_days = [];
+				var current_date =$scope.wfh_from;
+				while(current_date <= $scope_wfh_to){
 
-																$scope.wfh_activity<?php echo $key; ?> = [];
-																$scope.wfh_activity<?php echo $key; ?>.push(angular.copy($scope.item));
+					$scope.wfh_days.push(current_date);
+					current_date.addDays(1);
 
-																$scope.$watch('wfh_activity<?php echo $key; ?>', function(newValue, oldValue, scope){
-																	$('#wfh_activity<?php echo $key; ?>').text( JSON.stringify(newValue) );
-																	console.log(JSON.stringify(newValue));
-																}, true);
+				}
+			})
 
-																<?php $key++;  $wfh_from = strtotime("+1 day", $wfh_from); } ?>
+			// Activities and Items
+			$scope.item = {time : '', act: ''};
+			
+			<?php $key = 1; $wfh_from = $wfh_from_original; while($wfh_from <= $wfh_today) { ?>
 
-																// Add new activity item
-																$scope.addItem = function(act){
-																	$scope[act].push(angular.copy($scope.item));
-																}
+			$scope.wfh_activity<?php echo $key; ?> = [];
+			$scope.wfh_activity<?php echo $key; ?>.push(angular.copy($scope.item));
 
-																// Remove item
-																$scope.delItem = function(act, index){
-																	$scope[act].splice(index, 1);
-																}
+			$scope.$watch('wfh_activity<?php echo $key; ?>', function(newValue, oldValue, scope){
+				$('#wfh_activity<?php echo $key; ?>').text( JSON.stringify(newValue) );
+				console.log(JSON.stringify(newValue));
+			}, true);
 
-															});
-														});
+			<?php $key++;  $wfh_from = strtotime("+1 day", $wfh_from); } ?>
 
-														</script>
+			// Add new activity item
+			$scope.addItem = function(act){
+				$scope[act].push(angular.copy($scope.item));
+			}
+
+			// Remove item
+			$scope.delItem = function(act, index){
+				$scope[act].splice(index, 1);
+			}
+
+		});
+	});
+
+</script>
 <?php include(TEMP."/footer.php"); ?>
