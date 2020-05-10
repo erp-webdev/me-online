6d90b5da00aa69f77735d81a819ad8105184b543
Kevs <kevinayag@users.noreply.github.com>
2020-05-10 14:14:51 +0800

    update

--- template/2015/wfh.php
+++ template/2015/wfh.php
@@ -124,9 +124,8 @@ $(function() {
 																					<td style="border-bottom: 0px; margin: 0; padding: 0" width="50px"><input type="text" class=""></td>
 																					<td style="border-bottom: 0px; margin: 0; padding: 0"><textarea  name="" id="" cols="50" rows="1"></textarea></td>
 																				</tr>
-																				<tr ng-repeat="">
-																					<td style="border-bottom: 0px; margin: 0; padding: 0" width="50px"><input type="text" class=""></td>
-																					<td style="border-bottom: 0px; margin: 0; padding: 0"><textarea  name="" id="" cols="50" rows="1"></textarea></td>
+																				<tr>
+																					<td colspan="2"><button class="smlbtn">Add Activity</button></td>
 																				</tr>
 																			</table>
 																		</td>
