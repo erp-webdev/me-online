2f71106a8a7ef4928c2607ab533b132d8757f358
Kevs <kevinayag@users.noreply.github.com>
2020-05-10 17:45:22 +0800

    update

--- template/2015/wfh.php
+++ template/2015/wfh.php
@@ -128,7 +128,7 @@ $(function() {
 																						<input type="text" class="txtbox width80 " ng-model="wfh_activity<?php echo $key ?>[$index].time">
 																					</td>
 																					<td style="border-bottom: 0px; margin: 0; padding: 0" width="150px"><textarea class="txtarea" name="" id="" cols="30" rows="1" ng-model="wfh_activity<?php echo $key ?>[$index].act" ng-click="check()"></textarea></td>
-																					<td style="border-bottom: 0px; margin: 0; padding: 0" width="30px"><button type="button" class="smlbtn">Add</button></td>
+																					<td style="border-bottom: 0px; margin: 0; padding: 0" width="30px"><button style="" type="button" class="smlbtn">Add</button></td>
 																				</tr>
 																				
 																			</table>
