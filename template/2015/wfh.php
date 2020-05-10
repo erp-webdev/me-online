e8fc52fb7641129f3d6d732ee8f0ee6dbb21cedd
Kevs <kevinayag@users.noreply.github.com>
2020-05-10 15:36:02 +0800

    update

--- template/2015/wfh.php
+++ template/2015/wfh.php
@@ -125,9 +125,9 @@ $(function() {
 																			<table ng-app="WFHApp" ng-controller="WFHController">
 																				<tr ng-repeat="activity in wfh_activity<?php echo $key ?>">
 																					<td style="border-bottom: 0px; margin: 0; padding: 0" width="30px">
-																						<input type="text" class="txtbox width80 " readonly="" ng-bind="wfh_activity<?php echo $key ?>[$index].time">
+																						<input type="text" class="txtbox width80 " ng-model="wfh_activity<?php echo $key ?>[$index].time">
 																					</td>
-																					<td style="border-bottom: 0px; margin: 0; padding: 0" width="150px"><textarea class="txtarea" name="" id="" cols="30" rows="1" ng-bind="wfh_activity<?php echo $key ?>[$index].act"></textarea></td>
+																					<td style="border-bottom: 0px; margin: 0; padding: 0" width="150px"><textarea class="txtarea" name="" id="" cols="30" rows="1" ng-bind="wfh_activity<?php echo $key ?>[$index].act" ng-change="check()"></textarea></td>
 																					<td style="border-bottom: 0px; margin: 0; padding: 0" width="30px"><button type="button" class="smlbtn">Add</button></td>
 																				</tr>
 																				
@@ -202,6 +202,10 @@ $(document).ready(function () {
 			$('#wfh_activity1').text( JSON.stringify(newValue) );
 			console.log(JSON.stringify(newValue));
 		});
+
+		$scope.check = function(){
+			console.log(JSON.stringify($scope.wfh_activity1));
+		}
 	});
 });
 
