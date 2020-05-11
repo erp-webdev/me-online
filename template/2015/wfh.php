54905b9190655d368c591fb7d57d971a878b990d
megaworld-laptop <megaworld-laptop@ISM-Laptop14.mphi.com>
2020-05-11 13:25:42 +0800

    update

--- template/2015/wfh.php
+++ template/2015/wfh.php
@@ -77,7 +77,7 @@
 
 																});
 															</script>
-															<tr ng-repeat="wfh_day in wfh_days" id="tr{{ $index+1 }}" ng-init="myVar='$index+1'">
+															<tr ng-repeat="wfh_day in wfh_days" id="tr{{ $index+1 }}" ng-init="myVar=$index+1">
 																<td class="centertalign">{{ myVar }}</td>
 																<td class="centertalign">
 																	<input type="hidden" name="wfh_disable[{{ $index+1 }}]" id="wfh_disable{{ $index+1 }}" value=0><input id="mdtr_absent{{ $index+1 }}" type="checkbox" name="mdtr_absent[{{ $index+1 }}]" attribute="{{ $index+1 }}" class="mdtr_absent" ng-click="myFunction($index+1)">
