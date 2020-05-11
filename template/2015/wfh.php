32c80f9f35c0b98f688110253933ed01c0db0b35
megaworld-laptop <megaworld-laptop@ISM-Laptop14.mphi.com>
2020-05-11 12:18:52 +0800

    update

--- template/2015/wfh.php
+++ template/2015/wfh.php
@@ -93,7 +93,7 @@
 															<tr ng-repeat="wfh_day in wfh_days" id="tr{{ $index+1 }}">
 																<td class="centertalign">{{ $index+1 }}</td>
 																<td class="centertalign">
-																	<input type="hidden" name="wfh_disable[{{ $index+1 }}]" id="wfh_disable{{ $index+1 }}" value=0><input id="mdtr_absent{{ $index+1 }}" type="checkbox" name="mdtr_absent[{{ $index+1 }}]" attribute="{{ $index+1 }}" class="mdtr_absent" onclick="myFunction({{ $index+1 }});">
+																	<input type="hidden" name="wfh_disable[{{ $index+1 }}]" id="wfh_disable{{ $index+1 }}" value=0><input id="mdtr_absent{{ $index+1 }}" type="checkbox" name="mdtr_absent[{{ $index+1 }}]" attribute="{{ $index+1 }}" class="mdtr_absent" onclick="myFunction()">
 																</td>
 																<td class="centertalign">
 																	{{ wfh_day | date: 'EEE MM/dd/yy'}}
@@ -172,7 +172,7 @@
 
 
 		function myFunction(key){
-			alert(key);
+			alert("called");
 		}
 
 		var wfh_app = angular.module('WFHApp', []);
