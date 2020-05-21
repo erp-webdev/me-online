b0ebd9c11e4ab977eb6349fd5aad2e2c12c9b3fc
megaworld-laptop <megaworld-laptop@ISM-Laptop14.mphi.com>
2020-05-21 18:37:28 +0800

    update

--- lib/class/mainsql.class.php
+++ lib/class/mainsql.class.php
@@ -1972,7 +1972,7 @@ class mainsql {
 
     function get_shiftdtr($empid, $date, $dbname)
     {
-      $sql = "SELECT * FROM FN_GETSHIFT('".$empid."', '".$date."')";
+    $sql = "select '060' as SHIFT, '08:00:00.0000000' as STARTTIME, '18:00:00.0000000' as STARTTIME, '10' as NUMHRS";
       $result = $this->get_row($sql);
 
       return $result;
