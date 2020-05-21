7675dc1131a826dfdf9f1f91517b505c4849d9f2
megaworld-laptop <megaworld-laptop@ISM-Laptop14.mphi.com>
2020-05-21 18:24:02 +0800

    update

--- lib/class/mainsql.class.php
+++ lib/class/mainsql.class.php
@@ -1975,7 +1975,7 @@ class mainsql {
       $sql = "SELECT * FROM FN_GETSHIFT('".$empid."', '".$date."')";
       $result = $this->get_row($sql, 'GL_S10');
 
-      return $result[0]['SHIFT'];
+      return $sql;
     }
 
     function get_logs($id = NULL, $start = 0, $limit = 0, $search = NULL, $count = 0, $from = NULL, $to = NULL)
