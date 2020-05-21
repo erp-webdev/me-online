85ad2390f72a20019dbc72e7f2cef29e8f46c00f
megaworld-laptop <megaworld-laptop@ISM-Laptop14.mphi.com>
2020-05-21 18:05:23 +0800

    update

--- lib/class/mainsql.class.php
+++ lib/class/mainsql.class.php
@@ -1975,7 +1975,7 @@ class mainsql {
       $sql = "SELECT * FROM dbo.GET_SHIFT('".$empid."', '".$date."')";
       $result = $this->get_row($sql, $dbname);
 
-      return $sql;
+      return $result;
     }
 
     function get_logs($id = NULL, $start = 0, $limit = 0, $search = NULL, $count = 0, $from = NULL, $to = NULL)
