d3e99b5e262779ea564d8acda388641b25ac7201
Kevs <kevinayag@users.noreply.github.com>
2020-05-21 14:19:55 +0800

    test

--- template/2015/wfh.php
+++ template/2015/wfh.php
@@ -265,14 +265,14 @@
 					if(new Date($scope.wfh_from ) > new Date($scope.wfh_days[0].DTRDate) ){
 						var dtrdate = $scope.current_date.toISOString().split("T")[0];
 						var index = $scope.wfh_days.findIndex(item => item.DTR == dtrdate);						
-						$scope.wfh_days[index].ACTIVITIES.splice(0, index+1);
+						$scope.wfh_days[index+1].ACTIVITIES.splice(0, index+2);
 					}
 
 					if(new Date($scope.wfh_to ) < new Date($scope.wfh_days[$scope.wfh_days.length-1].DTR) ){
 						var dtrdate = new Date(angular.copy($scope.wfh_to)).toISOString().split("T")[0];
 						var index = $scope.wfh_days.findIndex(item => item.DTR == dtrdate);						
 						
-						$scope.wfh_days[index].ACTIVITIES.splice(index, $index + 1 - $scope.wfh_days.length);
+						$scope.wfh_days[index+1].ACTIVITIES.splice(index, $index + 2 - $scope.wfh_days.length);
 					}
 
 				}
