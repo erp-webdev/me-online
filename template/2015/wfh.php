f13ae9322e3b096238f2bf20368d629c16137b37
megaworld-laptop <megaworld-laptop@ISM-Laptop14.mphi.com>
2020-05-22 10:08:33 +0800

    update

--- template/2015/wfh.php
+++ template/2015/wfh.php
@@ -160,6 +160,7 @@
 					success: function(data) {
 						data = JSON.parse(data);
 
+						alert(data.STARTTIME);
 						if(data.STARTTIME != 'undefined' && data.STARTTIME != '' && data.STARTTIME != null ){
 
 
