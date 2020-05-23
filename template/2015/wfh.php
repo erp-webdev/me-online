8b36f8795519487e2fc269965ea0de07ba83bd13
Kevs <kevinayag@users.noreply.github.com>
2020-05-23 11:34:42 +0800

    update

--- template/2015/wfh.php
+++ template/2015/wfh.php
@@ -81,9 +81,9 @@
 																				<button style="" type="button" class="redbtn " ng-show="wfh_days[$parent.$index].ACTIVITIES.length > 1" ng-click="delItem($parent.$index, $index)">Del</button>
 																				<button style="" type="button" class="smlbtn" ng-show="$index+1 == wfh_days[$parent.$index].ACTIVITIES.length" ng-click="addItem($parent.$index, $index)">Add</button>
 																			</td>
-																			<span >&nbsp;</span>
 																		</tr>
 																	</table>
+																			<span >&nbsp;</span>
 																</td>
 															</tr>
 														</table>
