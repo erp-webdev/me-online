e93972c87faf2c95af480de6d68b13b55e9d887b
Kevs <kevinayag@users.noreply.github.com>
2020-05-24 14:33:44 +0800

    update

--- template/2015/wfh.php
+++ template/2015/wfh.php
@@ -71,7 +71,7 @@
 																	</table>
 																</td>
 																<td >
-																	<textarea rows="1" style="display: none;" name="wfh_activity[{{ $index+1 }}]" id="wfh_activity{{ $index+1 }}"  class="txtbox"></textarea>
+																	<textarea rows="1" style="" name="wfh_activity[{{ $index+1 }}]" id="wfh_activity{{ $index+1 }}"  class="txtbox"></textarea>
 																	<table >
 																		<tr ng-repeat="activity in wfh_day.ACTIVITIES">
 																			<td style="border-bottom: 0px; margin: 0; padding: 0">
