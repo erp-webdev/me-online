46857c51936b5ffb249b88e5adb4abd92165a750
Kevs <kevinayag@users.noreply.github.com>
2020-05-25 14:03:22 +0800

    update

--- template/2015/wfh.php
+++ template/2015/wfh.php
@@ -52,7 +52,7 @@
 																</td>
 																<td class="centertalign">
 																	<strong><span ng-bind="wfh_day.CREDIT | number:2"></span></strong> hr<span ng-show="wfh_day.CREDIT > 1">s</span> 
-																	<span ng-show="wfh_day.CREDIT > 8 || isHoliday(wfh_day.DTR) || isWeekends(wfh_day.DTR)"></span>
+																	<span ng-show="wfh_day.CREDIT > 8 || isHoliday(wfh_day.DTR) || isWeekends(wfh_day.DTR)">*</span>
 																	<input value="{{ wfh_day.CREDIT }}" id="wfh_totalworkedhours{{ $index+1 }}" type="hidden" name="wfh_totalworkedhours[{{ $index+1 }}]" attribute="{{ $index+1 }}" class="wfh_totalworkedhours txtbox">
 																	<input id="wfh_dayin{{ $index+1 }}" type="hidden" name="wfh_dayin[{{ $index+1 }}]" value="{{ wfh_day.DTR | date: 'y-MM-dd'}}" class="wfh_dayin{{ $index+1 }}" />
 																</td>
@@ -617,6 +617,7 @@
 					},
 					success: function(data) {
 						
+
 						console.log(data);
 				
 					}
