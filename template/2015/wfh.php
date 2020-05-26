0606869a959d2d48bdfbddbaa10a14f8fd4d81cb
megaworld-laptop <megaworld-laptop@ISM-Laptop14.mphi.com>
2020-05-26 15:05:50 +0800

    update

--- template/2015/wfh.php
+++ template/2015/wfh.php
@@ -46,6 +46,7 @@
 															<tr ng-repeat="wfh_day in wfh_days" id="tr{{ $index+1 }}">
 																<td class="centertalign">
 																	<input type="hidden" name="wfh_disable[{{ $index+1 }}]" id="wfh_disable{{ $index+1 	}}" value="0"><input id="mdtr_absent{{ $index+1 }}" type="checkbox" name="mdtr_absent[{{ $index+1 }}]" attribute="{{ $index+1 }}" class="mdtr_absent" ng-click="excludeFunction($index+1)" title="Excluded">
+																	{{ holiShift }}
 																</td>
 																<td class="centertalign">
 																	<span ng-bind="wfh_day.DTR | date: 'EEE MM/dd/yy'"></span> <br>
@@ -240,8 +241,6 @@
 					x.then(function(val) {
 						$scope.holiShift = val;
 					});
-
-					alert($scope.holiShift);
 
 				if($("#wfh_disable" + key).val() == 0){
 					$("#wfh_disable" + key).val(1);
