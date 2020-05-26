5fb6c72228f3ac13d5853cedd52d06f73771c2ba
megaworld-laptop <megaworld-laptop@ISM-Laptop14.mphi.com>
2020-05-26 14:43:01 +0800

    update

--- template/2015/wfh.php
+++ template/2015/wfh.php
@@ -238,7 +238,7 @@
 
 					var x = $scope.isSample("2020-05-14");
 
-					console.log(x.responseText);
+					console.log(x);
 
 				if($("#wfh_disable" + key).val() == 0){
 					$("#wfh_disable" + key).val(1);
