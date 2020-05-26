4fa4d11998fd646af186e48e69dcc3f0fa0a6b81
megaworld-laptop <megaworld-laptop@ISM-Laptop14.mphi.com>
2020-05-26 16:01:48 +0800

    update

--- template/2015/wfh.php
+++ template/2015/wfh.php
@@ -51,7 +51,7 @@
 																	<span ng-bind="wfh_day.DTR | date: 'EEE MM/dd/yy'"></span> <br>
 																</td>
 																<td class="centertalign">
-																	<span style="{{ (wfh_day.CREDIT > 8 ||  isWeekends(wfh_day.DTR) || isHoliday(wfh_day.DTR, this)) ? 'color:yellow' : '' }}">
+																	<span #warnDay style="{{ (wfh_day.CREDIT > 8 ||  isWeekends(wfh_day.DTR) || isHoliday(wfh_day.DTR, warnDay)) ? 'color:yellow' : '' }}">
 																		<strong><span ng-bind="wfh_day.CREDIT | number:2"></span></strong> hr<span ng-show="wfh_day.CREDIT > 1">s</span>
 																		<span ng-show="wfh_day.CREDIT > 8 ||  isWeekends(wfh_day.DTR)">*</span>
 																	</span>
@@ -621,8 +621,8 @@
 
 			});
 
-			$scope.isHoliday = function($dtr, this){
-				angular.element(this.currentTarget).attr("style", "color:red");
+			$scope.isHoliday = function($dtr, $element){
+				angular.element($element).attr("style", "color:red");
 
 				// var holiday = $.ajax(
 				// {
