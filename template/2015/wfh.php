d2fa20ad4ebc37588bbfd753b40ddd728346711a
Kevs <kevinayag@users.noreply.github.com>
2020-05-26 23:04:06 +0800

    update

--- template/2015/wfh.php
+++ template/2015/wfh.php
@@ -470,7 +470,7 @@
 						data: {date: $scope.wfh_days[k].DTR},
 					}).then(function checkHoliday(response) {
 						if(response.data.SHIFT == 'HOLIDAY')
-							$scope.holidays.push(data.config.data.date);
+							$scope.holidays.push(response.config.data.date);
 					}, function myError(response) {
 						console.log('error retrieving holiday');
 					});
