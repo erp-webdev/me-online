d6dc9281aa1e0747ef418d63259385188bc49843
megaworld-laptop <megaworld-laptop@ISM-Laptop14.mphi.com>
2020-05-27 15:47:14 +0800

    update

--- lib/requests/notification_request.php
+++ lib/requests/notification_request.php
@@ -6655,6 +6655,7 @@
 											arrayid = $(this).attr('attribute');
 											$("#wfhApprovedHrs" + arrayid).val(0);
 
+
 										});
 
 										$(".wfhwarn<?php echo $key; ?>").click(function(){
@@ -6693,11 +6694,11 @@
 												if($holiday['SHIFT'] == 'HOLIDAY'){
 													if($value['AppliedHrs'] > 8){
 											?>
-														<p class="wfhwarning<?php echo $key; ?> whwarning" style="color: red;">The applied hours falls on a holiday / Saturday / Sunday and exceeds 8 hours per day.</br><a href="#" class="wfhwarn<?php echo $key; ?>" attribute="<?php echo $key; ?>" attribute2="<?php echo $value['AppliedHrs']; ?>" attribute3="1"> <u>Approve Applied Hours</a></u> or <u><a href="#" onClick="$('.wfhcancel<?php echo $key; ?>').click();" class="<?php echo $key; ?>" attribute="<?php echo $key; ?>" attribute3="0">Cancel Application?</a></u></p>
+														<p class="wfhwarning<?php echo $key; ?> whwarning" style="color: red;">The applied hours falls on a Holiday / Saturday / Sunday and exceeds 8 hours per day.</br><a href="#" class="wfhwarn<?php echo $key; ?>" attribute="<?php echo $key; ?>" attribute2="<?php echo $value['AppliedHrs']; ?>" attribute3="1"> <u>Approve Applied Hours</a></u> or <u><a href="#" onClick="$('.wfhcancel<?php echo $key; ?>').click();" class="<?php echo $key; ?>" attribute="<?php echo $key; ?>" attribute3="0">Cancel Application?</a></u></p>
 											<?php
 													}else{
 											?>
-														<p class="wfhwarning<?php echo $key; ?> whwarning" style="color: red;">The applied hours falls on a holiday / Saturday / Sunday. Do you want to approve the applied hours?</br><u><a href="#" class="wfhwarn<?php echo $key; ?>" attribute="<?php echo $key; ?>" attribute2="<?php echo $value['AppliedHrs']; ?>" attribute3="1">Yes</a></u> or <u><a href="#" class="wfhwarn<?php echo $key; ?>" attribute="<?php echo $key; ?>" attribute3="0">No?</a></u></p>
+														<p class="wfhwarning<?php echo $key; ?> whwarning" style="color: red;">The applied hours falls on a Holiday / Saturday / Sunday. Do you want to approve the applied hours?</br><u><a href="#" class="wfhwarn<?php echo $key; ?>" attribute="<?php echo $key; ?>" attribute2="<?php echo $value['AppliedHrs']; ?>" attribute3="1">Yes</a></u> or <u><a href="#" class="wfhwarn<?php echo $key; ?>" attribute="<?php echo $key; ?>" attribute3="0">No?</a></u></p>
 											<?php
 													}
 												}else{
