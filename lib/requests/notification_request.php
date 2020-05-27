14f67f98cb8ac24ff03d66a6f3542d4840f203f0
megaworld-laptop <megaworld-laptop@ISM-Laptop14.mphi.com>
2020-05-27 16:11:08 +0800

    update

--- lib/requests/notification_request.php
+++ lib/requests/notification_request.php
@@ -6647,6 +6647,7 @@
 								foreach ($appwh_data as $key => $value) :
 
 									$holiday = $mainsql->get_shiftdtr($profile_idnum, $value['DTRDate'], $profile_dbname);
+									$wfday = date('w', strtotime($value['DTRDate']));
 									?>
 									<script>
 									$(function() {
@@ -6675,7 +6676,7 @@
 									});
 									</script>
 									<tr>
-										<td <?php if($key != 0){ ?>style="border-top: 1px solid #888"<?php } ?> class="centertalign"><p style="<?php if($holiday['SHIFT'] == 'HOLIDAY'){ echo 'color: red;'; } ?>"><?php echo date('F j, Y', strtotime($value['DTRDate'])); echo "-".date('w', strtotime($value['DTRDate'])); ?></p></td>
+										<td <?php if($key != 0){ ?>style="border-top: 1px solid #888"<?php } ?> class="centertalign"><p style="<?php if($holiday['SHIFT'] == 'HOLIDAY' || $wfday == 6 || $wfday == 0){ echo 'color: red;'; } ?>"><?php echo date('F j, Y', strtotime($value['DTRDate']));?></p></td>
 										<td <?php if($key != 0){ ?>style="border-top: 1px solid #888"<?php } ?> class="centertalign"><?php echo $value['AppliedHrs']; ?></td>
 										<td <?php if($key != 0){ ?>style="border-top: 1px solid #888"<?php } ?> class="centertalign">
 											<?php if(in_array($profile_idnum, $approvers)){ ?>
