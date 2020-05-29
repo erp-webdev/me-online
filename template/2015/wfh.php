badf382cc9d7fab8df21d774a9a86c441a7e5cce
Kevs <kevinayag@users.noreply.github.com>
2020-05-29 20:37:46 +0800

    test

--- template/2015/wfh.php
+++ template/2015/wfh.php
@@ -98,7 +98,7 @@
 											</tr>
 											<tr>
 												<td colspan="2">
-													<i>Maximum 7 days per application is allowed.</i><br>
+													<i>Maximum 7 days per application.</i><br>
 													<i>Sample task: Prepared monthly attendance of employees for Perfect Attendance Report</i><br>
 													<i><strong>Credit Hours for Approval</strong></i><br>
 													<ul>
