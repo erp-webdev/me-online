8e2ab0714419eb2f1dcf0069002484613d4c60fc
megaworld-laptop <megaworld-laptop@ISM-Laptop14.mphi.com>
2020-05-29 08:30:00 +0800

    update

--- lib/requests/notification_request.php
+++ lib/requests/notification_request.php
@@ -6995,7 +6995,7 @@
 												<table width="100%">
 													<tr>
 														<td><b>Signatory 1</b></td>
-														<td width="50%"><?php //echo $approver_data1[0]['FName'].' '.$approver_data1[0]['LName']; ?></td>
+														<td width="50%"><?php echo $approver_data1[0]['FName'].' '.$approver_data1[0]['LName']; ?></td>
 													</tr>
 												</table>
 											</td>
@@ -7005,7 +7005,7 @@
 												<table width="100%">
 													<tr>
 														<td><b>Signatory 4</b></td>
-														<td width="50%"><?php //echo $approver_data4[0]['FName'].' '.$approver_data4[0]['LName']; ?></td>
+														<td width="50%"><?php echo $approver_data4[0]['FName'].' '.$approver_data4[0]['LName']; ?></td>
 													</tr>
 												</table>
 												<!-- signatory 4 -->
