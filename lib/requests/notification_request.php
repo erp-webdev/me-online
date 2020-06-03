2cab01903417903c76ed2439735e9a8805d399a6
megaworld-laptop <megaworld-laptop@ISM-Laptop14.mphi.com>
2020-06-03 15:05:26 +0800

    update

--- lib/requests/notification_request.php
+++ lib/requests/notification_request.php
@@ -6750,7 +6750,7 @@
 														}
 													}
 												}
-												var_dump($show_warning);
+												var_dump($notifcation_data[0]['Signatory04'] == $profile_idnum);
 
 											if($show_warning){
 												if($holiday['SHIFT'] == 'HOLIDAY' || $wfday == 6  || $wfday == 0){
