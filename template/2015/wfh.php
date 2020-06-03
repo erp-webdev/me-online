eaba96e9f3cbbc5d081807fa074df698b5368e65
megaworld-laptop <megaworld-laptop@ISM-Laptop14.mphi.com>
2020-06-03 17:39:12 +0800

    update

--- template/2015/wfh.php
+++ template/2015/wfh.php
@@ -46,7 +46,8 @@
 															<tr ng-repeat="wfh_day in wfh_days" id="tr{{ $index+1 }}">
 																<td class="centertalign">
 																	<input type="hidden" name="wfh_disable[{{ $index+1 }}]" id="wfh_disable{{ $index+1 	}}" value="0">
-																	<input id="mdtr_absent{{ $index+1 }}" type="checkbox" name="mdtr_absent[{{ $index+1 }}]" attribute="{{ $index+1 }}" class="mdtr_absent" ng-click="excludeFunction($index+1)" {{ (isApplied(wfh_day.DTR)) ? 'applied' : 'not' }} title="Excluded">
+																	<input id="mdtr_absent{{ $index+1 }}" type="checkbox" name="mdtr_absent[{{ $index+1 }}]" attribute="{{ $index+1 }}" class="mdtr_absent" ng-click="excludeFunction($index+1)" title="Excluded">
+																	{{ (isApplied(wfh_day.DTR)) ? excludeFunction($index+1) : '' }}
 																</td>
 																<td class="centertalign">
 																	<span ng-bind="wfh_day.DTR | date: 'EEE MM/dd/yy'"></span> <br>
