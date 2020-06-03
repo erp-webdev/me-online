38ac210ed9e77f27374b4d412063cee92069514b
megaworld-laptop <megaworld-laptop@ISM-Laptop14.mphi.com>
2020-06-03 14:57:17 +0800

    update

--- lib/requests/notification_request.php
+++ lib/requests/notification_request.php
@@ -6716,6 +6716,37 @@
 												foreach($wh_act as $act_details){
 													echo "(".$act_details['start_time'] ." - " . $act_details['end_time'] .") ".$act_details['act']."</br></br>";
 												}
+												$show_warning = false;
+
+												if (!$chkexpire){
+													if($notification_data[0]['Signatory01'] == $profile_idnum){
+														if (!$notification_data[0]['ApprovedDate01'] && $notification_data[0]['Approved'] != 2){
+															$show_warning = true;
+														}
+													}elseif ($notifcation_data[0]['Signatory02'] == $profile_idnum) {
+														if(!$notification_data[0]['ApprovedDate02'] && $notification_data[0]['ApprovedDate01'] && $notification_data[0]['Approved'] != 2){
+															$show_warning = true;
+														}
+													}elseif ($notifcation_data[0]['Signatory03'] == $profile_idnum) {
+														if(!$notification_data[0]['ApprovedDate03'] && $notification_data[0]['ApprovedDate02'] && $notification_data[0]['Approved'] != 2){
+															$show_warning = true;
+														}
+													}elseif ($notifcation_data[0]['Signatory04'] == $profile_idnum) {
+														if(!$notification_data[0]['ApprovedDate04'] && $notification_data[0]['ApprovedDate03'] && $notification_data[0]['Approved'] != 2){
+															$show_warning = true;
+														}
+													}elseif ($notifcation_data[0]['Signatory05'] == $profile_idnum) {
+														if(!$notification_data[0]['ApprovedDate05'] && $notification_data[0]['ApprovedDate04'] && $notification_data[0]['Approved'] != 2){
+															$show_warning = true;
+														}
+													}elseif ($notifcation_data[0]['Signatory06'] == $profile_idnum) {
+														if(!$notification_data[0]['ApprovedDate06'] && $notification_data[0]['ApprovedDate05'] && $notification_data[0]['Approved'] != 2){
+															$show_warning = true;
+														}
+													}
+												}
+
+											if($show_warning){
 												if($holiday['SHIFT'] == 'HOLIDAY' || $wfday == 6  || $wfday == 0){
 													if($value['AppliedHrs'] > 8){
 											?>
@@ -6740,6 +6771,7 @@
 														<?php
 													}
 												}
+											}
 											?>
 										</td>
 										<td <?php if($key != 0){ ?>style="border-top: 1px solid #888"<?php } ?> class="centertalign">
