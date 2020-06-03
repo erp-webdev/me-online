95c4c5e154a4f06acd0b6b5a3f1a39e156dadc6a
megaworld-laptop <megaworld-laptop@ISM-Laptop14.mphi.com>
2020-06-03 09:34:23 +0800

    update

--- lib/requests/notification_request.php
+++ lib/requests/notification_request.php
@@ -7020,7 +7020,7 @@
                     </tr>
 
 										<tr>
-											<td>
+											<td colspan="2">
 												<table width="100%">
 
                     <?php if ($notification_data[0]['Approved'] != 3) : ?>
