1d3a35614760a5eef857eff52979a992d5f20200
megaworld-laptop <megaworld-laptop@ISM-Laptop14.mphi.com>
2020-06-04 11:25:29 +0800

    update

--- lib/class/mainsql.class.php
+++ lib/class/mainsql.class.php
@@ -1970,8 +1970,8 @@ class mainsql {
 
     function get_shiftdtr($empid, $date, $dbname)
     {
-      $sql = "SELECT SHIFT, SUBSTRING(CAST(STARTTIME as varchar), 1, 2) as STARTTIME, SUBSTRING(CAST(ENDTIME as varchar), 1, 2) as ENDTIME, NUMHRS FROM GL_S10.dbo.FN_GETSHIFT('".$empid."', '".$date."')";
-      $result = $this->get_row($sql);
+      $sql = "SELECT SHIFT, SUBSTRING(CAST(STARTTIME as varchar), 1, 2) as STARTTIME, SUBSTRING(CAST(ENDTIME as varchar), 1, 2) as ENDTIME, NUMHRS FROM dbo.FN_GETSHIFT('".$empid."', '".$date."')";
+      $result = $this->get_row($sql, $dbname);
 
       return $result[0];
     }
