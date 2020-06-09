a772576064e866827af857a9686585d96de843fa
megaworld-laptop <megaworld-laptop@ISM-Laptop14.mphi.com>
2020-06-09 13:24:30 +0800

    update

--- template/2015/wfh.php
+++ template/2015/wfh.php
@@ -344,7 +344,6 @@
 			       }else if(start >= twelve && end <= one){
 							 console.log('entry start and end within 12:00-1:00');
 			         //time start and end within 12:00 - 1:00
-			         var duration = ranges[i].duration;
 
 			       }else if( start < twelve && end > twelve  && end < one){
 			         console.log('entry end within 12:00-1:00');
