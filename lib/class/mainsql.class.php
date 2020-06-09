4933a0c24cd6dcbf4beee07aa1ee4f15061eed68
megaworld-laptop <megaworld-laptop@ISM-Laptop14.mphi.com>
2020-06-09 17:46:27 +0800

    update

--- lib/class/mainsql.class.php
+++ lib/class/mainsql.class.php
@@ -3035,6 +3035,7 @@ class mainsql {
                 ini_set('display_errors', 1);
                 ini_set('display_startup_errors', 1);
                 error_reporting(E_All);
+                fasf
                 $accepted_field = array('EMPID', 'REQNBR', 'TRANS', 'DATESTART', 'APPROVER01', 'APPROVER02', 'APPROVER03', 'APPROVER04', 'APPROVER05', 'APPROVER06', 'DBAPPROVER01', 'DBAPPROVER02', 'DBAPPROVER03', 'DBAPPROVER04', 'DBAPPROVER05', 'DBAPPROVER06', 'USER', 'REMARKS');
 
                 $knum = 0;
