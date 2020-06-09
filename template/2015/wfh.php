2cac69e6fa81761e15d3afb5237e98b9c71ba5be
megaworld-laptop <megaworld-laptop@ISM-Laptop14.mphi.com>
2020-06-09 14:02:57 +0800

    update

--- template/2015/wfh.php
+++ template/2015/wfh.php
@@ -71,7 +71,7 @@
 																				<input type="text" id="end_time" ng-click="timePick($event)" attribute1="{{wfh_day.DTR}}" readonly title="End Time: eg. 9:00" timepicker class="txtbox width55 wfh_time{{ $parent.$index+1 }} timepick_angular" data-dtr_index="{{ $parent.$index }}" ng-model="wfh_days[$parent.$index].ACTIVITIES[$index].end_time" required>
 																				<br>
 																				<label ng-show="$index == wfh_days[$parent.$index].ACTIVITIES.length - 1" title="When checked, credit hours will be deducted with 1 hour break time" >
-																					<input id="include_break{{ $parent.$index+1 }}" value="{{ wfh_days[$parent.$index].BREAKTIME }}" attribute1="{{ wfh_day.DTR }}"  type="checkbox" name="include_break[{{ $index+1 }}]" attribute="{{ $index+1 }}" class="mdtr_absent" ng-checked="wfh_days[$parent.$index].BREAKTIME == 1" ng-click="includeFunction($event)"> less 1 HR Break
+																					<input id="include_break{{ $parent.$index+1 }}" value="{{ wfh_days[$parent.$index].BREAKTIME }}" attribute1="{{ wfh_day.DTR }}"  type="checkbox" name="include_break[{{ $index+1 }}]" attribute="{{ $index+1 }}" class="mdtr_absent" ng-checked="wfh_days[$parent.$index].BREAKTIME == 1" ng-click="includeFunction($event)" disabled> less 1 HR Break
 																				</label>
 																			</td>
 																		</tr>
@@ -346,7 +346,7 @@
 	          //time start and end within 12:00 - 1:00
 	          console.log('entry start and end within 12:00-1:00');
 	          var duration = 0;
-	 
+
 	        }else if( start < twelve && end > twelve  && end < one){
 	          console.log('entry end within 12:00-1:00');
 	          //time end within 12:00 - 1:00 dont add end minutes
