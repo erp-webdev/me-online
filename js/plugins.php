d9f342353683c78ee53cdcbe3b21a48d3e49cf8f
megaworld-laptop <megaworld-laptop@ISM-Laptop14.mphi.com>
2020-06-11 10:56:13 +0800

    update

--- js/plugins.php
+++ js/plugins.php
@@ -5227,7 +5227,7 @@ $(function() {
 
 	$(".datepickwh").datepicker({
         dateFormat: 'yy-mm-dd',
-        minDate: $(this).attr('attribute1'),
+        minDate: $(".datepickwh").attr('attribute1'),
         maxDate: "-1D",
         changeMonth: true,
         changeYear: true
