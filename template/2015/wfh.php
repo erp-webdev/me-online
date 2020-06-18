3730cade0ddf33879821cd2fb486a6afa888b378
megaworld-laptop <megaworld-laptop@ISM-Laptop14.mphi.com>
2020-06-18 11:11:51 +0800

    update

--- template/2015/wfh.php
+++ template/2015/wfh.php
@@ -81,7 +81,7 @@
 																	</table>
 																</td>
 																<td >
-																	<textarea rows="1" style="display:none" name="wfh_activity[{{ $index+1 }}]" id="wfh_activity{{ $index+1 }}" value="{{ JSON.stringify($scope.wfh_days[$index+1].ACTIVITIES) }}" class="txtbox"></textarea>
+																	<textarea rows="1" style="display:none" name="wfh_activity[{{ $index+1 }}]" id="wfh_activity{{ $index+1 }}" value="{{ JSON.stringify($scope.wfh_days[$parent.$index].ACTIVITIES) }}" class="txtbox"></textarea>
 																	<table >
 																		<tr ng-repeat="activity in wfh_day.ACTIVITIES">
 																			<td style="border-bottom: 0px; margin: 0; padding: 0">
