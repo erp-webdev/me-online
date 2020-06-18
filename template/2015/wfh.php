4783335e7b68fbed7714ffff182beef7101ea712
megaworld-laptop <megaworld-laptop@ISM-Laptop14.mphi.com>
2020-06-18 11:03:03 +0800

    update

--- template/2015/wfh.php
+++ template/2015/wfh.php
@@ -654,7 +654,6 @@
 					else
 						$scope.wfh_days[i].CREDIT = 0;
 
-					console.log(eval(i+1)+"  "+JSON.stringify($scope.wfh_days[i].ACTIVITIES));
 					$('#wfh_activity'+eval(i+1)).attr('value', JSON.stringify($scope.wfh_days[i].ACTIVITIES));
 					$('#wfh_activity'+eval(i+1)).text(JSON.stringify($scope.wfh_days[i].ACTIVITIES));
 				}
@@ -688,6 +687,7 @@
 						$scope.wfh_days[i].CREDIT = 0;
 
 					$('#wfh_activity'+eval(i+1)).text(JSON.stringify($scope.wfh_days[i].ACTIVITIES));
+					$('#wfh_activity'+eval(i+1)).attr('value', JSON.stringify($scope.wfh_days[i].ACTIVITIES));
 				}
 
 			});
