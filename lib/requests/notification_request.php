a6bf430f2b4b2800f76253c9e832f1666a9181eb
megaworld-laptop <megaworld-laptop@ISM-Laptop14.mphi.com>
2020-07-03 11:01:16 +0800

    update

--- lib/requests/notification_request.php
+++ lib/requests/notification_request.php
@@ -7095,7 +7095,7 @@
 																	<td colspan="2">
 																		<div style="max-height: 120px; overflow-y: auto;">
 																			<script>
-																				$('#btncancel').scrollIntoView();
+																				$('#btncancel')[0].scrollIntoView();
 																			</script>
 																			<table width="100%">
 
