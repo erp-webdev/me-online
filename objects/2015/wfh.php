b3eeb447de9acc88630604fe0f5e559e2ad0644c
megaworld-laptop <megaworld-laptop@ISM-Laptop14.mphi.com>
2020-07-13 08:53:52 +0800

    update

--- objects/2015/wfh.php
+++ objects/2015/wfh.php
@@ -15,6 +15,12 @@
 
             global $sroot, $profile_id, $unix3month;
 
+						//test here
+						ini_set('session.gc_maxlifetime', 30);
+						echo session.gc_maxlifetime;
+						exit();
+						//
+
             $yesterday = date("d", strtotime('-1 day'));
 
             if($yesterday < 15){
