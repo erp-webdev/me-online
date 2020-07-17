fce5dddb25e4b7147cf4d7c29b2ff9c630f5bddf
megaworld-laptop <megaworld-laptop@ISM-Laptop14.mphi.com>
2020-07-17 14:22:19 +0800

    update

--- lib/requests/app_request.php
+++ lib/requests/app_request.php
@@ -1979,6 +1979,8 @@
                     $vdates = date('Y-m-d', $sdate_array[$key]);
 
                     $shiftsched2 = $mainsql->get_shiftid_fromhrdtr($idnum, $vdates);
+										var_dump($shiftsched2);
+										exit();
                     $sft2 = $shiftsched2[0]['ShiftID'];
 
 										// $empty_date = date('w', strtotime($vdates));
