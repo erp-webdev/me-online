af2f16d3c62d773e4f4da8f3352e7ea286ee65eb
megaworld-laptop <megaworld-laptop@ISM-Laptop14.mphi.com>
2020-07-20 11:09:32 +0800

    update

--- template/2015/otherforms.php
+++ template/2015/otherforms.php
@@ -44,13 +44,12 @@
 																										<label>{{ item.title }}</label>
 																									</td>
 																									<td>
-																										{{ item.type}}
-																										<!-- <div ng-if="{{ item.type }}">
+																										<div ng-if="{{ item.type != '' }}">
 																											<input type="{{ item.type }}" class="{{ item.hclass ? item.hclass : '' }}" value="{{ item.defaultValue ? item.defaultValue : '' }}">
 																										</div>
-																										<div ng-if="{{ !item.type }}">
+																										<div ng-if="{{ item.type == '' }}">
 																											<input type="text" class="{{ item.hclass ? item.hclass : '' }}" value="{{ item.defaultValue ? item.defaultValue : '' }}">
-																										</div> -->
+																										</div>
 																									</td>
 																								</tr>
 
