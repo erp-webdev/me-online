b7a0399fed6f1a694c718a8dc2ba7ffc4b8b79d3
megaworld-laptop <megaworld-laptop@ISM-Laptop14.mphi.com>
2020-07-20 11:28:37 +0800

    update

--- template/2015/otherforms.php
+++ template/2015/otherforms.php
@@ -46,7 +46,7 @@
 																									<td>
 																										<div ng-if="propExist(item.type) == 1">
 																											{{ item.type }}
-																											<input {{ "type=" }} class="{{ item.hclass ? item.hclass : '' }}" value="{{ item.defaultValue ? item.defaultValue : '' }}">
+																											<input type="{{ item.type ? 'radio' : text }}" class="{{ item.hclass ? item.hclass : '' }}" value="{{ item.defaultValue ? item.defaultValue : '' }}">
 																										</div>
 																										<div ng-if="propExist(item.type) == 0">
 																											no type
