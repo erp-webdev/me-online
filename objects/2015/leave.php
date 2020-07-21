0d52043156ff1eeee4ff626153ee6f7693fa2b71
megaworld-laptop <megaworld-laptop@ISM-Laptop14.mphi.com>
2020-07-21 11:25:49 +0800

    update

--- objects/2015/leave.php
+++ objects/2015/leave.php
@@ -36,7 +36,7 @@
 								}
 
 								$calculate_balance = $mainsql->calculate_lbalance($lbalancepost, $dbname);
-								echo "<script>console.log('$calculate_balance');</script>" 
+								echo "<script>console.log('$calculate_balance');</script>";
 								$cnt++;
 
 						endwhile;
