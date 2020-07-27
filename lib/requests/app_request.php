cf057e1c9f5712def9f02a7708fe9ab47cbae80a
megaworld-laptop <megaworld-laptop@ISM-Laptop14.mphi.com>
2020-07-27 14:46:16 +0800

    update

--- lib/requests/app_request.php
+++ lib/requests/app_request.php
@@ -2563,7 +2563,7 @@
 											"type" => 'short_text',
 											"hclass" => "form-control input-sm",
 											"defaultValue" => "",
-											"ronly" => "false"
+											"readonly" => "false"
 										]
 									],
 								"approvers" => "frmApplicationWHWeb",
