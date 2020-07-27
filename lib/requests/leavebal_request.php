42fa0751199219924523212638fbb796186a87a1
megaworld-laptop <megaworld-laptop@ISM-Laptop14.mphi.com>
2020-07-27 15:55:19 +0800

    update

--- lib/requests/leavebal_request.php
+++ lib/requests/leavebal_request.php
@@ -241,7 +241,7 @@
 										<tr>
 											<td><b>Usable Balance</b></td>
 											<?php if ($profile_compressed) : ?>
-											<td class="righttalign"><?php echo $usable_balance[0]['BalanceHrs'] ? number_format($usable_balance[0]['BalanceHrs'], 2) : 0; ?> hours (<?php echo $usable_balance[0]['BalanceHrs'] ? number_format($usable_balance[0]['BalanceHrs'], 2) /8 : 0; ?> days)</td>
+											<td class="righttalign"><?php echo $usable_balance[0]['BalanceHrs'] ? number_format($usable_balance[0]['BalanceHrs'], 2) : 0; ?> hours (<?php echo $usable_balance[0]['BalanceHrs'] ? number_format($usable_balance[0]['BalanceHrs']/8, 2)  : 0; ?> days)</td>
 											<?php else : ?>
 											<td class="righttalign"><?php echo $usable_balance[0]['BalanceHrs'] ? number_format($usable_balance[0]['BalanceHrs'], 2)/8 : 0; ?> days</td>
 											<?php endif; ?>
