4f5d02a4758b3aef6543bba9b75c64b6afb5c105
megaworld-laptop <megaworld-laptop@ISM-Laptop14.mphi.com>
2020-07-27 15:31:25 +0800

    update

--- objects/2015/leavebal.php
+++ objects/2015/leavebal.php
@@ -1,26 +1,46 @@
 <?php
-	
+
 	if ($logged == 1) {
 
 		# PAGINATION
 		$page = isset($_GET["page"]) ? (int)$_GET["page"] : 1 ;
 		$start = NUM_ROWS * ($page - 1);
-		
+
 		//*********************** MAIN CODE START **********************\\
-			
+
 		# ASSIGNED VALUE
-		$page_title = "Leave Balance";	
-		
+		$page_title = "Leave Balance";
+
 		//***********************  MAIN CODE END  **********************\\
-		
+
 		global $sroot, $profile_id, $unix3month;
 
         $leave_data = $mainsql->get_leave();
+
+				$cnt = 0;
+
+				while($cnt < 4) :
+
+						$lbalancepost['EMPID'] = $profile_idnum;
+						if($cnt == 0){
+							$lbalancepost['LEAVEID'] = 'L01';
+						}elseif ($cnt == 1) {
+							$lbalancepost['LEAVEID'] = 'L02';
+						}elseif ($cnt == 2) {
+							$lbalancepost['LEAVEID'] = 'L03';
+						}elseif ($cnt == 3) {
+							$lbalancepost['LEAVEID'] = 'L05';
+						}
+
+						$calculate_balance = $mainsql->calculate_lbalance($lbalancepost, $dbname);
+						$cnt++;
+
+				endwhile;
 
 	}
 	else
 	{
 		echo "<script language='javascript' type='text/javascript'>window.location.href='".WEB."/login'</script>";
 	}
-	
+
 ?>
