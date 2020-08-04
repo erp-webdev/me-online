b61b7149f35443e5f317ebcffd9c748882d45990
megaworld-laptop <megaworld-laptop@ISM-Laptop14.mphi.com>
2020-08-04 13:51:30 +0800

    update

--- objects/2015/leave.php
+++ objects/2015/leave.php
@@ -213,7 +213,7 @@
 											$balanceval = $usable_balance[0]['BalanceHrs'];
 										}
 
-										echo '{"success": false, "error": "Dev Ongoing. Balance :'.$usable_balance[0]['BalanceHrs'].'"}';
+										echo '{"success": false, "error": "Dev Ongoing. Balance :'.$profile_idnum.'"}';
 										exit();
 
 										if($balanceval < $backend_hours){
