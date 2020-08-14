18babd2b749b6adb0b08c1c0dabb0d5769d49a64
megaworld-laptop <megaworld-laptop@ISM-Laptop14.mphi.com>
2020-08-14 10:17:55 +0800

    update

--- objects/2015/wfh.php
+++ objects/2015/wfh.php
@@ -105,13 +105,6 @@
 										exit();
 									}
 								}
-
-								//DTR START AUG 16
-								// if(date('Y-m-d') < '2020-08-17'){
-								// 	echo '{"success": false, "error": "WFH Application will start on August 17, 2020"}';
-								// 	exit();
-								// }
-
                 //CHECK DATE IF ITS APPLIED - END
 
                 //WFH ITEMS
