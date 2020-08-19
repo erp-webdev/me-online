b538d0edd077c04df9bd58599522c786d755eadc
Kevs <kevinayag@users.noreply.github.com>
2020-08-19 09:42:29 +0800

    club access feature

--- template/2015/index.php
+++ template/2015/index.php
@@ -147,20 +147,20 @@
 
                             <?php if (in_array($profile_dbname , ['GL', 'MCTI']) ) : ?>
                             <a href="<?php echo WEB; ?>/uploads/download/GLEmployeeHandbook.pdf" target="_blank">
-                                <img src="<?php echo WEB; ?>/images/glhandbook.png" style="float: right; top: 350px; position: absolute; right: 29px;" />
+                                <img src="<?php echo WEB; ?>/images/glhandbook.png" style="float: right; top: 450px; position: absolute; right: 29px;" />
                             </a>
 
                             <?php else : ?>
                             <?php if ($profile_dbname != 'ECINEMA' && $profile_dbname != 'EPARKVIEW' && $profile_dbname != 'NEWTOWN' && $profile_dbname != 'LAFUERZA') : ?>
                             <a href="<?php echo WEB; ?>/uploads/download/EmployeeHandbook.pdf" target="_blank">
-                                <img src="<?php echo WEB; ?>/images/mwhandbook.png" style="float: right; top: 350px; position: absolute; right: 29px;" />
+                                <img src="<?php echo WEB; ?>/images/mwhandbook.png" style="float: right; top: 450px; position: absolute; right: 29px;" />
                             </a>
 
                             <?php endif; ?>
                             <?php endif; ?>
 
                             <a href="https://tinyurl.com/yyp6m44z" target="_blank">
-                                <img src="<?php echo WEB; ?>/images/CLUBACC.png" style="float: right; top: 470px; position: absolute; right: 29px;" />
+                                <img src="<?php echo WEB; ?>/images/CLUBACC.png" style="float: right; top: 590px; position: absolute; right: 29px;" />
                             </a>
 
 
