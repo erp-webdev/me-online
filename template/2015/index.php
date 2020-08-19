4912f20aa43431db27f42cef4d3197f9ec6052c0
Kevs <kevinayag@users.noreply.github.com>
2020-08-19 09:43:31 +0800

    club access feature

--- template/2015/index.php
+++ template/2015/index.php
@@ -147,7 +147,7 @@
 
                             <?php if (in_array($profile_dbname , ['GL', 'MCTI']) ) : ?>
                             <a href="<?php echo WEB; ?>/uploads/download/GLEmployeeHandbook.pdf" target="_blank">
-                                <img src="<?php echo WEB; ?>/images/glhandbook.png" style="float: right; top: 450px; position: absolute; right: 29px;" />
+                                <img src="<?php echo WEB; ?>/images/glhandbook.png" style="float: right; top: 400px; position: absolute; right: 29px;" />
                             </a>
 
                             <?php else : ?>
