db324e1ff6687dd9092ae9a82acbc610e0c37798
Kevs <kevinayag@users.noreply.github.com>
2020-08-19 09:43:03 +0800

    club access feature

--- template/2015/index.php
+++ template/2015/index.php
@@ -153,14 +153,14 @@
                             <?php else : ?>
                             <?php if ($profile_dbname != 'ECINEMA' && $profile_dbname != 'EPARKVIEW' && $profile_dbname != 'NEWTOWN' && $profile_dbname != 'LAFUERZA') : ?>
                             <a href="<?php echo WEB; ?>/uploads/download/EmployeeHandbook.pdf" target="_blank">
-                                <img src="<?php echo WEB; ?>/images/mwhandbook.png" style="float: right; top: 450px; position: absolute; right: 29px;" />
+                                <img src="<?php echo WEB; ?>/images/mwhandbook.png" style="float: right; top: 400px; position: absolute; right: 29px;" />
                             </a>
 
                             <?php endif; ?>
                             <?php endif; ?>
 
                             <a href="https://tinyurl.com/yyp6m44z" target="_blank">
-                                <img src="<?php echo WEB; ?>/images/CLUBACC.png" style="float: right; top: 550px; position: absolute; right: 29px;" />
+                                <img src="<?php echo WEB; ?>/images/CLUBACC.png" style="float: right; top: 500px; position: absolute; right: 29px;" />
                             </a>
 
 
