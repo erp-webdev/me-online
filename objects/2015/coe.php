d5076b9d6b121ec191dd902e07919de37879e688
megaworld-laptop <megaworld-laptop@ISM-Laptop14.mphi.com>
2020-08-24 09:25:51 +0800

    update

--- objects/2015/coe.php
+++ objects/2015/coe.php
@@ -22,7 +22,7 @@
 		$coe_count = $mainsql->get_coe(0, 0, $profile_idnum, 1);
 		$pages = $mainsql->pagination("coe", $coe_count, NUM_ROWS, 9);
 
-        // var_dump($profile_dbname);
+        var_dump($coe_users);
         //var_dump($mreq_data);
 
 	}
