d44c02b8b15ca9cd2b8e25912bdabccc51ef0ad7
Kevs <kevinayag@users.noreply.github.com>
2020-09-23 09:50:18 +0800

    New Cutoff payslip

--- template/2015/payslip.php
+++ template/2015/payslip.php
@@ -79,7 +79,7 @@
                                             if($payslip_period[1]['PRYear'] > 2020 || ($payslip_period[1]['PRYear'] == 2020 && !in_array( $payslip_period[1]['PeriodID'], ['S01', 'S02', 'S03', 'S04', 'S05', 'S06', 'S07', 'S08', 'S09'])))
                                                 echo date("m/d/Y", strtotime($payslip_period[1]['PRFrom'] ? $payslip_period[1]['PRFrom'] : $payslip_period[0]['PRFrom'])) . " to " . date("m/d/Y", strtotime($payslip_period[1]['PRTo'] ? $payslip_period[1]['PRTo'] : $payslip_period[0]['PRTo'])); 
                                             else
-                                                echo date("m/d/Y", strtotime($payslip_period[1]['PeriodFrom'] ? $payslip_period[1]['PeriodFrom'] : $payslip_period[0]['PeriodFrom'])). " to " . echo date("m/d/Y", strtotime($payslip_period[1]['PeriodTo'] ? $payslip_period[1]['PeriodTo'] : $payslip_period[0]['PeriodTo'])); 
+                                                echo date("m/d/Y", strtotime($payslip_period[1]['PeriodFrom'] ? $payslip_period[1]['PeriodFrom'] : $payslip_period[0]['PeriodFrom'])). " to " . date("m/d/Y", strtotime($payslip_period[1]['PeriodTo'] ? $payslip_period[1]['PeriodTo'] : $payslip_period[0]['PeriodTo'])); 
                                             ?></span><br>
                                     <b>Pay Period: </b><span id='payto'><?php echo date("m/d/Y", strtotime($payslip_period[1]['PRTo'] ? $payslip_period[1]['PRTo'] : $payslip_period[0]['PRTo'])); ?></span><br>
                                     <b>Account #: </b><?php echo $profile_acctnum; ?><br>
