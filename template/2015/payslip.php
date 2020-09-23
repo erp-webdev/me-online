a368e8ffcf709921bb16bcb187396eca256c3ac0
Kevs <kevinayag@users.noreply.github.com>
2020-09-23 09:45:00 +0800

    New Cutoff payslip

--- template/2015/payslip.php
+++ template/2015/payslip.php
@@ -29,7 +29,14 @@
 
                                                 <?php if ($value['PeriodID'] != 'SP23' && ($profile_idnum != '2019-12-0708') ) : ?>
                                                 <option value="<?php echo $value['PeriodID']; ?>"<?php echo $key == 1 ? ' selected' : ''; ?>>
-                                                    <?php echo $value['PaymentType']." ".$value['PRYear']." ".date("m/d/Y", strtotime($value['PeriodFrom']))." to ".date("m/d/Y", strtotime($value['PeriodTo'])); ?>
+                                                    <?php 
+                                                        // new payslip period beginning april2020
+                                                        if(value['PRYear'] > 2020 || (value['PRYear'] == 2020 && !in_array( value['PeriodID'], ['S01', 'S02', 'S03', 'S04', 'S05', 'S06', 'S07', 'S08', 'S09'])))
+                                                            echo $value['PaymentType']." ".$value['PRYear']." ".date("m/d/Y", strtotime($value['PRFrom']))." to ".date("m/d/Y", strtotime($value['PRTo']));
+                                                        else
+                                                            echo $value['PaymentType']." ".$value['PRYear']." ".date("m/d/Y", strtotime($value['PeriodFrom']))." to ".date("m/d/Y", strtotime($value['PeriodTo']));
+                                                    ?>
+                                                    <?php //echo $value['PaymentType']." ".$value['PRYear']." ".date("m/d/Y", strtotime($value['PeriodFrom']))." to ".date("m/d/Y", strtotime($value['PeriodTo'])); ?>
                                                 </option>  
                                                 <?php endif; ?>
                                                 <?php endforeach; ?>
