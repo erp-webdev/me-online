bf23037640f4aa2eadfca41983a9ac3b7083c1a6
megaworld-laptop <megaworld-laptop@ISM-Laptop14.mphi.com>
2020-10-07 09:25:24 +0800

    update

--- objects/2015/leave.php
+++ objects/2015/leave.php
@@ -213,8 +213,8 @@
 											$balanceval = $usable_lbalance[0]['BalanceHrs'];
 										}
 
-										echo '{"success": false, "error": "Dev Test: BalanceVal: '.$balanceval.' BackendHours: '.$backend_hours.' Compressed: '.$profile_compressed.'"}';
-										exit();
+										// echo '{"success": false, "error": "Dev Test: BalanceVal: '.$balanceval.' BackendHours: '.$backend_hours.' Compressed: '.$profile_compressed.'"}';
+										// exit();
 
 										if($balanceval < $backend_hours){
 											echo '{"success": false, "error": "Your leave with pay is greater than your leave balance"}';
