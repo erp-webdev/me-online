26d8ede8cd8cdd8bc6d8f04cc961a3914a58ce8c
megaworld-laptop <megaworld-laptop@ISM-Laptop14.mphi.com>
2020-11-16 08:19:36 +0800

    update profile

--- template/2015/profile.php
+++ template/2015/profile.php
@@ -1128,7 +1128,23 @@
 
 			// $("#EmailAdd2").inputmask("{1,20}@{1,20}.*{3}");
 
-			Inputmask("email").mask("#EmailAdd2");
+			// Inputmask("email").mask("#EmailAdd2");
+
+			//email mask
+		  $("#EmailAdd2").inputmask({
+		    mask: "*{1,20}[.*{1,20}][.*{1,20}][.*{1,20}]@*{1,20}[.*{2,6}][.*{1,2}]",
+		    greedy: false,
+		    onBeforePaste: function (pastedValue, opts) {
+		      pastedValue = pastedValue.toLowerCase();
+		      return pastedValue.replace("mailto:", "");
+		    },
+		    definitions: {
+		      '*': {
+		        validator: "[0-9A-Za-z!#$%&'*+/=?^_`{|}~\-]",
+		        casing: "lower"
+		      }
+		    }
+		  });
 
 
 			$("#samplebutton").on('click', function(){
@@ -1141,21 +1157,21 @@
 				//   alert('not a valid e-mail address');
 				// }​
 
-				var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-				if(userinput.match(mailformat))
-				{
-				alert("Valid email address!");
-				}
-				else
-				{
-				alert("You have entered an invalid email address!");
-				}
-
-				// if(!($('#EmailAdd2').inputmask("isComplete"))){
-				// 	alert("Your personal e-mail address is either incomplete or incorrect.");
-				// }else{
-				// 	alert($('#EmailAdd2').inputmask("isComplete"));
+				// var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+				// if(userinput.match(mailformat))
+				// {
+				// alert("Valid email address!");
 				// }
+				// else
+				// {
+				// alert("You have entered an invalid email address!");
+				// }
+
+				if(!($('#EmailAdd2').inputmask("isComplete"))){
+					alert("Your personal e-mail address is either incomplete or incorrect.");
+				}else{
+					alert($('#EmailAdd2').inputmask("isComplete"));
+				}
 
 			});
 
