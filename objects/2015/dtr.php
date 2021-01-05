51a1fba34362e54f89080597006568f61b9bcf5c
Spencer Hart <shart.global@mphi.com>
2021-01-05 15:39:01 +0800

    dtr checking

--- objects/2015/dtr.php
+++ objects/2015/dtr.php
@@ -1,59 +1,59 @@
 <?php
-	
+
 	if ($logged == 1) {
 
 		# PAGINATION
 		$page = isset($_GET["page"]) ? (int)$_GET["page"] : 1 ;
 		$start = NUM_ROWS * ($page - 1);
-		
+
 		//*********************** MAIN CODE START **********************\\
-			
+
 		# ASSIGNED VALUE
-		$page_title = "Daily Time Record";	
-		
+		$page_title = "Daily Time Record";
+
 		//***********************  MAIN CODE END  **********************\\
-		
+
 		global $sroot, $profile_id, $unix3month;
-        
-        $dtr_year = $mainsql->get_dtr_year($profile_comp);          
+
+        $dtr_year = $mainsql->get_dtr_year($profile_comp);
         $yearend = strtotime(date('Y').'-12-15');
         $yearval = date('U') > $yearend ? date("Y") + 1 : date("Y");
-        $dtr_period = $mainsql->get_dtr_period($yearval, $profile_comp, 1);          
-        
-        //var_dump($dtr_period);
-        
+        $dtr_period = $mainsql->get_dtr_period($yearval, $profile_comp, 1);
+
+        var_dump($dtr_period);
+
         $cleanfrom = str_replace("  ", " ", $dtr_period[0]['PeriodFrom']);
         $cleanto = str_replace("  ", " ", $dtr_period[0]['PeriodTo']);
-	
-	
+
+
         $expfrom = explode(" ", $cleanfrom);
         $expto = explode(" ", $cleanto);
-        
+
         $dfrom = strtotime($expfrom[0].' '.$expfrom[1].' '.$expfrom[2].' 00:00:00');
         $dto = strtotime($expto[0].' '.$expto[1].' '.$expto[2].' 23:59:59');
         $posted = $dtr_period[0]['AttPost'];
-        $daynow = date('j');            
-        
+        $daynow = date('j');
+
         //var_dump($dtr_period[0]['PeriodFrom'].' '.$dtr_period[0]['PeriodTo'].' '.$posted);
-        
+
         if ($posted == 0) :
-            
-            while($dfrom < $dto + 86400) : 
-        
-                $_POST['strEMPID'] = $profile_idnum;            
+
+            while($dfrom < $dto + 86400) :
+
+                $_POST['strEMPID'] = $profile_idnum;
                 $_POST['dteDTRDate'] = date("m/d/Y", $dfrom);
                 $_POST['OVERWRITE'] = 0;
-                $_POST['STATUS'] = 'INITIAL'; 
-                $_POST['intFINALPAY'] = 0;                
-        
+                $_POST['STATUS'] = 'INITIAL';
+                $_POST['intFINALPAY'] = 0;
+
                 $daydtr = date('j', $dfrom);
-                $month15 = date('U', strtotime(date('Y-m-15')));                        
+                $month15 = date('U', strtotime(date('Y-m-15')));
                 $lastyear = date('Y', strtotime("-1 month"));
                 $lastmonth = date('m', strtotime("-1 month"));
                 $lastday = date('t', strtotime("-1 month"));
-                $month15l = date('U', strtotime(date($lastyear.'-'.$lastmonth.'-15')));   
+                $month15l = date('U', strtotime(date($lastyear.'-'.$lastmonth.'-15')));
                 $monthlast = date('U', strtotime(date($lastyear.'-'.$lastmonth.'-'.$lastday)));
-        
+
                 if ($daynow >= 3 && $daynow < 18) :
                     $lastpoint = $monthlast;
                 else :
@@ -62,30 +62,30 @@
                     else :
                         $lastpoint = $month15;
                     endif;
-                endif;  
+                endif;
 
                 if ($dfrom > $lastpoint) :
                     $dtr_calculate = $mainsql->dtr_action($_POST, 'calculate');
-                endif;              
+                endif;
 
                 //$dtr_calculate = $mainsql->dtr_action($_POST, 'calculate');
-        
+
                 //var_dump($dfrom);
-                
+
                 $dfrom = $dfrom + 86400;
-            endwhile;     
+            endwhile;
         endif;
-        
-        //$dtr_data = $mainsql->get_dtr_data($profile_idnum, date("m/d/Y", strtotime($dtr_period[0])), date("m/d/Y", strtotime($dtr_period[1])));  
-        
-        $dtr_data = $mainsql->get_dtr_data($profile_idnum, date("m/d/Y", strtotime($expfrom[0].' '.$expfrom[1].' '.$expfrom[2].' 00:00:00')), date("m/d/Y", strtotime($expto[0].' '.$expto[1].' '.$expto[2].' 23:59:59')), $profile_comp);  
-        
+
+        //$dtr_data = $mainsql->get_dtr_data($profile_idnum, date("m/d/Y", strtotime($dtr_period[0])), date("m/d/Y", strtotime($dtr_period[1])));
+
+        $dtr_data = $mainsql->get_dtr_data($profile_idnum, date("m/d/Y", strtotime($expfrom[0].' '.$expfrom[1].' '.$expfrom[2].' 00:00:00')), date("m/d/Y", strtotime($expto[0].' '.$expto[1].' '.$expto[2].' 23:59:59')), $profile_comp);
+
         //var_dump($dtr_data);
-        
+
 	}
 	else
 	{
 		echo "<script language='javascript' type='text/javascript'>window.location.href='".WEB."/login'</script>";
 	}
-	
+
 ?>
