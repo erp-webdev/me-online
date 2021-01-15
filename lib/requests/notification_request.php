76ca2c8e71ce41807a37d07f31e4e643cce958a9
Spencer Hart <shart.global@mphi.com>
2021-01-15 16:39:22 +0800

    coe sending dev

--- lib/requests/notification_request.php
+++ lib/requests/notification_request.php
@@ -5818,7 +5818,7 @@
 				<center><h3>Please close print preview.</h3></center></div>
 			<?php
 			if($send_email){
-				echo "send email ".var_dump($send_email);
+
 				$html2pdf = new HTML2PDF('P', 'A4', 'en', true, 'UTF-8', array(0, 0, 0, 0));
 				$html2pdf->pdf->SetDisplayMode('fullpage');
 
@@ -5828,9 +5828,8 @@
 
 			    $html2pdf->writeHTML($content);
 				$html2pdf->Output('sample.pdf');
-			}else{
-				echo "print coe ".$send_email;
 
+			}else{
 				include('coe_template_request.php');
 			}
 
