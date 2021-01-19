692c1e34a4b7a6ec47447482b3b6c1b8acabecfd
Spencer Hart <shart.global@mphi.com>
2021-01-19 09:44:51 +0800

    coe pdf update

--- lib/requests/notification_request.php
+++ lib/requests/notification_request.php
@@ -5815,25 +5815,54 @@
 
 			if($send_email){
 
-				ob_start();
-				include('coe_template_request.php');
-				$content = ob_get_clean();
 
-				require_once(DOCUMENT.'/lib/html2pdf/html2pdf.class.php');
-				try
-				{
-					$html2pdf = new HTML2PDF('P', 'Letter', 'en');
-					$html2pdf->setDefaultFont('Arial');
-					ob_start();
-					$html2pdf->writeHTML($content, false);
-					ob_end_clean();
-					$html2pdf->Output('coe.pdf', 'I');
+				require_once(DOCUMENT.'/lib/tcpdf/tcpdf.php');
+				// Create new PDF
+				$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 
-				}
-				catch(HTML2PDF_exception $e) {
-					echo $e;
-					exit;
-				}
+				$pdf->SetFont("helvetica", '', 10);
+
+				$pdf->AddPage();
+
+				// Important: this will prevent the generation of the PDF in the browser
+				// as we are filling the output buffer of PHP
+				var_dump(array(
+				    "test" => "demo"
+				));
+
+				// create some HTML content
+				$html = '<h1>HTML Example</h1>
+				<h2>List</h2>
+				List example:
+				<ol>
+				    <li><b>bold text</b></li>
+				    <li><i>italic text</i></li>
+				    <li><u>underlined text</u></li>
+				    <li><b>b<i>bi<u>biu</u>bi</i>b</b></li>
+				    <li><a href="http://www.tecnick.com" dir="ltr">link to http://www.tecnick.com</a></li>
+				    <li>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.<br />Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</li>
+				    <li>SUBLIST
+				        <ol>
+				            <li>row one
+				                <ul>
+				                    <li>sublist</li>
+				                </ul>
+				            </li>
+				            <li>row two</li>
+				        </ol>
+				    </li>
+				    <li><b>T</b>E<i>S</i><u>T</u> <del>line through</del></li>
+				    <li><font size="+3">font + 3</font></li>
+				    <li><small>small text</small> normal <small>small text</small> normal <sub>subscript</sub> normal <sup>superscript</sup> normal</li>
+				</ol>
+				</div>';
+
+				// Output the HTML content
+				$pdf->writeHTML($html, true, false, true, false, '');
+
+				$pdf->lastPage();
+
+				$pdf->Output('example_006.pdf', 'I');
 
 
 			}else{
