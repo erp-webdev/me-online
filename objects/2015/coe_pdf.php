5daac91d1d80128f60633b89a6f07106889388cd
Spencer Hart <shart.global@mphi.com>
2021-01-19 11:20:46 +0800

    coe pdf update

--- objects/2015/coe_pdf.php
+++ objects/2015/coe_pdf.php
@@ -88,6 +88,10 @@ require_once(DOCUMENT.'/lib/tcpdf/tcpdf.php');
 // Output the HTML content
 $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 
+$pdf->AddPage();
+$pdf->SetPrintHeader(false);
+$pdf->SetPrintFooter(false);
+
 $pdf->writeHTML($content);
 
 $pdf->lastPage();
