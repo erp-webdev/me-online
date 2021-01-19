5d3a790d9e345d877a9e5770102b22126f68ef96
Spencer Hart <shart.global@mphi.com>
2021-01-19 15:10:38 +0800

    coe pdf update

--- template/2015/coe_pdf.php
+++ template/2015/coe_pdf.php
@@ -337,7 +337,7 @@ if($coe[0]["type"] == "COEAPPROVEDLEAVE"){ // COE with Approved Leave
 
 		$tasks = json_decode($coe[0]["job_desc"], true);
 		?>
-			<h3 class="coetitle" align="center" style="padding-top: 150px; letter-spacing: 10px;">CERTIFICATION</h3>
+			<h3 class="coetitle" align="center" style="padding-bottom: 150px; letter-spacing: 10px;">CERTIFICATION</h3>
 			&nbsp;
 
 		<div style="text-align: justify;  text-justify: inter-word;">
