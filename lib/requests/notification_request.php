3b8bc176c1ef19d8af3d5ad73064b687e40887c3
Spencer Hart <shart.global@mphi.com>
2021-01-19 08:51:23 +0800

    coe pdf update

--- lib/requests/notification_request.php
+++ lib/requests/notification_request.php
@@ -5825,6 +5825,7 @@
 					$html2pdf = new HTML2PDF('P', 'Letter', 'en');
 					$html2pdf->setDefaultFont('Arial');
 					$html2pdf->writeHTML($content, FALSE);
+					ob_end_clean();
 					$html2pdf->Output('coe.pdf');
 				}
 				catch(HTML2PDF_exception $e) {
