ec4b67f293daf1e9d19b9d1c5629e02a13993138
Spencer Hart <shart.global@mphi.com>
2021-01-20 09:40:58 +0800

    coe pdf update

--- objects/2015/coe_pdf.php
+++ objects/2015/coe_pdf.php
@@ -1,120 +1,123 @@
 <?php
 
-$id = $_GET["id"];
-$type = $_GET["type"];
-if($_GET["send"] == "true"){
-	$send_email = TRUE;
-}else{
-	$send_email = FALSE;
-}
+	$id = $_GET["id"];
+	$type = $_GET["type"];
 
-$sql = "SELECT * FROM COERequests WHERE id = $id";
+	if($_GET["send"] == "true"){
+		$send_email = TRUE;
+	}else{
+		$send_email = FALSE;
+	}
 
-$coe = $mainsql->get_row($sql);
-$emp_id = $coe[0]['emp_id'];
+	$sql = "SELECT * FROM COERequests WHERE id = $id";
 
-$query = "SELECT top 1
-			CASE
-				WHEN A.GENDER = 'F' or A.GENDER = 'FEMALE' THEN 'Ms.'
-				WHEN A.GENDER = 'M' or A.GENDER = 'MALE' THEN 'Mr.'
-			END AS Salutation,
-			CASE
-				WHEN A.GENDER = 'F' or A.GENDER = 'FEMALE' THEN 'She'
-				WHEN A.GENDER = 'M' or A.GENDER = 'MALE' THEN 'He'
-			END AS Gender,
-			CASE
-				WHEN A.GENDER = 'F' or A.GENDER = 'FEMALE' THEN 'her'
-				WHEN A.GENDER = 'M' or A.GENDER = 'MALE' THEN 'his'
-			END AS Gender2,
-			CASE
-				WHEN A.GENDER = 'F' or A.GENDER = 'FEMALE' THEN 'her'
-				WHEN A.GENDER = 'M' or A.GENDER = 'MALE' THEN 'him'
-			END AS Gender3,
-			A.FName+' '+SUBSTRING(A.MNAME, 1, 1)+'. '+A.LName AS FullName,
-			A.Allowance,
-			A.MonthlyRate,
-			A.RankID,
-			B.RankDesc,
-			C.DeptDesc,
-			D.DivisionName,
-			E.PositionDesc,
-			A.HireDate as HireDate,
-			getdate() as CurrentDate,
-			A.DateResigned as DateResigned,
-			A.CompanyID,
-			F.CompanyName
-			FROM
-				viewhrempmaster A
-			LEFT JOIN
-				HRRank B on A.RankID = B.RankID
-			LEFT JOIN
-				HRDepartment C on A.DeptID = C.DeptID
-			LEFT JOIN
-				HRDivision D on A.DivisionID = D.DivisionID
-			LEFT JOIN
-				HRPosition E on A.PositionID = E.PositionID
-			LEFT JOIN
-				HRCompany F on A.CompanyID = F.CompanyID
-			WHERE
-				A.EmpID = '$emp_id'";
+	$coe = $mainsql->get_row($sql);
+	$emp_id = $coe[0]['emp_id'];
 
-
-$emp_info = $mainsql->get_row($query);
-
-$emp_info[0]['HireDate'] = $emp_info[0]['HireDate'] ?  date('F j, Y', strtotime($emp_info[0]['HireDate'])) : null;
-$emp_info[0]['CurrentDate'] = $emp_info[0]['CurrentDate'] ?  date('F j, Y', strtotime($emp_info[0]['CurrentDate'])) : null;
-$emp_info[0]['DateResigned'] = $emp_info[0]['DateResigned'] ?  date('F j, Y', strtotime($emp_info[0]['DateResigned'])) : null;
-
-ob_start();
-include(TEMP.'/coe_pdf.php');
-$content = ob_get_clean();
-
-require_once(DOCUMENT.'/lib/tcpdf/tcpdf.php');
-
-$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
-$pdf->SetPrintHeader(false);
-$pdf->SetPrintFooter(false);
-$pdf->SetLeftMargin(25);
-$pdf->SetRightMargin(25);
-$pdf->SetTopMargin(50);
-$pdf->SetFont('times');
-$pdf->AddPage();
-$pdf->writeHTML($content);
+	$query = "SELECT top 1
+				CASE
+					WHEN A.GENDER = 'F' or A.GENDER = 'FEMALE' THEN 'Ms.'
+					WHEN A.GENDER = 'M' or A.GENDER = 'MALE' THEN 'Mr.'
+				END AS Salutation,
+				CASE
+					WHEN A.GENDER = 'F' or A.GENDER = 'FEMALE' THEN 'She'
+					WHEN A.GENDER = 'M' or A.GENDER = 'MALE' THEN 'He'
+				END AS Gender,
+				CASE
+					WHEN A.GENDER = 'F' or A.GENDER = 'FEMALE' THEN 'her'
+					WHEN A.GENDER = 'M' or A.GENDER = 'MALE' THEN 'his'
+				END AS Gender2,
+				CASE
+					WHEN A.GENDER = 'F' or A.GENDER = 'FEMALE' THEN 'her'
+					WHEN A.GENDER = 'M' or A.GENDER = 'MALE' THEN 'him'
+				END AS Gender3,
+				A.FName+' '+SUBSTRING(A.MNAME, 1, 1)+'. '+A.LName AS FullName,
+				A.Allowance,
+				A.MonthlyRate,
+				A.RankID,
+				B.RankDesc,
+				C.DeptDesc,
+				D.DivisionName,
+				E.PositionDesc,
+				A.HireDate as HireDate,
+				getdate() as CurrentDate,
+				A.DateResigned as DateResigned,
+				A.CompanyID,
+				F.CompanyName
+				FROM
+					viewhrempmaster A
+				LEFT JOIN
+					HRRank B on A.RankID = B.RankID
+				LEFT JOIN
+					HRDepartment C on A.DeptID = C.DeptID
+				LEFT JOIN
+					HRDivision D on A.DivisionID = D.DivisionID
+				LEFT JOIN
+					HRPosition E on A.PositionID = E.PositionID
+				LEFT JOIN
+					HRCompany F on A.CompanyID = F.CompanyID
+				WHERE
+					A.EmpID = '$emp_id'";
 
 
-ob_end_clean();
-$file_attach = $pdf->Output('CertificateOfEmployment.pdf', 'E');
+	$emp_info = $mainsql->get_row($query);
 
-require_once(DOCUMENT.'/lib/phpmailer/src/Exception.php');
-require_once(DOCUMENT.'/lib/phpmailer/src/PHPMailer.php');
-require_once(DOCUMENT.'/lib/phpmailer/src/SMTP.php');
+	$emp_info[0]['HireDate'] = $emp_info[0]['HireDate'] ?  date('F j, Y', strtotime($emp_info[0]['HireDate'])) : null;
+	$emp_info[0]['CurrentDate'] = $emp_info[0]['CurrentDate'] ?  date('F j, Y', strtotime($emp_info[0]['CurrentDate'])) : null;
+	$emp_info[0]['DateResigned'] = $emp_info[0]['DateResigned'] ?  date('F j, Y', strtotime($emp_info[0]['DateResigned'])) : null;
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
+	$file_attachment = getCoePdf();
+	function getCoePdf(){
+		ob_start();
+		include(TEMP.'/coe_pdf.php');
+		$content = ob_get_clean();
 
-$email = new PHPMailer();
-$email->SetFrom(NOTIFICATION_EMAIL); //Name is optional
+		require_once(DOCUMENT.'/lib/tcpdf/tcpdf.php');
 
-$message = "<div style='display: block; border: 5px solid #024485; padding: 10px; font-size: 12px; font-family: Verdana; width: 95%;'><span style='font-size: 18px; color: #024485; font-weight: bold;'>Certificate of Employment Request</span><br><br>Hi ,<br><br>";
+		$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+		$pdf->SetPrintHeader(false);
+		$pdf->SetPrintFooter(false);
+		$pdf->SetLeftMargin(25);
+		$pdf->SetRightMargin(25);
+		$pdf->SetTopMargin(50);
+		$pdf->SetFont('times');
+		$pdf->AddPage();
+		$pdf->writeHTML($content);
 
-$message .= "Your Certificate of Employment has been processed, Kindly see the attached file.";
-$message .= "<br><br>Thanks,<br>";
-$message .= SITENAME." Admin";
-$message .= "<hr />".MAILFOOT."</div>";
+		ob_end_clean();
+		return $pdf->Output('CertificateOfEmployment.pdf', 'E');
+	}
 
-$email->Subject   = 'COE Request Update';
-$email->Body      = $message;
-$email->AddAddress( 'shart.global@megaworldcorp.com' );
+	require_once(DOCUMENT.'/lib/phpmailer/src/Exception.php');
+	require_once(DOCUMENT.'/lib/phpmailer/src/PHPMailer.php');
+	require_once(DOCUMENT.'/lib/phpmailer/src/SMTP.php');
 
-$email->AddStringAttachment($file_attach , 'CertificateOfEmployment.pdf' );
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\SMTP;
+	use PHPMailer\PHPMailer\Exception;
 
-$email->Send();
+	$email = new PHPMailer();
+	$email->SetFrom(NOTIFICATION_EMAIL); //Name is optional
+
+	$message = "<div style='display: block; border: 5px solid #024485; padding: 10px; font-size: 12px; font-family: Verdana; width: 95%;'><span style='font-size: 18px; color: #024485; font-weight: bold;'>Certificate of Employment Request</span><br><br>Hi ,<br><br>";
+
+	$message .= "Your Certificate of Employment has been processed, Kindly see the attached file.";
+	$message .= "<br><br>Thanks,<br>";
+	$message .= SITENAME." Admin";
+	$message .= "<hr />".MAILFOOT."</div>";
+
+	$email->Subject   = 'COE Request Update';
+	$email->Body      = $message;
+	$email->AddAddress( 'shart.global@megaworldcorp.com' );
+
+	$email->AddStringAttachment($file_attachment , 'CertificateOfEmployment.pdf', 'base64', 'application/pdf');
+
+	$email->Send();
 
 
-if ($email) {
-    echo "The email was sent.";
-} else {
-    echo "There was an error sending the mail.";
-}
+	if ($email) {
+	    echo "The email was sent.";
+	} else {
+	    echo "There was an error sending the mail.";
+	}
 ?>
