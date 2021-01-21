c29ae35b6981765fa2b2ec7db85183e1448550b8
Spencer Hart <shart.global@mphi.com>
2021-01-21 09:27:53 +0800

    coe sending pdf with coe update

--- template/2015/memo.php
+++ template/2015/memo.php
@@ -2,9 +2,9 @@
 
     <!-- BODY -->
 
-                    <div id="floatdiv" class="floatdiv invisible">                                         
-                        <?php if ($profile_level == 7 || $profile_level == 9 || $profile_level == 10) : ?>
-                        <!-- CREATE MEMO - BEGIN --> 
+                    <div id="floatdiv" class="floatdiv invisible">
+                        <?php if ($profile_level == 7 || $profile_level == 9 || $profile_level == 10 || $profile_id == "2016-06-0457" ) : ?>
+                        <!-- CREATE MEMO - BEGIN -->
                         <div id="madd" class="fadd" style="display: none;">
                             <div class="closebutton cursorpoint"><i class="fa fa-times-circle fa-3x redtext"></i></div>
                             <div id="madd_title" class="robotobold cattext dbluetext">Create Memo</div>
@@ -36,17 +36,17 @@
                                     </td>
                                 </tr>
                                 </form>
-                            </table>  
+                            </table>
                         </div>
-                        <!-- CREATE MEMO - END -->                    
-                        <!-- UPDATE MEMO - BEGIN --> 
+                        <!-- CREATE MEMO - END -->
+                        <!-- UPDATE MEMO - BEGIN -->
                         <div id="medit" class="fedit" style="display: none;">
                             <div class="closebutton cursorpoint"><i class="fa fa-times-circle fa-3x redtext"></i></div>
                             <div id="medit_title" class="robotobold cattext dbluetext">Update Memo</div>
                             <input type="hidden" id="json_data" name="json_data" />
                             <table class="tdataform2 rightmargin margintop10 vsmalltext" width="100%" border="0" cellpadding="0" cellspacing="0">
                                 <form name="edit_memo" id="edit_memo" method="POST" action="?ignore-page-cache=true" enctype="multipart/form-data">
-                                
+
                                 <tr>
                                     <td>Memo Title</td>
                                     <td>
@@ -64,9 +64,9 @@
                                     <td>
                                         <input type="file" name="memo_attach" id="umemo_attach" class="txtbox width200" /><br /><i>* must be image (jpg, jpeg, gif, png) or PDF and less than or equal 10Mb</i>
                                     </td>
-                                    
+
                                 </tr>
-                            
+
                                 <tr>
                                     <td colspan="2" align="center">
                                         <input type="submit" name="btnupdatememo" value="Update" class="btn btnupdatememo" />
@@ -79,27 +79,27 @@
                                     </td>
                                 </tr>
                                 </form>
-                            </table> 
+                            </table>
                         </div>
-                        <!-- UPDATE MEMO - END -->                    
+                        <!-- UPDATE MEMO - END -->
                         <?php endif; ?>
                     </div>
-                    
-                    <div id="mainsplashtext" class="mainsplashtext lefttalign">  
+
+                    <div id="mainsplashtext" class="mainsplashtext lefttalign">
                         <div class="topsplashtext lefttalign robotobold cattext whitetext"><?php echo WELCOME; ?></div>
                         <div class="leftsplashtext lefttalign"><?php include(TEMP."/menu.php"); ?></div>
                         <div class="rightsplashtext lefttalign">
-                            <div id="mainmemo" class="mainbody lefttalign whitetext">  
-                                <b class="mediumtext lorangetext">MEMORANDUM</b><br><br>                                
-                                
+                            <div id="mainmemo" class="mainbody lefttalign whitetext">
+                                <b class="mediumtext lorangetext">MEMORANDUM</b><br><br>
+
                                 <table class="width100per">
                                     <tr>
-                                        <td><span class="fa fa-search"></span> Search: 
-                                            <input type="text" id="memofrom" name="memofrom" value="<?php echo $_SESSION['memofrom'] ? $_SESSION['memofrom'] : '08/01/2014'; ?>" placeholder="From.." class="smltxtbox width55 datepick" /> - 
-                                            <input type="text" id="memoto" name="memoto" value="<?php echo $_SESSION['memoto'] ? $_SESSION['memoto'] : date("m/d/Y"); ?>" placeholder="To..." class="smltxtbox width55 datepick" /> by 
+                                        <td><span class="fa fa-search"></span> Search:
+                                            <input type="text" id="memofrom" name="memofrom" value="<?php echo $_SESSION['memofrom'] ? $_SESSION['memofrom'] : '08/01/2014'; ?>" placeholder="From.." class="smltxtbox width55 datepick" /> -
+                                            <input type="text" id="memoto" name="memoto" value="<?php echo $_SESSION['memoto'] ? $_SESSION['memoto'] : date("m/d/Y"); ?>" placeholder="To..." class="smltxtbox width55 datepick" /> by
                                             <input type="text" id="searchmemo" name="searchmemo" value="<?php echo $_SESSION['searchmemo'] ? $_SESSION['searchmemo'] : ''; ?>" placeholder="memo title" class="smltxtbox searchmemo" />&nbsp;
                                             <input type="button" id="btnsearchmemo" name="btnsearchmemo" value="Search" class="smlbtn btnsearchmemo" />
-                                            <input type="button" id="btnsearchallmemo" name="btnsearchallmemo" value="View All" class="btnsearchallmemo smlbtn<?php if (!$_SESSION['searchmemo'] && !$_SESSION['memofrom'] && !$_SESSION['memoto']) : ?> invisible<?php endif; ?>" />                                            
+                                            <input type="button" id="btnsearchallmemo" name="btnsearchallmemo" value="View All" class="btnsearchallmemo smlbtn<?php if (!$_SESSION['searchmemo'] && !$_SESSION['memofrom'] && !$_SESSION['memoto']) : ?> invisible<?php endif; ?>" />
                                         </td>
                                         <td class="righttalign">
                                             <?php if ($profile_level == 7 || $profile_level == 10) : ?>
@@ -108,19 +108,19 @@
                                         </td>
                                     </tr>
                                 </table>
-                                
+
                                 <div id="memodata">
                                 <table border="0" cellspacing="0" class="tdata width100per margintop25">
                                     <?php if ($memo_data) : ?>
-                                    <?php foreach ($memo_data as $key => $value) : ?>                                    
+                                    <?php foreach ($memo_data as $key => $value) : ?>
                                     <tr class="trdata centertalign">
                                         <td width="30%"<?php if ($key == 0) : ?> class="topborder"<?php endif; ?>>
                                             <a href="<?php echo WEB; ?>/uploads/memo/<?php echo $value['announce_filename']; ?>" target="_blank">
                                                 <button class="smlbtn cursorpoint">View</button>
-                                            </a>        
+                                            </a>
                                         </td>
                                         <td width="70%" class="lefttalign<?php if ($key == 0) : ?> topborder<?php endif; ?>"><span class="bold"><?php echo $value['announce_title']; ?></span><br>Published: <?php echo date('F j, Y', $value['announce_date']); ?><?php if ($profile_level == 7 || $profile_level == 10) : ?><br><span class="btneditmemo cursorpoint" attribute="<?php echo $value['announce_id']; ?>">Edit</span> | <span class="btndelmemo cursorpoint" attribute="<?php echo $value['announce_id']; ?>">Delete</span><?php endif; ?></td>
-                                    </tr>   
+                                    </tr>
                                     <?php endforeach; ?>
                                     <?php if ($pages) : ?>
                                     <tr>
@@ -133,7 +133,7 @@
                                     </tr>
                                     <?php endif; ?>
                                 </table>
-                                <input type="hidden" id="memopage" name="memopage" value="<?php echo $page; ?>" />   
+                                <input type="hidden" id="memopage" name="memopage" value="<?php echo $page; ?>" />
                                 </div>
                             </div>
                         </div>
