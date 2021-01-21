16cffc0e147186487208561e6cbaea80c7e571f0
Spencer Hart <shart.global@mphi.com>
2021-01-21 15:25:38 +0800

    coe template cleaning

--- template/2015/coe_pdf.php
+++ template/2015/coe_pdf.php
@@ -295,10 +295,10 @@ if($coe[0]["type"] == "COEAPPROVEDLEAVE"){ // COE with Approved Leave
 
 						<div>
 							<pre>				<b>Basic Salary</b>		<b><?php if(true){echo number_format($emp_info[0]["MonthlyRate"], 2);}else{ echo "SAMPLE"; }; ?></b></pre>
-						<?php if($emp_info[0]["Allowance"] != 0 || true){ ?>
-								<pre>				<b>Allowance</b>		<b><u><?php echo number_format($emp_info[0]["Allowance"], 2); ?></u></b></pre>
-								<pre>				<b>Total</b>			<b><u><?php echo number_format($emp_info[0]["Allowance"] + $emp_info[0]["MonthlyRate"], 2); ?></u></b></pre>
-						<?php } ?>
+							<?php if($emp_info[0]["Allowance"] != 0 || true){ ?>
+									<pre>				<b>Allowance</b>		<b><u><?php echo number_format($emp_info[0]["Allowance"], 2); ?></u></b></pre>
+									<pre>				<b>Total</b>			<b><u><?php echo number_format($emp_info[0]["Allowance"] + $emp_info[0]["MonthlyRate"], 2); ?></u></b></pre>
+							<?php } ?>
 						</div>
 
 			<p style="padding-left: 50px; padding-right: 50px;">In addition to the above compensation package, <?php echo strtolower($emp_info[0]["Gender"]); ?> receives the mandatory
