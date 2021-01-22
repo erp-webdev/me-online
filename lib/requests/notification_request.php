fc33012ba66d4a12fa00e587d6d0b8dc895bf06c
Spencer Hart <shart.global@mphi.com>
2021-01-22 13:45:13 +0800

    coe upload update

--- lib/requests/notification_request.php
+++ lib/requests/notification_request.php
@@ -4966,7 +4966,7 @@
 						// data: "emp=" + emp + "&type=" + type + "&category=" + category + "&reason=" + reason + "&other=" + other +
 						// 	  "&leavefrom=" + leavefrom + "&leaveto=" + leaveto + "&leavereturn=" + leavereturn + "&correctionname=" +correction_name + "&" + tasks +
 						// 	  "&hpa_percentage=" + hpa_percentage + "&avail_no=" + avail_no + "&coe_company=" +coe_company + "&leave_file=" +leavefile,
-						data: form_data;
+						data: form_data,
 						type: "POST",
 						complete: function(){
 							$("#loading").hide();
