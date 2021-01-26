136ba81c1d7c33b2ef01df7544607cbe108348db
Spencer Hart <shart.global@mphi.com>
2021-01-26 17:27:19 +0800

    template update headers/footers

--- template/2015/coe_pdf.php
+++ template/2015/coe_pdf.php
@@ -24,6 +24,8 @@
 			<p><img style="width: <?php echo $_POST['send'] == 'true' ? '150px' : '200px'; ?>;" src="<?php echo IMG_WEB; ?>/ecinema_coe.png"/></p>
 		<?php } elseif ($coe[0]["company"] == 'ECOC01') {?>
 			<p><img style="width: <?php echo $_POST['send'] == 'true' ? '150px' : '200px'; ?>;" src="<?php echo IMG_WEB; ?>/ecoc_coe.png"/></p>
+		<?php } elseif ($coe[0]["company"] == 'ERA01') {?>
+			<p><img style="width: <?php echo $_POST['send'] == 'true' ? '150px' : '200px'; ?>;" src="<?php echo IMG_WEB; ?>/erex_coe.png"/></p>
 		<?php } ?>
 	</div>
 	&nbsp;
