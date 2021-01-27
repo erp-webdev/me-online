0c71add3f4db19e76ccca204e2e4b4f78ba41eb4
Spencer Hart <shart.global@mphi.com>
2021-01-27 16:50:24 +0800

    template update headers/footers

--- lib/requests/notification_request.php
+++ lib/requests/notification_request.php
@@ -4955,11 +4955,13 @@
 							return false;
 						}
 					}else if (type == 'COEAPPROVEDLEAVE') {
-						if(!coeleavefile){
+						if($("#coeleavefile")get(0).files.length === 0){
 							alert("Image attachment is required!");
 							return false;
 						}
 					}
+
+					return false;
 
 					form_data.append('file', leavefile);
 					form_data.append('emp', emp);
