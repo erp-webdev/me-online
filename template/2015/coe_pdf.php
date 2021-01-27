18baa18185265dcf072f9a05371a95f70a9032a9
Spencer Hart <shart.global@mphi.com>
2021-01-27 08:48:04 +0800

    template update headers/footers

--- template/2015/coe_pdf.php
+++ template/2015/coe_pdf.php
@@ -8,19 +8,22 @@
 		<?php } elseif ($coe[0]["company"] == 'LGMI01') {?>
 			<p><img style="width: <?php echo $_POST['send'] == 'true' ? '150px' : '200px'; ?>;" src="<?php echo IMG_WEB; ?>/lgmi_coe.png"/></p>
 		<?php } elseif ($coe[0]["company"] == 'SIRUS') {?>
-			<!-- No Address Yet -->
+			<!--  -->
 			<p><img style="width: <?php echo $_POST['send'] == 'true' ? '150px' : '200px'; ?>;" src="<?php echo IMG_WEB; ?>/sirus_coe.png"/></p>
 		<?php } elseif ($coe[0]["company"] == 'MEGA01') {?>
 			<p><img style="width: <?php echo $_POST['send'] == 'true' ? '150px' : '200px'; ?>;" src="<?php echo IMG_WEB; ?>/mega_coe.png"/></p>
 		<?php } elseif ($coe[0]["company"] == 'TOWN01') {?>
 			<p><img style="width: <?php echo $_POST['send'] == 'true' ? '200px' : '250px'; ?>;" src="<?php echo IMG_WEB; ?>/townsquare_coe.png"/></p>
 		<?php } elseif ($coe[0]["company"] == 'NCCAI') {?>
+			<!--  -->
 			<p><img style="width: <?php echo $_POST['send'] == 'true' ? '200px' : '250px'; ?>;" src="<?php echo IMG_WEB; ?>/nccai_coe.png"/></p>
 		<?php } elseif ($coe[0]["company"] == 'FCI01') {?>
+			<!--  -->
 			<p><img style="width: <?php echo $_POST['send'] == 'true' ? '100px' : '150px'; ?>;" src="<?php echo IMG_WEB; ?>/firstcentro_coe.png"/></p>
 		<?php } elseif ($coe[0]["company"] == 'CITYLINK01') {?>
 			<p><img style="width: <?php echo $_POST['send'] == 'true' ? '150px' : '200px'; ?>;" src="<?php echo IMG_WEB; ?>/citylink_coe.png"/></p>
 		<?php } elseif ($coe[0]["company"] == 'ECC02') {?>
+			<!--  -->
 			<p><img style="width: <?php echo $_POST['send'] == 'true' ? '150px' : '200px'; ?>;" src="<?php echo IMG_WEB; ?>/ecinema_coe.png"/></p>
 		<?php } elseif ($coe[0]["company"] == 'ECOC01') {?>
 			<p><img style="width: <?php echo $_POST['send'] == 'true' ? '150px' : '200px'; ?>;" src="<?php echo IMG_WEB; ?>/ecoc_coe.png"/></p>
@@ -29,9 +32,14 @@
 		<?php } elseif ($coe[0]["company"] == 'LUCK01') {?>
 			<p><img style="width: <?php echo $_POST['send'] == 'true' ? '150px' : '200px'; ?>;" src="<?php echo IMG_WEB; ?>/lctm_coe.png"/></p>
 		<?php } elseif ($coe[0]["company"] == 'LFI01') {?>
+			<!--  -->
 			<p><img style="width: <?php echo $_POST['send'] == 'true' ? '150px' : '200px'; ?>;" src="<?php echo IMG_WEB; ?>/lafuerza_coe.png"/></p>
 		<?php } elseif ($coe[0]["company"] == 'MLI01') {?>
 			<p><img style="width: <?php echo $_POST['send'] == 'true' ? '150px' : '200px'; ?>;" src="<?php echo IMG_WEB; ?>/mli_coe.png"/></p>
+		<?php } elseif ($coe[0]["company"] == 'MCTI') {?>
+			<p><img style="width: <?php echo $_POST['send'] == 'true' ? '150px' : '200px'; ?>;" src="<?php echo IMG_WEB; ?>/mcti_coe.png"/></p>
+		<?php } else {?>
+			<p><img style="width: <?php echo $_POST['send'] == 'true' ? '150px' : '200px'; ?>;" src="<?php echo IMG_WEB; ?>/blank_coe.png"/></p>
 		<?php } ?>
 	</div>
 	&nbsp;
