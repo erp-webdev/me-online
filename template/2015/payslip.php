5021919e99f6dd1ed21bcf8b92972d419ea274e8
Spencer Hart <shart.global@mphi.com>
2021-01-29 13:47:05 +0800

    payslip_data fix

--- template/2015/payslip.php
+++ template/2015/payslip.php
@@ -1252,7 +1252,7 @@
                                                     <td>SSS</td>
                                                     <td class="righttalign"><?php echo number_format($payslip_data[0]['SSSEE'], 2); ?></td>
                                                 </tr>
-												<?php if($payslip_period[0]['PRYear'] >= 2021) : ?>
+												<?php if($payslip_data[0]['PRYear'] >= 2021) : ?>
 												<tr class="trdata">
                                                     <td>Provident Fund</td>
                                                     <td class="righttalign"><?php echo number_format($payslip_data[0]['SSSEEP'], 2); ?></td>
