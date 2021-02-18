bec68473703e8763a217c91c75601a4b53926fd8
Kevs <kevinayag@users.noreply.github.com>
2021-02-18 13:43:10 +0800

    ITR uploading

--- template/2015/index.php
+++ template/2015/index.php
@@ -66,7 +66,8 @@
                                    
                                         <br>- Due Date: <b><?php echo date('F j, Y', strtotime($deadline)); ?></b>
                                         <br>- Fill-up item No. <b>6, 6A, 7, 52 & 54</b>
-                                        <br>- Save in .pdf file with name format  (ALL CAPS) <br> <b> SURNAME_9 DIGIT TIN NO.-0000_12312020</b>
+                                        <br>- Save in .pdf file with name format  (ALL CAPS) <br> <b> SURNAME_9DIGITTINNO.-0000_12312020
+                                            <br> e.g.: DELACRUZ_123-456-789-0000_12312020</b>
                                         <br>- Send the ITR to <b>
                                             <?php 
                                                 if(in_array($profile_dbname, ['MEGAWORLD', 'GLOBAL_HOTEL', 'LAFUERZA', 'MLI', 'TOWNSQUARE', 'SIRUS', 'NEWTOWN']))
