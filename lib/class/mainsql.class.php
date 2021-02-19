fa8d131721b0a300782e1e4b324caf11ed720efc
Kevs <kevinayag@users.noreply.github.com>
2021-02-19 13:21:42 +0800

    testing mdtr application

--- lib/class/mainsql.class.php
+++ lib/class/mainsql.class.php
@@ -2958,7 +2958,7 @@ class mainsql {
                 endforeach;
 
                 $add_md = $this->get_sp_data_status('SP_INSERT_APPLY_MD', $val);
-                echo var_dump($add_md);
+                echo var_dump($val);
 
                 if($add_md) {
                     return $add_md;
