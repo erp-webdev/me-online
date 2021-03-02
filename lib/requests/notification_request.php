41721c18b8e9dfc338f362e16a664f923c550e41
Spencer Hart <shart.global@mphi.com>
2021-03-02 16:33:29 +0800

    coe approval update

--- lib/requests/notification_request.php
+++ lib/requests/notification_request.php
@@ -5658,7 +5658,7 @@
 											<?php } ?>
 
 											<?php if($result[0]['status'] == 'On Process' || $result[0]['status'] == 'For Approval'){ ?>
-												<option id="statusopt" value="For Approval"<?php if($result[0]['status'] == 'For Approval'){ echo "selected"; }  ?>>For Approval - (E-signature for sending)</option>
+												<option id="statusopt" value="For Approval"<?php if($result[0]['status'] == 'For Approval'){ echo "selected"; }  ?>>For Approval - (for sending CoE)</option>
 											<?php } ?>
 
 											<?php if($result[0]['status'] == 'On Process' || $result[0]['status'] == 'For Release'){ ?>
