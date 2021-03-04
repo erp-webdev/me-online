ccdc9c11cb573a75068850373d6bbd3e13598678
Spencer Hart <shart.global@mphi.com>
2021-03-04 16:58:11 +0800

    coe approval update

--- lib/requests/notification_request.php
+++ lib/requests/notification_request.php
@@ -5702,7 +5702,7 @@
 								<label>E-sig. Approval: </label>
 							</td>
 							<td align="left">
-								<?php echo $result[0]['approved'] ? 'Approved' : 'No Approval'; ?>
+								<?php echo $result[0]['approved'] ? 'Approved' : 'No Approval Process'; ?>
 							</td>
 						</tr>
 					<?php } ?>
