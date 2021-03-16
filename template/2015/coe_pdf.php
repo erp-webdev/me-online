822af7b1c1cce766c626ae40ebbed1ce1134a0f0
Spencer Hart <hart.spencer.m@gmail.com>
2021-03-16 17:42:44 +0800

    Update coe_pdf.php

--- template/2015/coe_pdf.php
+++ template/2015/coe_pdf.php
@@ -434,7 +434,7 @@
 			}elseif ($coe[0]["company"] == 'GLOBAL01' || $coe[0]["company"] == 'LGMI01' || $coe[0]["company"] == 'MIB01') {
 			?>
 				<?php
-				if($send_pdf){
+				if(!$send_pdf){
 				?>
 					<img style="width: 150px" src="<?php echo IMG_WEB; ?>/coe/coe_gl.png"/>
 				<?php
