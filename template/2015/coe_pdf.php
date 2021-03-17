7ed7a2c025e104f563d7f8e26bc4dbdff59c2961
Spencer Hart <hart.spencer.m@gmail.com>
2021-03-17 08:27:24 +0800

    Update coe_pdf.php

--- template/2015/coe_pdf.php
+++ template/2015/coe_pdf.php
@@ -478,7 +478,7 @@
 		<!-- Breaklines START -  for Template - TCPDF cant render css height of div - FOOTER BREAKLINES -->
 		<?php if (in_array($coe[0]["type"], ['COE', 'COEHOUSINGPLAN', 'COEGOODMORAL', 'COEGOODMORAL', 'COESEPARATED', 'COECORRECTIONNAME', 'COEAPPROVEDLEAVE'])){ ?>
 			&nbsp;<br />&nbsp;<br />&nbsp;<br />&nbsp;<br />&nbsp;<br />
-			&nbsp;<br />&nbsp;<br />&nbsp;<br />&nbsp;<br />&nbsp;<br />
+			&nbsp;<br />&nbsp;<br />&nbsp;<br />
 			<?php if($coe[0]["type"] == 'COE' && !$send_pdf){ ?>
 				&nbsp;<br />&nbsp;<br />&nbsp;<br />
 				&nbsp;<br />&nbsp;<br />&nbsp;<br />
