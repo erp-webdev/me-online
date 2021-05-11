eb23723659ff9d733a88e40b4d10242fa53817fe
Spencer Hart <hart.spencer.m@gmail.com>
2021-05-11 08:27:22 +0800

    CoE for Approved Leave

--- template/2015/coe_pdf.php
+++ template/2015/coe_pdf.php
@@ -97,9 +97,9 @@
 				<?php } ?>
 
 				<p style="padding-left: 50px; padding-right: 50px;">This is to further certify that <?php echo $emp_info[0]["salutation"]." ".strtoupper(mb_convert_encoding($emp_info[0]["FullName"], 'UTF-8', 'HTML-ENTITIES')); ?> shall be on leave from <b><?php echo date('F d, Y', strtotime($start_date))." to ".date('F d, Y', strtotime($end_date)); ?>
-				</b>as approved by the Management. He is expected to report back for work on <b><?php echo date('F d, Y', strtotime($return_date)); ?></b>.</p>
+				</b>as approved by the Management. <?php echo $emp_info[0]["Gender"]; ?> is expected to report back for work on <b><?php echo date('F d, Y', strtotime($return_date)); ?></b>.</p>
 
-				<p style="padding-left: 50px; padding-right: 50px;">This certification is being issued upon the request of <?php $emp_info[0]["salutation"]." ".strtoupper(mb_convert_encoding($emp_info[0]["FullName"], 'UTF-8', 'HTML-ENTITIES')); ?>
+				<p style="padding-left: 50px; padding-right: 50px;">This certification is being issued upon the request of <?php echo $emp_info[0]["salutation"]." ".strtoupper(mb_convert_encoding($emp_info[0]["FullName"], 'UTF-8', 'HTML-ENTITIES')); ?>
 				<?php if($coe[0]["other_reason"]){ ?>
 					as a requirement for <?php echo $emp_info[0]["Gender2"]. " ".$coe[0]["other_reason"]; ?>.</p>
 				<?php }else{ ?>
