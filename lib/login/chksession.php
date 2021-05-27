ee24b1b95e55a9cff592bd15a4e7d02852acb984
Spencer Hart <hart.spencer.m@gmail.com>
2021-05-27 15:15:37 +0800

    test session

--- lib/login/chksession.php
+++ lib/login/chksession.php
@@ -1,12 +1,18 @@
 <?php
 
+	// server should keep session data for 1 hour
+	ini_set('session.gc_maxlifetime', 600);
+
+	// each client remember their session id for exactly 1 hour
+	session_set_cookie_params(600);
+
     session_start();
 
 	$cookiename = 'megassep_user';
 
-	$username = $_SESSION[$cookiename];	
-	
-	if ($username) {	        
+	$username = $_SESSION[$cookiename];
+
+	if ($username) {
         echo "1";
 	}
 	else
