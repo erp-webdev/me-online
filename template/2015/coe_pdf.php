537ad36afc6953313134a5adcb5d96f494f3d5df
Kevs <kevinayag@users.noreply.github.com>
2021-09-28 09:50:45 +0800

    Update coe_pdf.php

--- template/2015/coe_pdf.php
+++ template/2015/coe_pdf.php
@@ -542,20 +542,20 @@
 		</b><p style="font-size: <?php echo $send_pdf ? '8px' : '10px'; ?>; padding-left: 50px; padding-right: 50px;">Ref No.:<?php echo $coe[0]["ref_no"]; ?></p>
 
 		<?php
-		if ((in_array($coe[0]["company"], ['GLOBAL01', 'LGMI01'])) && $coe[0]["type"] != 'COECOMPENSATION') {
+		if ((in_array($coe[0]["company"], ['GLOBAL01', 'LGMI01'])) ) {
 		?>
 			&nbsp;<br />
 			<p style="font-size: <?php echo $send_pdf ? '8px' : '10px'; ?>; text-align: center;">Unit G, Ground Floor, 331 Building, 331 Sen. Gil Puyat Avenue, Barangay Bel-Air, Makati City 1200 • Tels (632) 5411979 / 8946345 <br />
 			<a href="www.globalcompanies.com.ph">www.globalcompanies.com.ph</a> • Email: <a href="globalonehr@globalcompanies.com.ph">globalonehr@globalcompanies.com.ph</a></p>
 		<?php
-		} elseif (($coe[0]["company"] == 'MEGA01') && $coe[0]["type"] != 'COECOMPENSATION') {
+		} elseif (($coe[0]["company"] == 'MEGA01')) {
 		?>
 			&nbsp;
 			<p style="font-size: <?php echo $send_pdf ? '8px' : '10px'; ?>; text-align: center;">25/F Alliance Global Tower, 36th Street corner 11th Avenue Uptown Bonifacio, Taguig City 1634 <br />
 			Trunkline: (632) 905-2900 • (632) 905-2800 <br />
 			www.megaworldcorp.com • Email: infodesk@megaworldcorp.com</p>
 		<?php
-		} elseif (($coe[0]["company"] == 'MCTI') && $coe[0]["type"] != 'COECOMPENSATION') {
+		} elseif (($coe[0]["company"] == 'MCTI') ) {
 		?>
 			&nbsp;<br />
 			<p style="font-size: <?php echo $send_pdf ? '8px' : '10px'; ?>; color: #005f2f; text-align: center;">Capitol Boulevard, Barangay Sto. Niño, City of San Fernando, Pampanga 2000 | Tels 045-963-1990<br />
