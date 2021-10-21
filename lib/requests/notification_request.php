277f41c5169b824ce9e1efe55b95c4eba5a51ddc
Kevs <kevinayag@users.noreply.github.com>
2021-10-21 09:50:13 +0800

    Update

--- lib/requests/notification_request.php
+++ lib/requests/notification_request.php
@@ -7198,14 +7198,12 @@
 								$rank_approver = 'VP';
 
 							$approver_email = $COEC_APPROVERS[$emp_info[0]['CompanyID']]['approver'][$rank_approver]['email']; 
-                            $message .= 'intent::' . $approver_email;
 							
-                            array_push($hr_emails, 'kayag.global@megaworldcorp.com');
-                            // array_push($hr_emails, $approver_email);
-                            // if($emp_info[0]['CompanyID'] != 'ASIAAPMI'){
-							//     array_push($hr_emails, 'abranco@megaworldcorp.com');
-							//     array_push($hr_emails, 'mguarina@megaworldcorp.com');
-                            // }
+                            array_push($hr_emails, $approver_email);
+                            if($emp_info[0]['CompanyID'] != 'ASIAAPMI'){
+							    array_push($hr_emails, 'abranco@megaworldcorp.com');
+							    array_push($hr_emails, 'mguarina@megaworldcorp.com');
+                            }
 							$sendmail = mail(implode(',', $hr_emails), "COE Request Update (Approvers Notification)", $message, $headers); 
 
 						}
