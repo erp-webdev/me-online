17446fe06b1b5976acc1dc162304018a21d8381e
Kevs <kevinayag@users.noreply.github.com>
2022-01-26 16:42:06 +0800

    update

--- lib/requests/leavebal_request.php
+++ lib/requests/leavebal_request.php
@@ -21,7 +21,9 @@
     $profile_location = $location;
     $profile_minothours = $minothours;
     $profile_dbname = $dbname;
-    $profile_compressed = $compressed;
+    // $profile_compressed = $compressed;
+    // treat all employee compressed so as to compute leave per hour instead of per day
+    $profile_compressed = 1; 
 
     if ($profile_dbname == "ECINEMA" || $profile_dbname == "EPARKVIEW" || $profile_dbname == "LAFUERZA" || $profile_dbname == "GLOBAL_HOTEL") :
         $adminarray = array("2011-03-V835");
