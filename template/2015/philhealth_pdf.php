5d8627c9bc5c7fa54d4f90d7127e022792b06032
John Joshua Bandang <jbandang.global@megawolrdcorp.com>
2022-02-14 15:13:15 +0800

    test

--- template/2015/philhealth_pdf.php
+++ template/2015/philhealth_pdf.php
@@ -88,11 +88,11 @@
 		place_text(date('Y', strtotime($emp_info[0]["BirthDate"])), 167.50, 81.80, 'letter-spacing: 7.3px;');
 
 		$ph_no = clean_str($emp_info[0]["CompPhilHealthNbr"]);
-		place_text(substr($ph_no, 0, 2), 63, 173.5, 'letter-spacing: 8px');
-		place_text(substr($ph_no, 2, 9), 73, 173.5, 'letter-spacing: 8px');
-		place_text(substr($ph_no, 11, 1),110, 173.5, 'letter-spacing:8px');
+		place_text(substr($ph_no, 0, 2), 63, 174, 'letter-spacing: 8px');
+		place_text(substr($ph_no, 2, 9), 73, 174, 'letter-spacing: 8px');
+		place_text(substr($ph_no, 11, 1),110, 174, 'letter-spacing:8px');
 
-		place_text($emp_info[0]["CompanyName"], 39, 175.3, '');
+		place_text($emp_info[0]["CompanyName"], 39, 178.3, '');
 		place_text($approver->name, 10, 198, '');
 		place_text($approver->position, 83, 198, '');
 
