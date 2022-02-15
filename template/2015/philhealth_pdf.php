a40088f9afca1d56d01ff8733bea50435e5d5955
John Joshua Bandang <jbandang.global@megawolrdcorp.com>
2022-02-15 08:23:31 +0800

    test

--- template/2015/philhealth_pdf.php
+++ template/2015/philhealth_pdf.php
@@ -220,9 +220,9 @@
 			place_text(substr($ph_no, 2, 9), 74, 248, 'letter-spacing: 7px');
 			place_text(substr($ph_no, 11, 1),112, 248, 'letter-spacing:8px');
 	
-			place_text($emp_info[0]["CompanyName"], 39, 178.1, '');
-			place_text($approver->name, 11.5, 202, '');
-			place_text($approver->position, 85.5, 202, '');
+			place_text($emp_info[0]["CompanyName"], 39, 278.1, '');
+			place_text($approver->name, 11.5, 302, '');
+			place_text($approver->position, 85.5, 302, '');
 	
 			?>
 		</body>
