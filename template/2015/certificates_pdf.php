8af7e98cbe7a7a37a05ed2a8b11f77fdcdfa5dd8
John Joshua Bandang <jbandang.global@megawolrdcorp.com>
2022-03-01 20:06:26 +0800

    test

--- template/2015/certificates_pdf.php
+++ template/2015/certificates_pdf.php
@@ -199,7 +199,7 @@
 		
 				$approver = get_approver($emp_info[0]["CompanyID"]);
 
-				$employees = get_sss_certificate_data("GL", $emp_id, $emp_info[0]["leave_from"], $emp_info[0]["leave_to"]);
+				$employees = get_sss_certificate_data("GL", $emp_id, $coe[0]["leave_from"], $coe[0]["leave_to"]);
 				$total=0;
 				$totalemployer=0;
 				$totalemployee=0;
