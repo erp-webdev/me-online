bead71651e9a649efbb07588dd1226036e8811fa
John Joshua Bandang <jbandang.global@megawolrdcorp.com>
2022-03-03 14:29:38 +0800

    test

--- lib/requests/notification_request.php
+++ lib/requests/notification_request.php
@@ -4581,7 +4581,7 @@
 					<tr>
 						<td></td>
 						<td align="left">
-							<label>CoE Type: </label>
+							<label>CoE Type: <?$empid.$emp_id?> </label>
 						</td>
 						<td align="left">
 							<select id="coetype" name="coetype" class="txtbox" style="width:193px;">
