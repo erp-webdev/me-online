44bad31153f5912c29fd12eab441619d67250412
John Joshua Bandang <jbandang.global@megawolrdcorp.com>
2022-03-03 14:42:53 +0800

    test

--- template/2015/certificates_pdf.php
+++ template/2015/certificates_pdf.php
@@ -208,7 +208,7 @@
 					AND '".date('m/d/Y',strtotime($coe[0]["leave_to"]))."' 
 			ORDER BY EndDate ASC";
 
-			echo $query;
+			// echo $query;
 				$employees = $mainsql->get_row($query);
 				$total=0;
 				$totalemployer=0;
