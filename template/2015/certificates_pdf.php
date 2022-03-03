32eb1d9d911ef3d07cee3deab6942e5e3936a62d
John Joshua Bandang <jbandang.global@megawolrdcorp.com>
2022-03-03 14:57:07 +0800

    test

--- template/2015/certificates_pdf.php
+++ template/2015/certificates_pdf.php
@@ -2,6 +2,47 @@
 	<div style="text-align: center; padding-top: 10px;">
 
 		<?php $send_pdf = $_POST['send'] == 'true' ? true : false; ?>
+		<?php if ($coe[0]["company"] == 'GLOBAL01') { ?>
+			<p><img style="width: <?php echo $send_pdf ? '150px' : '200px'; ?>;" src="<?php echo IMG_WEB; ?>/gl_coe.png"/></p>
+		<?php } elseif ($coe[0]["company"] == 'LGMI01') {?>
+			<p><img style="width: <?php echo $send_pdf ? '150px' : '200px'; ?>;" src="<?php echo IMG_WEB; ?>/lgmi_coe.png"/></p>
+		<?php } elseif ($coe[0]["company"] == 'SIRUS') {?>
+			<p><img style="width: <?php echo $send_pdf ? '150px' : '200px'; ?>;" src="<?php echo IMG_WEB; ?>/sirus_coe.png"/></p>
+		<?php } elseif ($coe[0]["company"] == 'MEGA01') {?>
+			<p><img style="width: <?php echo $send_pdf ? '150px' : '200px'; ?>;" src="<?php echo IMG_WEB; ?>/mega_coe.png"/></p>
+		<?php } elseif ($coe[0]["company"] == 'TOWN01') {?>
+			<p><img style="width: <?php echo $send_pdf ? '200px' : '250px'; ?>;" src="<?php echo IMG_WEB; ?>/townsquare_coe.png"/></p>
+		<?php } elseif ($coe[0]["company"] == 'NCCAI') {?>
+			<!--  -->
+			<p><img style="width: <?php echo $send_pdf ? '200px' : '250px'; ?>;" src="<?php echo IMG_WEB; ?>/nccai_coe.png"/></p>
+		<?php } elseif ($coe[0]["company"] == 'FCI01') {?>
+			<!-- NOT INCLUDED -->
+			<p><img style="width: <?php echo $send_pdf ? '100px' : '150px'; ?>;" src="<?php echo IMG_WEB; ?>/firstcentro_coe.png"/></p>
+		<?php } elseif ($coe[0]["company"] == 'CITYLINK01') {?>
+			<p><img style="width: <?php echo $send_pdf ? '150px' : '200px'; ?>;" src="<?php echo IMG_WEB; ?>/citylink_coe.png"/></p>
+		<?php } elseif ($coe[0]["company"] == 'ECC02') {?>
+			<!--  -->
+			<p><img style="width: <?php echo $send_pdf ? '150px' : '200px'; ?>;" src="<?php echo IMG_WEB; ?>/ecinema_coe.png"/></p>
+		<?php } elseif ($coe[0]["company"] == 'ECOC01') {?>
+			<p><img style="width: <?php echo $send_pdf ? '150px' : '200px'; ?>;" src="<?php echo IMG_WEB; ?>/ecoc_coe.png"/></p>
+		<?php } elseif ($coe[0]["company"] == 'ERA01') {?>
+			<p><img style="width: <?php echo $send_pdf ? '150px' : '200px'; ?>;" src="<?php echo IMG_WEB; ?>/erex_coe.png"/></p>
+		<?php } elseif ($coe[0]["company"] == 'LUCK01') {?>
+			<p><img style="width: <?php echo $send_pdf ? '150px' : '200px'; ?>;" src="<?php echo IMG_WEB; ?>/lctm_coe.png"/></p>
+		<?php } elseif ($coe[0]["company"] == 'LFI01') {?>
+			<!--  -->
+			<p><img style="width: <?php echo $send_pdf ? '150px' : '200px'; ?>;" src="<?php echo IMG_WEB; ?>/lafuerza_coe.png"/></p>
+		<?php } elseif ($coe[0]["company"] == 'MLI01') {?>
+			<p><img style="width: <?php echo $send_pdf ? '150px' : '200px'; ?>;" src="<?php echo IMG_WEB; ?>/mli_coe.png"/></p>
+		<?php } elseif ($coe[0]["company"] == 'MCTI') {?>
+			<p><img style="width: <?php echo $send_pdf ? '150px' : '200px'; ?>;" src="<?php echo IMG_WEB; ?>/mcti_coe.png"/></p>
+		<?php } elseif ($coe[0]["company"] == 'SUNT01') {?>
+			<p><img style="width: <?php echo $send_pdf ? '150px' : '200px'; ?>;" src="<?php echo IMG_WEB; ?>/suntrust_coe.png"/></p>
+		<?php } elseif ($coe[0]["company"] == 'ASIAAPMI') {?>
+			<p><img style="width: <?php echo $send_pdf ? '150px' : '200px'; ?>;" src="<?php echo IMG_WEB; ?>/asiaapmi_coe.png"/></p>
+		<?php } else {?>
+			<p><img style="width: <?php echo $send_pdf ? '150px' : '200px'; ?>;" src="<?php echo IMG_WEB; ?>/blank_coe.png"/></p>
+		<?php } ?>
 
 	<?php
 	//R&F to Supervisor
