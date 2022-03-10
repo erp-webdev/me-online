a0036e689ba0d624b8413c4f9d8a54df17e7c0ea
John Joshua Bandang <jbandang.global@megawolrdcorp.com>
2022-03-10 15:59:56 +0800

    test

--- lib/requests/dtrman_request.php
+++ lib/requests/dtrman_request.php
@@ -158,6 +158,7 @@
             if ($isapprover > 0)
             {
                 echo "this worked";
+                echo $profile_idnum;
                 if (strlen($searchdtrm) >= 3) :
                     $dtrman_data = $mainsql->get_employee1($start, REQ_NUM_ROWS, $searchdtrm, 0);
                     $dtrman_count = $mainsql->get_employee1(0, 0, $searchdtrm, 1);
