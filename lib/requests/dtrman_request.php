db135b2b43ebfc23d0d3c02718cb1b3ec28849c7
John Joshua Bandang <jbandang.global@megawolrdcorp.com>
2022-03-24 17:53:38 +0800

    DTR viewing by Approver

--- lib/requests/dtrman_request.php
+++ lib/requests/dtrman_request.php
@@ -86,8 +86,6 @@
     $isapprover = $mainsql->get_row($sql);
     $isapprover = $isapprover[0]['approving'];
 
-	echo $isapprover;
-    echo $notadmin;
 	//***************** USER MANAGEMENT - END *****************\\
 
     $sec = $profile_id ? $_GET['sec'] : NULL;
