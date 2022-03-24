c2effaf763205ad0b6467802b3589c15071f2873
John Joshua Bandang <jbandang.global@megawolrdcorp.com>
2022-03-24 16:40:51 +0800

    DTR viewing by Approver

--- template/2015/menu.php
+++ template/2015/menu.php
@@ -104,7 +104,7 @@
                         <?php endif; ?>
                         
                         <?php if ($profile_level >= 9 || $isapprover > 0 || $profile_idnum == '2016-06-0457') : ?>
-                        <?php if ($section != "dtrman") { ?><a href="<?php echo WEB; ?>/dtrman" onclick="clickAndDisable(this);"><?php } ?><div<?php if ($section == "dtrman") { ?> class="dselected"<?php } ?>>System &amp; DTR Management<?php echo $isapprover?> </div><?php if ($section != "dtrman") { ?></a><?php } ?>
+                        <?php if ($section != "dtrman") { ?><a href="<?php echo WEB; ?>/dtrman" onclick="clickAndDisable(this);"><?php } ?><div<?php if ($section == "dtrman") { ?> class="dselected"<?php } ?>>System &amp; DTR Management<?php echo $notadmin?> </div><?php if ($section != "dtrman") { ?></a><?php } ?>
                         <?php endif; ?>
 
                         <?php if ($profile_ps) : ?>
