14f4f71403ba32f7dd4feff90e8d7a3cadfc9545
John Joshua Bandang <jbandang.global@megawolrdcorp.com>
2022-03-28 20:11:39 +0800

    CoE Others, Blank on Email

--- lib/requests/notification_request.php
+++ lib/requests/notification_request.php
@@ -5475,7 +5475,7 @@
 				$message = "<div style='display: block; border: 5px solid #024485; padding: 10px; font-size: 12px; font-family: Verdana; width: 95%;'><span style='font-size: 18px; color: #024485; font-weight: bold;'>New Certificate of Employment Request</span><br><br>Hi ".$emp_info[0]['NickName'].",<br><br>";
 				if($coeemp == $profile_idnum){
 					if($coetype == 'COE'){
-                        if($category == 'Others'){
+                        if($category == 'OTHERS'){
                             $message .= "You have successfully submitted a Certificate of Employment (".$coeothers.") with a Reference No: ".$refno.".";
                         }else{
                             $message .= "You have successfully submitted a Certificate of Employment (".$coes2["$coecategory"].") with a Reference No: ".$refno.".";
