d33240718c6b75936737c37f5dc1833ee80fbb9e
Kevs <keven.ayag@adamson.edu.ph>
2022-03-29 07:50:54 +0800

    Revert "Sending CoE before Resignation"
    This reverts commit 51f4986949cb22720d5e8a049489f5eaafe63cb6.

--- lib/requests/notification_request.php
+++ lib/requests/notification_request.php
@@ -6087,7 +6087,10 @@
 						if( new Date(date_resigned) < new Date() && type != 'COECOMPENSATION'){
 							alert('You can send the COE on or after the resignation effectivity date. (Resignation Date: '+date_resigned+')');
 							return;
-						}
+						} else
+                        {
+                            alert('Working');
+                        }
 
 						if(type == 'COEAPPROVEDLEAVE'){
 							var start_date = "<?php echo date('m/d/Y', strtotime($result[0]['leave_from'])); ?>";
