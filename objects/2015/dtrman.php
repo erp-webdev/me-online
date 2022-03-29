b3895cd5119e8061506e8b6b7d0f3dce0b7dabe5
Kevs <keven.ayag@adamson.edu.ph>
2022-03-29 16:38:42 +0800

    update

--- objects/2015/dtrman.php
+++ objects/2015/dtrman.php
@@ -31,6 +31,7 @@
                 $_POST['searchdtrm'] = NULL;
             }                              
 
+            if($profile_level){
                 if (strlen($searchdtrm) >= 3) :
                     $dtrman_data = $mainsql->get_employee($start, REQ_NUM_ROWS, $searchdtrm, 0);
                     $dtrman_count = $mainsql->get_employee(0, 0, $searchdtrm, 1);
@@ -40,6 +41,7 @@
                     $dtrman_count = NULL;
                     $pages = NULL;
                 endif;
+            }
         
         else :
         
