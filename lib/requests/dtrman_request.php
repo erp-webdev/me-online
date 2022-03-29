af124f6d820acb8852eda071e9aefdaa038228fa
Kevs <keven.ayag@adamson.edu.ph>
2022-03-29 16:54:18 +0800

    update

--- lib/requests/dtrman_request.php
+++ lib/requests/dtrman_request.php
@@ -151,7 +151,7 @@
                     $dtrman_count = $mainsql->get_employee(0, 0, $searchdtrm, 1);
                     $pages = $mainsql->pagination("dtrman", $dtrman_count, REQ_NUM_ROWS, 9);     
                 else: 
-                    echo count($approver_employees); exit;
+                    $approver_employees = $mainsql->get_approver_employee($start, REQ_NUM_ROWS, null, 0, $profile_idnum, $profile_dbname);
                     if(count($approver_employees)):
                         $dtrman_data = $mainsql->get_approver_employee($start, REQ_NUM_ROWS, $searchdtrm, 0, $profile_idnum, $profile_dbname);
                         $dtrman_count = $mainsql->get_approver_employee(0, 0, $searchdtrm, 1, $profile_idnum, $profile_dbname);
--- objects/2015/dtrman.php
+++ objects/2015/dtrman.php
@@ -38,7 +38,6 @@
                     $pages = $mainsql->pagination("dtrman", $dtrman_count, REQ_NUM_ROWS, 9);          
                 else :
                     // allow viewing of dtr if approver of the employee
-                    $approver_employees = $mainsql->get_approver_employee($start, REQ_NUM_ROWS, null, 0, $profile_idnum, $profile_dbname);
                     if(count($approver_employees)) :
                         $dtrman_data = $mainsql->get_approver_employee($start, REQ_NUM_ROWS, $searchdtrm, 0, $profile_idnum, $profile_dbname);
                         $dtrman_count = $mainsql->get_approver_employee(0, 0, $searchdtrm, 1, $profile_idnum, $profile_dbname);
