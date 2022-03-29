e319dc0fe2a148a86b57058bfb99280a35f020dd
Kevs <keven.ayag@adamson.edu.ph>
2022-03-29 16:56:42 +0800

    update

--- lib/requests/dtrman_request.php
+++ lib/requests/dtrman_request.php
@@ -22,15 +22,15 @@
     $profile_minothours = $minothours;
     $profile_dbname = $dbname;
 
-    // if ($profile_dbname == "ECINEMA" || $profile_dbname == "EPARKVIEW" || $profile_dbname == "LAFUERZA" || $profile_dbname == "GLOBAL_HOTEL") :
-    //     $adminarray = array("2011-03-V835");
-    // elseif ($profile_dbname == "CITYLINK" || $profile_dbname == "ECOC" || $profile_dbname == "EREX" || $profile_dbname == "FIRSTCENTRO" || $profile_dbname == "LCTM" || $profile_dbname == "MLI" || $profile_dbname == "NCCAI" || $profile_dbname == "SUNTRUST" || $profile_dbname == "TOWNSQUARE") :
-    //     $adminarray = array("2009-10-V255");        
-    // elseif ($profile_dbname == "GL") :
-    //     $adminarray = array("2014-10-0004", "2014-10-0568", "2016-03-0261", "2017-01-0792"); 
-    // else :
-    //     $adminarray = array("2014-05-N791", "2009-09-V206", "2012-04-U384", "MASTER", "2012-03-U273", "2014-01-N506");
-    // endif;
+    if ($profile_dbname == "ECINEMA" || $profile_dbname == "EPARKVIEW" || $profile_dbname == "LAFUERZA" || $profile_dbname == "GLOBAL_HOTEL") :
+        $adminarray = array("2011-03-V835");
+    elseif ($profile_dbname == "CITYLINK" || $profile_dbname == "ECOC" || $profile_dbname == "EREX" || $profile_dbname == "FIRSTCENTRO" || $profile_dbname == "LCTM" || $profile_dbname == "MLI" || $profile_dbname == "NCCAI" || $profile_dbname == "SUNTRUST" || $profile_dbname == "TOWNSQUARE") :
+        $adminarray = array("2009-10-V255");        
+    elseif ($profile_dbname == "GL") :
+        $adminarray = array("2014-10-0004", "2014-10-0568", "2016-03-0261", "2017-01-0792", '2016-06-0457'); 
+    else :
+        $adminarray = array("2014-05-N791", "2009-09-V206", "2012-04-U384", "MASTER", "2012-03-U273", "2014-01-N506");
+    endif;
 
     /* MAIN DB CONNECTOR - START */
 
