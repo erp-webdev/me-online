8af320a0a67919267a0b74ae76f7ee9a77c5bbbc
Kevs <keven.ayag@adamson.edu.ph>
2022-03-29 16:36:59 +0800

    update

--- objects/2015/dtrman.php
+++ objects/2015/dtrman.php
@@ -45,7 +45,7 @@
             }elseif(count($approver_employees) > 0){
                 // enable dtr searching if approver of the employee being searched
                 if (strlen($searchdtrm) >= 3) :
-                    $dtrman_data = $mainsql->get_approver_employee($start, REQ_NUM_ROWS, $searchdtrm, 0, $profile_idnum, $profile_dbname);
+                    $dtrman_data = $mainsql->get_employee($start, REQ_NUM_ROWS, $searchdtrm, 0);
                     $dtrman_count = $mainsql->get_employee(0, 0, $searchdtrm, 1);
                     $pages = $mainsql->pagination("dtrman", $dtrman_count, REQ_NUM_ROWS, 9);            
                 else :
