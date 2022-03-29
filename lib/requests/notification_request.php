02ea41c39c22628be4d5d42d16a0c0199b0ebcd9
Kevs <keven.ayag@adamson.edu.ph>
2022-03-29 11:27:26 +0800

    test

--- lib/requests/notification_request.php
+++ lib/requests/notification_request.php
@@ -6253,19 +6253,16 @@
 						WHERE
 							A.EmpID = '$emp_id'";
 
+            // if date of resignation is not yet in-effect, employee must be issued working up to present.
+            if(!empty($emp_info[0]['DateResigned']) && (date('Y-m-d', strtotime($emp_info[0]['DateResigned'])) > date('Y-m-d') )){
+                $emp_info[0]['DateResigned'] = null;
+            }
 
 			$emp_info = $mainsql->get_row($query);
 			$emp_info[0]['HireDate'] = $emp_info[0]['HireDate'] ?  date('F j, Y', strtotime($emp_info[0]['HireDate'])) : null;
 			$emp_info[0]['CurrentDate'] = $emp_info[0]['CurrentDate'] ?  date('F j, Y', strtotime($emp_info[0]['CurrentDate'])) : null;
 		    $DateResigned2 = $emp_info[0]['DateResigned'] ?  date('Y-m-d', strtotime($emp_info[0]['DateResigned'])) : null;
 			$emp_info[0]['DateResigned'] = $emp_info[0]['DateResigned'] ?  date('F j, Y', strtotime($emp_info[0]['DateResigned'])) : null;
-
-            // if date of resignation is not yet in-effect, employee must be issued working up to present.
-            // if($emp_info[0]['DateResigned'] > date('Y-m-d') || $DateResigned2 > date('Y-m-d')){
-            //     $emp_info[0]['DateResigned'] = null;
-            //     $DateResigned2 = null;
-            // }
-
 
             if ($coe[0]["type"] == 'PHILHEALTHCSF' || $coe[0]["type"] == 'PHILHEALTHCF1'){
 			    include(TEMP.'/philhealth_pdf.php');
