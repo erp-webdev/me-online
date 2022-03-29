93aed7719a5d580f63e9a30fdd05ceba070662f7
Kevs <keven.ayag@adamson.edu.ph>
2022-03-29 09:15:39 +0800

    remove google analytics

--- template/2015/header.php
+++ template/2015/header.php
@@ -10,16 +10,6 @@
         <meta name="description" content="">
         <meta name="keywords" content="">
         
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-159577526-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-159577526-1');
-</script>
-	
         <!-- FAVICON -->
         
         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
