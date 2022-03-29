adf3b5262ce7f076fd63f50568d3301440c32805
Kevs <keven.ayag@adamson.edu.ph>
2022-03-29 16:51:23 +0800

    update

--- lib/requests/dtrman_request.php
+++ lib/requests/dtrman_request.php
@@ -150,7 +150,10 @@
                     $dtrman_data = $mainsql->get_employee($start, REQ_NUM_ROWS, $searchdtrm, 0);
                     $dtrman_count = $mainsql->get_employee(0, 0, $searchdtrm, 1);
                     $pages = $mainsql->pagination("dtrman", $dtrman_count, REQ_NUM_ROWS, 9);     
-                
+                else: 
+                    $dtrman_data = $mainsql->get_employee($start, REQ_NUM_ROWS, $searchdtrm, 0);
+                    $dtrman_count = $mainsql->get_employee(0, 0, $searchdtrm, 1);
+                    $pages = $mainsql->pagination("dtrman", $dtrman_count, REQ_NUM_ROWS, 9); 
                 endif;       
             else :
                 $dtrman_data = NULL;
