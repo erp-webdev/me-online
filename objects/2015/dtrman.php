8eba5141b76e4644d3cec4aa04768ea30072a88e
Kevs <keven.ayag@adamson.edu.ph>
2022-03-29 16:39:43 +0800

    update

--- objects/2015/dtrman.php
+++ objects/2015/dtrman.php
@@ -31,15 +31,15 @@
                 $_POST['searchdtrm'] = NULL;
             }                              
 
-                if (strlen($searchdtrm) >= 3) :
-                    $dtrman_data = $mainsql->get_employee($start, REQ_NUM_ROWS, $searchdtrm, 0);
-                    $dtrman_count = $mainsql->get_employee(0, 0, $searchdtrm, 1);
-                    $pages = $mainsql->pagination("dtrman", $dtrman_count, REQ_NUM_ROWS, 9);            
-                else :
-                    $dtrman_data = NULL;
-                    $dtrman_count = NULL;
-                    $pages = NULL;
-                endif;
+            if (strlen($searchdtrm) >= 3) :
+                $dtrman_data = $mainsql->get_employee($start, REQ_NUM_ROWS, $searchdtrm, 0);
+                $dtrman_count = $mainsql->get_employee(0, 0, $searchdtrm, 1);
+                $pages = $mainsql->pagination("dtrman", $dtrman_count, REQ_NUM_ROWS, 9);            
+            else :
+                $dtrman_data = NULL;
+                $dtrman_count = NULL;
+                $pages = NULL;
+            endif;
         
         else :
         
