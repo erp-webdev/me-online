8b523351355428e7fed8905e1c5c45603a7e1199
Kevs <keven.ayag@adamson.edu.ph>
2022-03-30 08:23:05 +0800

    update

--- template/2015/memo.php
+++ template/2015/memo.php
@@ -5,8 +5,7 @@
                     <div id="floatdiv" class="floatdiv invisible">
                         <?php if ($profile_level == 7 || $profile_level == 9 || $profile_level == 10) : ?>
                         <!-- CREATE MEMO - BEGIN -->
-                        --kevs <?php var_dump($profile_level); ?>
-                        <div id="madd" class="fadd" style="display: none;">
+                        <div id="madd" class="fadd" style="">
                             <div class="closebutton cursorpoint"><i class="fa fa-times-circle fa-3x redtext"></i></div>
                             <div id="madd_title" class="robotobold cattext dbluetext">Create Memo</div>
                             <table class="tdataform2 rightmargin margintop10 vsmalltext" width="100%" border="0" cellpadding="0" cellspacing="0">
