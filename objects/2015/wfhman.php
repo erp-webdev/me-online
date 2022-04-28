2c6880459a200e522f3105477808d86e472d491b
Kevs <keven.ayag@adamson.edu.ph>
2022-04-28 14:26:20 +0800

    WFH

--- objects/2015/wfhman.php
+++ objects/2015/wfhman.php
@@ -1,150 +1,51 @@
 <?php
+	
 	if ($logged == 1) {
-        if ($rwh_app || true) :
+        
+        if ($profile_ps) :
+
             # PAGINATION
             $page = isset($_GET["page"]) ? (int)$_GET["page"] : 1 ;
-            $start = NUM_ROWS * ($page - 1);
+            $start = APPR_NUM_ROWS * ($page - 1);
 
             //*********************** MAIN CODE START **********************\\
 
             # ASSIGNED VALUE
-            $page_title = "WFH Management";
+            $page_title = "WFH Management";	
 
             //***********************  MAIN CODE END  **********************\\
 
             global $sroot, $profile_id, $unix3month;
 
-            // REGISTER wfh
-            if ($_POST['btnwfhapply'] || $_POST['btnwfhapply_x']) :
+            $searchpsman_sess = $_SESSION['searchpsman'];
+            if ($_POST) {        
+                $searchpsman = $_POST['searchpsman'] ? $_POST['searchpsman'] : NULL;            
+                $_SESSION['searchpsman'] = $searchpsman;
+            }
+            elseif ($searchpsman_sess) {
+                $searchpsman = $searchpsman_sess ? $searchpsman_sess : NULL;
+                $_POST['searchpsman'] = $searchpsman != 0 ? $searchpsman : NULL;
+            }
+            else {
+                $searchpsman = NULL;
+                $_POST['searchpsman'] = NULL;
+            }                              
 
-                //CHECK FOR INVALID DATE/TIME IN AND OUT
+            $psman_data = $tblsql->get_employee_with_inactive($start, APPR_NUM_ROWS, $searchpsman, 0, $profile_dbname);
+            $psman_count = $tblsql->get_employee_with_inactive(0, 0, $searchpsman, 1, $profile_dbname);
 
-                $invalidcnt = 0;
-
-                //CHECK DATE IF ITS APPLIED - START
-
-				
-				if($wfherror){
-					echo '{"success": false, "error": "All inputs on Work from Home must be completed (Total Worked Hours & Acitivities). Unless the date is excluded."}';
-					exit();
-				}
-
-
-                $wfhpost['EMPID'] = $_POST['empid'];
-                $wfhpost['REQNBR'] = $add_wfhitem;
-                $wfhpost['TRANS'] = "APPLY";
-				$wfhpost['DATESTART'] = $_POST['wfh_from'];
-                $wfhpost['DATEEND'] = $_POST['wfh_to'];
-                $wfhpost['APPROVER01'] = $_POST['approver1'];
-                $wfhpost['APPROVER02'] = $_POST['approver2'];
-                $wfhpost['APPROVER03'] = $_POST['approver3'];
-                $wfhpost['APPROVER04'] = $_POST['approver4'];
-                $wfhpost['APPROVER05'] = $_POST['approver5'];
-                $wfhpost['APPROVER06'] = $_POST['approver6'];
-                $wfhpost['DBAPPROVER01'] = $_POST['dbapprover1'];
-                $wfhpost['DBAPPROVER02'] = $_POST['dbapprover2'];
-                $wfhpost['DBAPPROVER03'] = $_POST['dbapprover3'];
-                $wfhpost['DBAPPROVER04'] = $_POST['dbapprover4'];
-                $wfhpost['DBAPPROVER05'] = $_POST['dbapprover5'];
-                $wfhpost['DBAPPROVER06'] = $_POST['dbapprover6'];
-                $wfhpost['USER'] = $_POST['user'];
-                $wfhpost['REMARKS'] = "";
-
-
-                $add_wfh = $mainsql->wh_action($wfhpost, 'add');
-
-
-                if($add_wfh) :
-
-                    $requestor = $register->get_member($_POST['empid']);
-                    $request_info = $tblsql->get_mrequest(7, 0, 0, 0, $add_wfh, 0, NULL, NULL, NULL, NULL);
-                    $approver = $logsql->get_allmember($_POST['approver1'], $_POST['dbapprover1']);
-
-                    $reqemailblock = $mainsql->get_newemailblock($_POST['empid']);
-                    $appemailblock = $mainsql->get_newemailblock($_POST['approver1']);
-
-                    if ($reqemailblock) :
-
-                        //SEND EMAIL (REQUESTOR)
-
-                        $message = "<div style='display: block; border: 5px solid #024485; padding: 10px; font-size: 12px; font-family: Verdana; width: 100%;'><span style='font-size: 18px; color: #024485; font-weight: bold;'>New Work from Home Request</span><br><br>Hi ".$requestor[0]['NickName'].",<br><br>";
-                        $message .= "You opened a new request for Work from Home with Reference No: ".$add_wfh." on ".date('F j, Y')." and it's subject for approval. ";
-                        $message .= "<br><br>Thanks,<br>";
-                        $message .= SITENAME." Admin";
-						$message .= "<br>Click<a href='https://portal.megaworldcorp.com/me/login'> here</a> to login";
-						$message .= "<hr />".MAILFOOT."</div>";
-
-                        $headers = "From: ".NOTIFICATION_EMAIL."\r\n";
-                        $headers .= "Reply-To: ".NOTIFICATION_EMAIL."\r\n";
-                        $headers .= "MIME-Version: 1.0\r\n";
-                        $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-
-                        $sendmail = mail($requestor[0]['EmailAdd'], "New Work from Home Request", $message, $headers);
-
-                    endif;
-
-                    if ($appemailblock) :
-
-                        //SEND EMAIL (APPROVER)
-
-                        $message = "<div style='display: block; border: 5px solid #024485; padding: 10px; font-size: 12px; font-family: Verdana; width: 100%;'><span style='font-size: 18px; color: #024485; font-weight: bold;'>New Work from Home Request from ".$requestor[0]['FName']." ".$requestor[0]['LName']."</span><br><br>Hi ".$approver[0]['NickName'].",<br><br>";
-                        $message .= "New request ".$requestor[0]['FName']." ".$requestor[0]['LName']." for Work from Home with Reference No: ".$add_wfh." on ".date('F j, Y')." for your approval. ";
-                        $message .= "<br><br>Thanks,<br>";
-                        $message .= SITENAME." Admin";
-			$message .= "<br>Click<a href='https://portal.megaworldcorp.com/me/login'> here</a> to login";
-                        $message .= "<hr />".MAILFOOT."</div>";
-
-                        $headers = "From: ".NOTIFICATION_EMAIL."\r\n";
-                        $headers .= "Reply-To: ".NOTIFICATION_EMAIL."\r\n";
-                        $headers .= "MIME-Version: 1.0\r\n";
-                        $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-
-                        $sendmail = mail($approver[0]['EmailAdd'], "New Work from Home Request for your Approval", $message, $headers);
-
-                    endif;
-
-                    //READ STATUS
-                    if ($_POST['approver1'] && $add_wfh) :
-                        $insert_read = $mainsql->insert_read($_POST['approver1'], $add_wfh);
-                    endif;
-
-                    //AUDIT TRAIL
-                    $post['EMPID'] = $profile_idnum;
-                    $post['TASKS'] = "CREATE_WORK_FROM_HOME";
-                    $post['DATA'] = $add_wfh;
-                    $post['DATE'] = date("m/d/Y H:i:s.000");
-
-                    $log = $mainsql->log_action($post, 'add');
-
-                    if ($err_item) :
-                        echo '{"success": false, "error": "One or more of Work from Home item hasn\'t been process due to a network problem. Please forward email notification with its request ID."}';
-                        exit();
-                    else :
-                        echo '{"success": true}';
-                        exit();
-                    endif;
-                else :
-                    echo '{"success": false, "error": "There was a problem on Work from Home application"}';
-                    exit();
-                endif;
-
-            endif;
-
-            
-
+            $pages = $mainsql->pagination("pslipman", $psman_count, APPR_NUM_ROWS, 9);
+        
         else :
-            echo "<script language='javascript' type='text/javascript'>window.location.href='".WEB."'</script>";
+        
+            echo "<script language='javascript' type='text/javascript'>window.location.href='".WEB."/login'</script>";    
+        
         endif;
 
 	}
 	else
 	{
-		if ($_POST['btnwfhapply'] || $_POST['btnwfhapply_x']){
-			echo '{"success": false, "error": "Your session has expired! Kindly logout and login again to continue."}';
-			exit();
-		}
 		echo "<script language='javascript' type='text/javascript'>window.location.href='".WEB."/login'</script>";
-
 	}
-
+	
 ?>
---
+++ template/2015/wfhman.php
@@ -0,0 +1,62 @@
+<?php include(TEMP."/header.php"); ?>
+
+<!-- BODY -->
+
+                
+                
+                <div id="mainsplashtext" class="mainsplashtext lefttalign">  
+                    <div class="topsplashtext lefttalign robotobold cattext whitetext"><?php echo WELCOME; ?></div>
+                    <div class="leftsplashtext lefttalign"><?php include(TEMP."/menu.php"); ?></div>
+                    <div class="rightsplashtext lefttalign">
+                        <div id="mainapprovers" class="mainbody lefttalign whitetext">  
+                            <b class="mediumtext lorangetext">WFH MANAGEMENT</b><br><br>                                
+                            
+                            <table class="width100per">
+                                <tr>
+                                    <td><span class="fa fa-search"></span> Search: 
+                                        <input type="text" id="searchpsman" name="searchpsman" value="<?php echo $_SESSION['searchpsman'] ? $_SESSION['searchpsman'] : ''; ?>" placeholder="by <?php echo $profile_nadd; ?> ID, lastname or firstname" class="smltxtbox width250" />&nbsp;
+                                        <input type="button" id="btnpsman" name="btnpsman" value="Search" class="smlbtn" />
+                                        <input type="button" id="btnpsmanall" name="btnpsmanall" value="View All" class="smlbtn<?php if (!$_SESSION['searchpsman']) : ?> invisible<?php endif; ?>" />                                            
+                                    </td>
+                                    <td class="righttalign">
+                                        <!--input type="button" id="btnread" name="btnread" value="Mark as Read" class="smlbtn btnred" />
+                                        <input type="button" id="btnunread" name="btnunread" value="Mark as Unread" class="smlbtn btnred" /-->                            
+                                    </td>
+                                </tr>
+                            </table>
+                            
+                            <div id="psmandata">
+                            <table border="0" cellspacing="0" class="tdata width100per">
+                                <?php if ($psman_data) : ?>
+                                <tr>
+                                    <th width="20%"><?php echo ucfirst($profile_nadd); ?> ID</th>
+                                    <th width="30%">Last Name</th>
+                                    <th width="30%">First Name</th>
+                                    <th width="20%">Manage</th>
+                                </tr>
+                                <?php foreach ($psman_data as $key => $value) : ?>    
+                                <tr class="trdata centertalign whitetext">
+                                    <td><?php echo $value['EmpID']; ?></td>
+                                    <td><?php echo $value['LName']; ?></td>
+                                    <td><?php echo $value['FName']; ?></td>
+                                    <td><a href="<?php echo WEB.'/userps?id='.md5($value['EmpID']); ?>" class="lorangetext">View Payslip</a></td>
+                                </tr>
+                                <?php endforeach; ?>
+                                <?php if ($pages) : ?>
+                                <tr>
+                                    <td colspan="4" class="centertalign"><?php echo $pages; ?></td>
+                                </tr>
+                                <?php endif; ?>
+                                <?php else : ?>
+                                <tr>
+                                    <td class="bold centertalign noborder"><br><br>You have no employees listed</td>
+                                </tr>
+                                <?php endif; ?>
+                            </table>
+                            <input type="hidden" id="psmanpage" name="psmanpage" value="<?php echo $page; ?>" />      
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+<?php include(TEMP."/footer.php"); ?>
