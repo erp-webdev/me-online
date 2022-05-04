ed4a471a6b0922260ae4a82155bb3b14e5e2d103
Kevs <keven.ayag@adamson.edu.ph>
2022-05-04 09:34:10 +0800

    update

--- template/2015/accessman.php
+++ template/2015/accessman.php
@@ -95,8 +95,8 @@
         , newDiv = $('<div>', { height: width, width: height })
         , newInnerDiv = $('<div>', { text: newText, 'class': 'rotated' });
 
-        newInnerDiv.css('-webkit-transform-origin', (50) + 'px ' + (50) + 'px');
-        newInnerDiv.css('-moz-transform-origin', (50) + 'px ' + (50) + 'px');
+        // newInnerDiv.css('-webkit-transform-origin', (50) + 'px ' + (50) + 'px');
+        // newInnerDiv.css('-moz-transform-origin', (50) + 'px ' + (50) + 'px');
         newDiv.append(newInnerDiv);
 
         betterCells.push(newDiv);
