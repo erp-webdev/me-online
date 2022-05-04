9a4a897e62fec7ca8fab0a55ae254b9ea1eb5837
Kevs <keven.ayag@adamson.edu.ph>
2022-05-04 11:34:59 +0800

    update

--- template/2015/accessman.php
+++ template/2015/accessman.php
@@ -56,15 +56,15 @@
                     <tr class="cursorpoint trdata centertalign whitetext" attribute="<?php echo md5($value['EmpID']); ?>">
                         <td><?php echo $value['EmpID']; ?></td>
                         <td><?php echo $value['Fullname']; ?></td>
-                        <td style="text-align: left"><input type="checkbox"></td>
-                        <td style="text-align: left"><input type="checkbox"></td>
-                        <td style="text-align: left"><input type="checkbox"></td>
-                        <td style="text-align: left"><input type="checkbox"></td>
-                        <td style="text-align: left"><input type="checkbox"></td>
-                        <td style="text-align: left"><input type="checkbox"></td>
-                        <td style="text-align: left"><input type="checkbox"></td>
-                        <td style="text-align: left"><input type="checkbox"></td>
-                        <td style="text-align: left"><input type="checkbox"></td>
+                        <td style="text-align: left"><input type="checkbox" name="dtr" id="dtr" <?php if($value['Form'] == 'dtr') ? 'checked' : '' ?>></td>
+                        <td style="text-align: left"><input type="checkbox" name="payslip" id="payslip" <?php if($value['Form'] == "payslip") ? 'checked' : '' ?>></td>
+                        <td style="text-align: left"><input type="checkbox" name="requests" id="requests" <?php if($value['Form'] == "requests") ? 'checked' : '' ?>></td>
+                        <td style="text-align: left"><input type="checkbox" name="approvers" id="approvers" <?php if($value['Form'] == "approvers") ? 'checked' : '' ?>></td>
+                        <td style="text-align: left"><input type="checkbox" name="activities" id="activities" <?php if($value['Form'] == "activities") ? 'checked' : '' ?>></td>
+                        <td style="text-align: left"><input type="checkbox" name="memo" id="memo" <?php if($value['Form'] == "memo") ? 'checked' : '' ?>></td>
+                        <td style="text-align: left"><input type="checkbox" name="ads" id="ads" <?php if($value['Form'] == "ads") ? 'checked' : '' ?>></td>
+                        <td style="text-align: left"><input type="checkbox" name="bday" id="bday" <?php if($value['Form'] == "bday") ? 'checked' : '' ?>></td>
+                        <td style="text-align: left"><input type="checkbox" name="access" id="access" <?php if($value['Form'] == "access") ? 'checked' : '' ?>></td>
                     </tr>
                     <?php endforeach; ?>
                     <?php if ($pages) : ?>
