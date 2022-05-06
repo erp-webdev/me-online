88a21980ed38bf60a7af53c40dfb9b29bcd8b60a
Kevs <keven.ayag@adamson.edu.ph>
2022-05-06 08:40:51 +0800

    update

--- objects/2015/payslip.php
+++ objects/2015/payslip.php
@@ -22,7 +22,7 @@
         if(isset($_GET['pryear']) && isset($_GET['period'])){
             $payper = $_GET['period'];
             $pryear = $_GET['pryear'];
-            $payslip_period = $mainsql->get_payslip_period($pryear, $profile_comp, $payper);          
+            $payslip_period = $mainsql->get_payslip_period_by_id($pryear, $profile_comp, $payper);          
         }
 
         $dtr_year = $mainsql->get_dtr_year($profile_comp);     
