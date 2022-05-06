f9b46bd27126aa867bf5e8892348ac94adf511ee
Kevs <keven.ayag@adamson.edu.ph>
2022-05-06 08:58:46 +0800

    update

--- template/2015/payslip.php
+++ template/2015/payslip.php
@@ -83,9 +83,16 @@
                                         <?php
                                             // new pay period beginning apr2020
                                             if($payslip_period[1]['PRYear'] > 2020 || ($payslip_period[1]['PRYear'] == 2020 && !in_array( $payslip_period[1]['PeriodID'], ['S01', 'S02', 'S03', 'S04', 'S05', 'S06', 'S07', 'S08', 'S09'])))
-                                                echo date("m/d/Y", strtotime($payslip_period[1]['PRFrom'] ? $payslip_period[1]['PRFrom'] : $payslip_period[0]['PRFrom'])) . " to " . date("m/d/Y", strtotime($payslip_period[1]['PRTo'] ? $payslip_period[1]['PRTo'] : $payslip_period[0]['PRTo']));
-                                            else
-                                                echo date("m/d/Y", strtotime($payslip_period[1]['PeriodFrom'] ? $payslip_period[1]['PeriodFrom'] : $payslip_period[0]['PeriodFrom'])). " to " . date("m/d/Y", strtotime($payslip_period[1]['PeriodTo'] ? $payslip_period[1]['PeriodTo'] : $payslip_period[0]['PeriodTo']));
+                                            
+                                                // echo date("m/d/Y", strtotime($payslip_period[1]['PRFrom'] ? $payslip_period[1]['PRFrom'] : $payslip_period[0]['PRFrom'])) . " to " . date("m/d/Y", strtotime($payslip_period[1]['PRTo'] ? $payslip_period[1]['PRTo'] : $payslip_period[0]['PRTo']));
+                                                
+                                                echo date("m/d/Y", strtotime($payslip_period_[0]['PRFrom'])) . " to " . date("m/d/Y", strtotime($payslip_period_[0]['PRTo']));
+
+                                                else
+
+                                                // echo date("m/d/Y", strtotime($payslip_period[1]['PeriodFrom'] ? $payslip_period[1]['PeriodFrom'] : $payslip_period[0]['PeriodFrom'])). " to " . date("m/d/Y", strtotime($payslip_period[1]['PeriodTo'] ? $payslip_period[1]['PeriodTo'] : $payslip_period[0]['PeriodTo']));
+
+                                                echo date("m/d/Y", strtotime($payslip_period_[0]['PeriodFrom'])). " to " . date("m/d/Y", strtotime($payslip_period_[0]['PeriodTo']));
                                             ?></span><br>
                                     <b>Pay Period: </b><span id='payto'>
                                         <?php
