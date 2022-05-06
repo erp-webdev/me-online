6d069ad04e90f777b53ef7c1cd1a976028e8c2d5
Kevs <keven.ayag@adamson.edu.ph>
2022-05-06 08:07:24 +0800

    update

--- lib/class/mainsql.class.php
+++ lib/class/mainsql.class.php
@@ -1715,7 +1715,7 @@ class mainsql {
         endif;
 
         $sql .= " AND CompanyID = '".$company."' AND PaymentType = 'SEMI-MONTHLY' 
-            ORDER BY CASE WHEN PeriodID = '".$period."' THEN '-' ELSE PeriodID END DESC";
+            ORDER BY PeriodID DESC";
 		$result = $this->get_row($sql, $dbname);
 		return $result;
     }
