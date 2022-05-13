6dcc9dfdfb579ec8f8e06f9b1168971bd4103daa
Kevs <keven.ayag@adamson.edu.ph>
2022-05-13 15:33:38 +0800

    update

--- template/2015/wfh.php
+++ template/2015/wfh.php
@@ -1,5 +1,12 @@
 <?php include(TEMP."/header.php"); ?>
 
-<?php include(TEMP."/wfh/wfh.php"); ?>
+<?php 
+	if($wfh_user[0]["EmpID"] == $profile_idnum && $wfh_user[0]["DBNAME"] == $profile_dbname) {
+		include(TEMP."/wfh/wfh.php"); 
+	}else{
+		include(TEMP."/wfh/wfh_clearance.php"); 
+	}
+
+?>
 
 <?php include(TEMP."/footer.php"); ?>
