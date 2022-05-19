6dc176a82891ca967f05f752db32faa5932fc1ee
Kevs <keven.ayag@adamson.edu.ph>
2022-05-19 15:44:54 +0800

    update

--- template/2015/pafglobal.php
+++ template/2015/pafglobal.php
@@ -270,7 +270,7 @@
                         if (target.length) {
                             $('html,body,#pafm').animate({
                                 scrollTop: target.offset().top
-                            }, 1000);
+                            }, 800);
                             return false;
                         }
                     });
