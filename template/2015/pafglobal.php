da2fd198995b6444aa8d2a526b145a847eb35b45
Kevs <keven.ayag@adamson.edu.ph>
2022-05-19 15:50:02 +0800

    update

--- template/2015/pafglobal.php
+++ template/2015/pafglobal.php
@@ -269,7 +269,7 @@
                         var target = $('#<?php echo $_GET['ref']; ?>')
                         if (target.length) {
                             $('html,body,#pafm').animate({
-                                scrollTop: target.offset().top - 200
+                                scrollTop: target.offset().top - 400
                             }, 1000);
                             return false;
                         }
