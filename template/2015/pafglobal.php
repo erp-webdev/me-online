0f903c85da06ade2b4908331a1bd72f0d504482a
Kevs <keven.ayag@adamson.edu.ph>
2022-05-19 15:40:02 +0800

    update

--- template/2015/pafglobal.php
+++ template/2015/pafglobal.php
@@ -268,27 +268,14 @@
                     $(document).ready(function () {
                         var target = $('#<?php echo $_GET['ref']; ?>')
                         if (target.length) {
-                            $('html,body').animate({
+                            $('html,body,#pafm').animate({
                                 scrollTop: target.offset().top
                             }, 1000);
                             return false;
                         }
                     });
 
-                    $(function() {
-                        $('a[href*=#]:not([href=#])').click(function() {
-                            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-                                var target = $(this.hash);
-                                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-                                if (target.length) {
-                                    $('#pafm').animate({
-                                        scrollTop: target.offset().top
-                                    }, 1000);
-                                    return false;
-                                }
-                            }
-                        });
-                    });
+                   
                 });
             </script>
 
