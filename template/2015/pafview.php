0fcd580b675112f1d2cadfe55e5946d3c1ab539b
Kevs <keven.ayag@adamson.edu.ph>
2022-06-07 09:12:12 +0800

    update

--- template/2015/pafview.php
+++ template/2015/pafview.php
@@ -38,9 +38,11 @@
                     if($appdt <= date('Y-m-d', strtotime('2018-12-31'))){
                         include(TEMP.'/pms/pafview_old.php');
                     }else{
+                        echo '<!-- kevs -->';
                         include(TEMP.'/pms/pafview_2019.php');
                     }
                  ?>
+                        echo '<!-- kevs1 -->';
                 </div>
             </div>
         </div>
