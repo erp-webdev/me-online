5f95139f7e18b682dce774e7d4e66af5fc135d21
Kevs <keven.ayag@adamson.edu.ph>
2022-06-08 16:22:11 +0800

    update

--- template/2015/menu.php
+++ template/2015/menu.php
@@ -102,7 +102,9 @@
                         <?php endif; ?>
                         <?php if ($profile_level >= 9 || count($approver_employees) > 0) : ?>
                         <?php if ($section != "dtrman") { ?>
-                            <span class="spanred">NEW!</span>
+                            <?php if ($profile_level < 9 || count($approver_employees) > 0) : ?>
+                            <span class="spanred" style="display:block">NEW!</span>
+                            <?php endif; ?>
                             <a href="<?php echo WEB; ?>/dtrman" onclick="clickAndDisable(this);"><?php } ?><div<?php if ($section == "dtrman") { ?> class="dselected"<?php } ?>>System &amp; DTR Management</div><?php if ($section != "dtrman") { ?></a><?php } ?>
                         <?php endif; ?>
 
