e902273992dc91167777a008e4af48df301491e6
Kevs <keven.ayag@adamson.edu.ph>
2022-06-15 11:45:32 +0800

    update

--- objects/2015/qrcode.php
+++ objects/2015/qrcode.php
@@ -10,10 +10,10 @@
 
 		global $sroot, $profile_id, $unix3month;
 
-		$table_draw_query = "SELECT * FROM SUBSIDIARY.DBO.TABLEDRAW$ WHERE EmpID = '$profile_id' AND DBNAME = '$profile_dbname'";
-		$table_draw = $mainsql->get_row($table_draw_query);
-		$dept_name = $table_draw[0]['TABLE ASSIGNMENT'];
-		$table = $table_draw[0]['TABLE NUMBER'];
+		// $table_draw_query = "SELECT * FROM SUBSIDIARY.DBO.TABLEDRAW$ WHERE EmpID = '$profile_id' AND DBNAME = '$profile_dbname'";
+		// $table_draw = $mainsql->get_row($table_draw_query);
+		// $dept_name = $table_draw[0]['TABLE ASSIGNMENT'];
+		// $table = $table_draw[0]['TABLE NUMBER'];
 
 
 		// $emp_dept_query = "SELECT DeptDesc FROM $profile_dbname.DBO.HRDepartment where DeptID = '$dept_id'";
