a81c6450c474770256f2737116cea4cf977b5691
Kevs <keven.ayag@adamson.edu.ph>
2022-06-15 11:23:09 +0800

    activate feed on event start date

--- template/2015/qrcode.php
+++ template/2015/qrcode.php
@@ -11,18 +11,7 @@
 
                                 <img src="https://chart.googleapis.com/chart?chs=400x400&cht=qr&chl=<?php echo $id; ?>&choe=UTF-8" width="400" height="400" onerror="alert('QR Code failed to load. Please check your internet connection');">
 								<!-- place if condition here if activity is xmas party -->
-								<?php if($_SESSION["xmas"] == 1){ ?>
-								<div>
-									<table>
-										<tbody align="left">
-											<?php
-												echo "<tr><td>Table: </td><td>".$table.'</td></tr>';
-												echo "<tr><td>Department: </td><td>".$dept_name.'</td></tr>';
-											?>
-										</tbody>
-									</table>
-								</div>
-								<?php } ?>
+								               
 								<div class="clearboth">
                                     <button onclick="window.history.back();" class="btn btnred margintop25">Back</button>
                                 </div>
