9f5e051465c98227313d03aa1b89887843e26d57
Kevs <keven.ayag@adamson.edu.ph>
2022-06-16 09:33:23 +0800

    testing dtr

--- lib/requests/dtr_request.php
+++ lib/requests/dtr_request.php
@@ -161,9 +161,8 @@
                 endif;
             endif;
 
-            echo $_POST['db']; exit;
             if ($dfrom > $lastpoint) :
-                $dtr_calculate = $mainsql->dtr_action($_POST, 'calculate', 0, $_POST['db']);
+                $dtr_calculate = $mainsql->dtr_action($_POST, 'calculate');
             else :
                 $dtr_calculate = 0;
             endif;
