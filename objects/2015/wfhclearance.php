0b65dca61ae7b166cc5427aa61dbd75cb25702bb
Kevs <keven.ayag@adamson.edu.ph>
2022-06-21 09:01:37 +0800

    work from home clearance

--- objects/2015/wfhclearance.php
+++ objects/2015/wfhclearance.php
@@ -1,1 +1,31 @@
-test
+<?php
+	if ($logged == 1) {
+        if ($wfhc_app) :
+            
+            // include OBJ . '/wfh/WFHClearance.php';
+            // $wfh_clearance = new WFHClearance();
+
+            # PAGINATION
+            $page = isset($_GET["page"]) ? (int)$_GET["page"] : 1 ;
+            $start = NUM_ROWS * ($page - 1);
+
+            //*********************** MAIN CODE START **********************\\
+
+            # ASSIGNED VALUE
+            $page_title = "WFH Clearance";
+            global $sroot, $profile_id, $unix3month;
+
+            //***********************  MAIN CODE END  **********************\\
+        endif;
+	}
+	else
+	{
+		if ($_POST['btnwfhapply'] || $_POST['btnwfhapply_x']){
+			echo '{"success": false, "error": "Your session has expired! Kindly logout and login again to continue."}';
+			exit();
+		}
+		echo "<script language='javascript' type='text/javascript'>window.location.href='".WEB."/login'</script>";
+
+	}
+
+?>
