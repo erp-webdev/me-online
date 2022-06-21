1cf3c98df586bb9368fecfde4c1ef4bb152d4cd4
Kevs <keven.ayag@adamson.edu.ph>
2022-06-21 09:44:44 +0800

    work from home clearance

--- objects/2015/wfh/WFHClearance.php
+++ objects/2015/wfh/WFHClearance.php
@@ -1,14 +1,3 @@
 <?php 
 include OBJ . '/Mail/Mail.php';
 
-private class WFHClearance{
-
-    private $types = [
-        'sickness' => 'Sickness / Illness',
-    ];
-
-    public function getTypes(){
-        return $this->types;
-    }
-
-}
