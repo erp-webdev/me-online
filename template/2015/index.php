f5aa9da85cb79b24d36f56d36fc02580e9e7f30f
Kevs <keven.ayag@adamson.edu.ph>
2022-06-22 11:16:22 +0800

    meg@33

--- template/2015/index.php
+++ template/2015/index.php
@@ -6,7 +6,7 @@
                             <div class="closebutton cursorpoint"><i class="fa fa-times-circle fa-3x redtext"></i></div>
                             <div id="adview_title" class="robotobold cattext dbluetext">Megaworld Turns 33</div>
                             <div id="ads_data" class="floatdata margintop15">
-                                <video src="<?php echo WEB; ?>/uploads/ltg_megaworld_turns_33.mp4" type="video/mp4" autoplay></video>
+                                <video width="100%" src="<?php echo WEB; ?>/uploads/ltg_megaworld_turns_33.mp4" type="video/mp4" autoplay></video>
                             </div>
                         </div>
                     </div>
