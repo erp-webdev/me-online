ae76eda1c603238793ce3a8d3eab3a79847f1de1
Kevs <keven.ayag@adamson.edu.ph>
2022-06-22 13:51:41 +0800

    meg@33

--- template/2015/index.php
+++ template/2015/index.php
@@ -134,6 +134,7 @@
                                 <?php if (strtotime('2022-06-22') <= strtotime(date('Y-m-d'))) : ?>
                                 <div id="mainnotification" class="dashcomp dashincentive centertalign  marginbottom25">
                                     <a onclick="showmeg()" class="roboto bluetext mediumtext" style="text-align:center">Megaworld Turns 33!</a>
+                                    <img src="<?php echo WEB; ?>/uploads/megaworld_turns_33.png" alt="Megaworld Turns 33!" width="100%">
                                 </div>
                                 <?php endif; ?>
 
