a6b2cbbce032dd7fc40ee7766a48e03169a3c620
Kevs <keven.ayag@adamson.edu.ph>
2022-06-22 11:40:22 +0800

    meg@33

--- template/2015/index.php
+++ template/2015/index.php
@@ -123,7 +123,7 @@
                                 <?php endif; ?>
                                 <?php if ($profile_dbname != 'ECINEMA' && $profile_dbname != 'EPARKVIEW' && $profile_dbname != 'NEWTOWN' && $profile_dbname != 'LAFUERZA') : ?>
 
-                                <div id="mainnotification" class="lefttalign whitetext marginbottom25">
+                                <div id="mainnotification" class="dashcomp dashincentive lefttalign whitetext marginbottom25">
                                     <b class="smalltext dorangetext">Megaworld Turns 33</b>
                                 </div>
 
