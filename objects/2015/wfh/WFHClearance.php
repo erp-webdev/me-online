32eb88672399925f66cb17fd0592ca26fc6891c7
Kevs <keven.ayag@adamson.edu.ph>
2022-06-23 08:11:00 +0800

    wfh clearance

--- objects/2015/wfh/WFHClearance.php
+++ objects/2015/wfh/WFHClearance.php
@@ -41,6 +41,7 @@ class WFHClearance{
     public function submit($params)
     {
 		echo '{"success": false, "error": "Your session has expired! Kindly logout and login again to continue."}';
+        exit();
     }
 
 }
