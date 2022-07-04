8c5ea63fdc914100e3f5f78e557e29c79d90f6d4
Kevs <keven.ayag@adamson.edu.ph>
2022-07-04 17:26:48 +0800

    update

--- objects/2015/wfhclearance.php
+++ objects/2015/wfhclearance.php
@@ -63,8 +63,8 @@
                     if($add_wc) : 
 
                         $wfh_clearance->saveAttachment($add_wc);
-                        $wfh_clearance->notifyRequestor($_POST['empid'], $add_wc);
-                        $wfh_clearance->notifyApprovers($_POST['empid'], $add_wc, $_POST['approver1'], $_POST['dbapprover1']);
+                        // $wfh_clearance->notifyRequestor($_POST['empid'], $add_wc);
+                        // $wfh_clearance->notifyApprovers($_POST['empid'], $add_wc, $_POST['approver1'], $_POST['dbapprover1']);
                         echo 'kevs---'; exit;
                         echo '{"success": true}';
                         exit();
