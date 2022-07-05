98af0a3473a2a026882424730622fdb105d349f5
Kevs <keven.ayag@adamson.edu.ph>
2022-07-05 08:17:11 +0800

    update

--- lib/class/mainsql.class.php
+++ lib/class/mainsql.class.php
@@ -1497,11 +1497,11 @@ class mainsql {
                     $sql .= " WHERE [outer].[ROW_NUMBER] BETWEEN ".(intval($start) + 1)." AND ".intval($start + $limit)." ORDER BY [outer].[ROW_NUMBER] ";
                 endif;
 
-                echo $sql; exit;
-
                 if ($count) : $result = $this->get_numrow($sql);
                 else : $result = $this->get_row($sql);
                 endif;
+
+                var_dump($result); exit;
 
             break;
         }
