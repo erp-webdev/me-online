b5212bc611c858897656ad2881250f0ebe8c9035
Kevs <keven.ayag@adamson.edu.ph>
2022-07-06 08:38:56 +0800

    update

--- objects/2015/mail/Mail.php
+++ objects/2015/mail/Mail.php
@@ -15,9 +15,14 @@ class Mail{
         $headers .= "MIME-Version: 1.0\r\n";
         $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-        if($this->mailtest)
+        if($this->mailtest){
+            $body .= '<p><b>NOTICE<b> This is a test mail and not intended for actual notification. 
+                Should you receive this email, please let us know through devteam@megaworldcorp.com. 
+                The email test is inteded for ' . $to . ' </p>';
+        
             $to = 'kayag.global@megaworldcorp.com';
-            
+        }
+
         return $sendmail = mail($to, $subject, $body, $headers);
     }
 }
