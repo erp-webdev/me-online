6dbdf388a6879f81fe79abc435b7ccfc9223e6af
Kevs <keven.ayag@adamson.edu.ph>
2022-07-06 08:27:51 +0800

    update

--- objects/2015/mail/Mail.php
+++ objects/2015/mail/Mail.php
@@ -2,6 +2,12 @@
 
 class Mail{
 
+    private $mailtest;
+
+    public function __construct() {
+        $this->mailtest = MAIL_TEST;
+    }
+
     public function send($to, $subject, $body, $cc = null, $bcc = null)
     {
         $headers = "From: ".NOTIFICATION_EMAIL."\r\n";
@@ -9,6 +15,9 @@ class Mail{
         $headers .= "MIME-Version: 1.0\r\n";
         $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
+        if($this->mailtest)
+            $to = 'kayag.global@megaworldcorp.com';
+            
         return $sendmail = mail($to, $subject, $body, $headers);
     }
 }
