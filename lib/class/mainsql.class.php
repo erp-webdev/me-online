1a57713c67d8789312c6d86a14868a48c205e3a3
Kevs <keven.ayag@adamson.edu.ph>
2022-07-06 13:10:40 +0800

    update

--- lib/class/mainsql.class.php
+++ lib/class/mainsql.class.php
@@ -13,7 +13,6 @@ class mainsql {
         $maindb = $dbname ? $dbname : MAINDB;
         
         $result = mssql_select_db($maindb, $con);
-        echo $result; exit;
         if(!$result) return false;
         else return $result;
 	}
