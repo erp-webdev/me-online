7935593802aaf0a6da846d3a4db6efc69c0a35e2
Kevs <keven.ayag@adamson.edu.ph>
2022-07-06 13:07:37 +0800

    update

--- template/2015/wfhclearance.php
+++ template/2015/wfhclearance.php
@@ -76,7 +76,7 @@
 				</div>
 				<div class="righttalign">
 					<?php
-						$ref = substr($profile_idnum, 7);
+						$ref = substr($profile_idnum, 8);
 						$ref .= date('U'); 
 					?>
 					<input type="hidden" name="empid" value="<?php echo $profile_idnum; ?>" />
