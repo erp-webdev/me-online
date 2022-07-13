922000c5b03d0b4b573cefda4102b6c6e01c4371
Kevs <keven.ayag@adamson.edu.ph>
2022-07-13 14:06:09 +0800

    update on wfh

--- lib/requests/notification_request.php
+++ lib/requests/notification_request.php
@@ -4125,7 +4125,8 @@
                         <?php if ($notification_data[0]['ApprovedDate01']) : ?>
                         <tr>
                             <td><b>Date</b></td>
-                            <td><?php echo date("F j, Y | g:ia", strtotime($notification_data[0]['ApprovedDate01'])); ?></td>
+                            <td><?php echo date("F j, Y | g:ia", strtotime($notification_data[0]['ApprovedDate01'])); ?> <br>
+                        <?php echo $notification_data[0]['Remarks01']; ?></td>
                         </tr>
                             <?php if (trim($notification_data[0]['Remarks01'])) : ?>
                             <tr>
--- objects/2015/wfh.php
+++ objects/2015/wfh.php
@@ -237,7 +237,7 @@
                         $message .= "New request ".$requestor[0]['FName']." ".$requestor[0]['LName']." for Work from Home with Reference No: ".$add_wfh." on ".date('F j, Y')." for your approval. ";
                         $message .= "<br><br>Thanks,<br>";
                         $message .= SITENAME." Admin";
-			$message .= "<br>Click<a href='https://portal.megaworldcorp.com/me/login'> here</a> to login";
+			            $message .= "<br>Click<a href='https://portal.megaworldcorp.com/me/login'> here</a> to login";
                         $message .= "<hr />".MAILFOOT."</div>";
 
                         $headers = "From: ".NOTIFICATION_EMAIL."\r\n";
