f405f07d78390aa2fc5b2ef7b0744cb021eaf087
Kevs <keven.ayag@adamson.edu.ph>
2022-07-18 14:40:07 +0800

    update

--- template/2015/coe/approved-leave.php
+++ template/2015/coe/approved-leave.php
@@ -3,7 +3,10 @@
 <p>
     This is to certify that <strong><?php 
         echo $emp_info[0]["Salutation"]." ".strtoupper(mb_convert_encoding($emp_info[0]["FullName"], 'UTF-8', 'HTML-ENTITIES')); ?></strong> <?php 
-        if($emp_info[0]["DateResigned"]) echo 'was employed'; else echo 'is currently employed'; ?> with <strong><?php echo $emp_info[0]["CompanyName"]; ?></strong> as <strong><?php echo $emp_info[0]["PositionDesc"]." - ".$emp_info[0]["DeptDesc"]."</strong>  Since <strong>".$emp_info[0]["HireDate"]; ?> </strong> <?php if($emp_info[0]["DateResigned"]) echo 'to ' . .$emp_info[0]["DateResigned"]; else echo 'up to the present.'; ?>
+        if($emp_info[0]["DateResigned"]) echo 'was employed'; else echo 'is currently employed'; ?> with <strong><?php 
+        echo $emp_info[0]["CompanyName"]; ?></strong> as <strong><?php 
+        echo $emp_info[0]["PositionDesc"]." - ".$emp_info[0]["DeptDesc"]."</strong>  Since <strong>".$emp_info[0]["HireDate"]; ?> </strong> <?php 
+        if($emp_info[0]["DateResigned"]) echo 'to ' .$emp_info[0]["DateResigned"]; else echo 'up to the present.'; ?>
 </p>
 <p>
     This is to further certify that <?php echo $emp_info[0]["salutation"]." ".strtoupper(mb_convert_encoding($emp_info[0]["FullName"], 'UTF-8', 'HTML-ENTITIES')); ?> shall be on leave from <b><?php echo date('F d, Y', strtotime($start_date))." to ".date('F d, Y', strtotime($end_date)); ?> </b>as approved by the Management. <?php echo $emp_info[0]["Gender"]; ?> is expected to report back for work on <b><?php echo date('F d, Y', strtotime($return_date)); ?></b>.
