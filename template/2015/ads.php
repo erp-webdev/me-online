e27f27de3a7649e11d818f024699548692c437a4
Kevs <keven.ayag@adamson.edu.ph>
2022-07-19 09:05:08 +0800

    update

--- template/2015/ads.php
+++ template/2015/ads.php
@@ -103,7 +103,7 @@
                                 
                                 <table class="width100per">
                                     <tr>
-                                        <td><span class="fa fa-search"></span> Search by: 
+                                        <td><span class="fa fa-search"></span> <?php var_dump($accessman->hasAccess($profile_id, $profile_dbname, 'ads')); ?> Search by: 
                                             <input type="text" id="searchads" name="searchads" value="<?php echo $_SESSION['searchads'] ? $_SESSION['searchads'] : ''; ?>" placeholder="ads title" class="searchads smltxtbox" />&nbsp;
                                             <input type="button" id="btnsearchads" name="btnsearchads" value="Search" class="smlbtn btnsearchads" />
                                             <input type="button" id="btnsearchallads" name="btnsearchallads" value="View All" class="btnsearchallads smlbtn<?php if (!$_SESSION['searchads']) : ?> invisible<?php endif; ?>" />                                            
