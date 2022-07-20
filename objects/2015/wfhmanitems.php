196fa68c802416448f9db9959b23bfb0c7d2795a
Kevs <keven.ayag@adamson.edu.ph>
2022-07-20 10:41:44 +0800

    update

--- objects/2015/wfhmanitems.php
+++ objects/2015/wfhmanitems.php
@@ -35,7 +35,7 @@
             }                              
 
             $wciman_data = $wfhman->getWfhClearanceItems($start, REQ_NUM_ROWS, $searchwciman, 0);
-            $wciman_count = $wfhman->getWfhClearanceItems(0, 0, $searchdtrm, 1);
+            $wciman_count = $wfhman->getWfhClearanceItems(0, 0, $searchwciman, 1);
 
             $pages = $mainsql->pagination("wfhman", $wciman_count, APPR_NUM_ROWS, 9);
         
