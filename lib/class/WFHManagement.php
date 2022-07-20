2069bae7c5b7641aebbbebe432f6c77c07b9badf
Kevs <keven.ayag@adamson.edu.ph>
2022-07-20 15:18:03 +0800

    update

--- lib/class/WFHManagement.php
+++ lib/class/WFHManagement.php
@@ -26,7 +26,7 @@ class WFHManagement extends mainsql{
         if ($limit) :
             $sql .= " WHERE [outer].[ROW_NUMBER] BETWEEN ".(intval($start) + 1)." AND ".intval($start + $limit)." ORDER BY [outer].[ROW_NUMBER] ";
         endif;
-        
+        echo $sql; exit;
 		if ($count) : $result = $this->get_numrow($sql);
         else : $result = $this->get_row($sql);
         endif;
