e0c596c11478ed1b29c991a97193ccc6688a5f68
Kevs <keven.ayag@adamson.edu.ph>
2022-07-28 11:05:03 +0800

    update

--- template/2015/coe/layout.php
+++ template/2015/coe/layout.php
@@ -9,6 +9,8 @@
     <style>
         body {
             background: rgb(204,204,204); 
+            width: 21cm;
+            height: 29.7cm;
         }
 
         page {
@@ -17,8 +19,7 @@
             margin: 0 auto;
             margin-bottom: 0.5cm;
             box-shadow: 0 0 0.5cm rgba(0,0,0,0.5);
-            width: 21cm;
-            height: 29.7cm; 
+             
         }
 
         page[size="A4"] {  
