34a5f441be00319176c48cf482b8d86fe47372b6
Kevs <keven.ayag@adamson.edu.ph>
2022-07-28 11:17:10 +0800

    update

--- template/2015/coe/layout.php
+++ template/2015/coe/layout.php
@@ -7,66 +7,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>COE Certificates</title>
     <style>
-        body {
-            background: rgb(204,204,204); 
-            width: 21cm;
-            height: 29.7cm;
-        }
-
-        page {
-            background: white;
-            display: block;
-            margin: 0 auto;
-            margin-bottom: 0.5cm;
-            box-shadow: 0 0 0.5cm rgba(0,0,0,0.5);
-             
-        }
-
-        page[size="A4"] {  
-            width: 21cm;
-            height: 29.7cm; 
-        }
-
-        page[size="A4"][layout="portrait"] {
-            width: 29.7cm;
-            height: 21cm;  
-        }
-
-        page[size="A4"][layout="landscape"] {
-            height: 29.7cm;
-            width: 21cm;  
-        }
-
-        .container {
-            /* min-height:100%; */
-            position:relative;
-            margin: 1in;
-            width: 21cm;
-            height: 29.7cm;
-        }
-
-        .header {
-            padding:1in;
-        }
         
-        .body {
-            padding:1in;
-            padding-bottom:0.5in;   /* Height of the footer */
+        @page {
+            size: A4;
+            padding: 1in;
         }
 
-        .footer {
-            position:absolute !important;
-            bottom:0;
-            width:100%;
-            height:0.5in;   /* Height of the footer */
-        }
-
-        @media print {
-            body, page {
-                margin: 0;
-                box-shadow: 0;
-            }
-        }
+        
 
     </style>
 </head>
