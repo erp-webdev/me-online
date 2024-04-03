1d53cd23ade6fe9dbbf983a40027e1eef533d9e0
Kevs-desktop <kayag.global@megaworldcorp.com>
2024-04-03 10:28:49 +0800

    update

--- template/2015/pms/pms_evaluation_form_mega.php
+++ template/2015/pms/pms_evaluation_form_mega.php
@@ -275,7 +275,7 @@
                                     <span ng-bind="goal.MeasureOfSuccess"  ng-show="goal.Goals != '8 hrs mandatory training' || goal.id != null"></span>
                                     <br> <br>
                                     <strong>Comments</strong><br>
-                                    <textarea class="checker" cols="80" rows="2" ng-model="goal.Comments" placeholder="Provide your comments" ng-disabled="is_approved"></textarea>
+                                    <textarea class="checker" cols="80" rows="2" ng-model="goal.Comments" placeholder="Provide your comments" ng-disabled="is_approved" minlength="10"></textarea>
                                     <span ng-show="goal.id == null && goal.Goals != '8 hrs mandatory training'">
                                     <br><br>
                                     <a class="smlbtn" id="delrowg" style="background-color:#D20404;" ng-click="deleteGoal($index)">Delete</a>
