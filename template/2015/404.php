2e7a5fff6f0cbf79b7216f167b04c6c803501162
Kevs-desktop <kayag.global@megaworldcorp.com>
2024-09-20 11:21:54 +0800

    update 404 error page

--- template/2015/404.php
+++ template/2015/404.php
@@ -7,8 +7,7 @@
     <!-- BODY -->
                     
                     <div id="mainsplashtext" class="mainsplashtext lefttalign">  
-                        <div class="topsplashtext lefttalign robotobold cattext whitetext"><?php echo WELCOME; ?></div>
-                        <div class="leftsplashtext lefttalign"><?php include(TEMP."/menu.php"); ?></div>
+                        <!-- <div class="topsplashtext lefttalign robotobold cattext whitetext"><?php echo WELCOME; ?></div> -->
                         <div class="rightsplashtext lefttalign">
                             
                             <div id="mainnotification" class="mainbody lefttalign whitetext">  
@@ -18,7 +17,7 @@
                                 <br>
                                 You can either head back to the following link: 
                                     <ul>
-                                    <li onclick="window.history.back();" class="underlined cursorpoint whitetext">where you belong</li><a href="<?php echo WEB; ?>" class="underlined whitetext"><li>on our homepage</li></a><li>click one of the menu on the left</li>
+                                    <li onclick="window.history.back();" class="underlined cursorpoint whitetext">where you belong</li><a href="<?php echo WEB; ?>" class="underlined whitetext"><li>on our homepage</li></a>
                                     </ul>
                             </div>
                             
