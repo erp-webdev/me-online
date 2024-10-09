fd111a71850535decb5ea42a7733c5452bb8c7d1
Anna Eugenio <anna@noreply@alias.megaworldcorp.com>
2024-10-09 17:38:29 +0800

    Update on password update advisory

--- objects/2015/waive_advisory.php
+++ objects/2015/waive_advisory.php
@@ -1,15 +1,20 @@
 <?php
    if($logged == 1){
       $user = $logsql->check_login_user($profile_idnum, $profile_email);
-      $waive = $user[0]['password_waive'] ? $user[0]['password_waive'] + 1 : 1;
+      $last_password_update = $user[0]['password_update_at'];
 
-      if($waive<=MAX_PASSWORD_WAIVE){
-         $logsql->update_users_activity($profile_idnum, $profile_email, $waive);
-         echo "<script language='javascript' type='text/javascript'>window.location.href='".WEB."'</script>";
-      }
-      else{
-         echo "<script> alert('You have already waived the password update advisory three times which is the maximum limit. To ensure the security of your account, it is now mandatory to change your password.')</script>";
-         echo "<script language='javascript' type='text/javascript'>window.location.href='".WEB."/change_password'</script>";
+      if(date('Y-m-d') >= date('Y-m-d', strtotime('+'.PASSWORD_REMINDER.' month', strtotime($last_password_update)))){
+         $waive = $user[0]['password_waive'] ? $user[0]['password_waive'] + 1 : 1;
+
+         if($waive<=MAX_PASSWORD_WAIVE){
+            $logsql->update_users_activity($profile_idnum, $profile_email, $waive);
+         }
+         else{
+            echo "<script> alert('You have reached the maximum limit of waiving the Password Update Advisory. To ensure the security of your account, you are now required to change your password.')</script>";
+            echo "<script language='javascript' type='text/javascript'>window.location.href='".WEB."/change_password'</script>";
+         }
       }
    }
+
+   echo "<script language='javascript' type='text/javascript'>window.location.href='".WEB."'</script>";
 ?>
