6d5eff04504732685f6c095a8672a4bdce095449
Anna Eugenio <anna@noreply@alias.megaworldcorp.com>
2024-11-08 11:02:10 +0800

    update on password update advisory

--- objects/2015/waive_advisory.php
+++ objects/2015/waive_advisory.php
@@ -3,7 +3,14 @@
       $user = $logsql->check_login_user($profile_idnum, $profile_email);
       $last_password_update = $user[0]['password_update_at'];
 
-      if(date('Y-m-d') >= date('Y-m-d', strtotime('+'.PASSWORD_UPDATE_IF_WAIVE.' month', strtotime($last_password_update)))){
+      if($is_password_hashed){
+         $last_password_update_span = date('Y-m-d', strtotime('+'.PASSWORD_REMINDER.' month', strtotime($last_password_update)));
+     }
+     else{
+         $last_password_update_span = date('Y-m-d', strtotime('+'.PASSWORD_UPDATE_IF_NOT_HASHED.' month', strtotime($last_password_update)));
+     }
+
+     if(date('Y-m-d') >= $last_password_update_span){
          $waive = $user[0]['password_waive'] ? $user[0]['password_waive'] + 1 : 1;
 
          if($waive<=MAX_PASSWORD_WAIVE){
@@ -17,9 +24,5 @@
    }
 
    echo "<script language='javascript' type='text/javascript'>window.location.href='".WEB."'</script>";
-
-
-   //simulation
-   //
 ?>
 
