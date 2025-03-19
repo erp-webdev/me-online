7024a39e4371d8a6a736993354024f4b8e944e7e
anna <anna@dev.megaworldcorp.com>
2025-03-19 17:37:01 +0800

    Enhance api request with header authorization token

---
+++ objects/2015/ape.php
@@ -0,0 +1,64 @@
+<?php
+    if ($logged == 1 ) {
+        $url = MEWEB.'/peoplesedge/api/jwt/login'; 
+
+        $data = [
+            'email' => 'noreply@megaworldcorp.com',
+            'password' => 'QL5qdf7Bzcrp9a83RZ'
+        ];
+
+        $options = [
+            'http' => [
+                'header' => "Content-Type: application/json\r\n",
+                'method' => 'POST',
+                'content' => json_encode($data),
+                'ignore_errors' => true
+            ]
+        ];
+
+        $context = stream_context_create($options);
+        $response = file_get_contents($url, false, $context);
+
+        if($response){
+            $result = json_decode($response, true);
+
+            if (isset($result['access_token'])) {
+                $_SESSION['peoplesedge_access_token'] = $result['access_token'];
+    
+                $access_file_url = MEWEB.'/peoplesedge/api/employee/medical'; 
+                $data = [
+                    'EmpID' => $profile_idnum,
+                    'EmpDB' => $profile_dbname,
+                    'CompanyID' => $profile_comp
+                ];
+    
+                $options = [
+                    'http' => [
+                        'header' => "Content-Type: application/json\r\n" .
+                                    "Authorization: Bearer " . $_SESSION['peoplesedge_access_token'] . "\r\n",
+                        'method' => 'POST',
+                        'content' => json_encode($data),
+                        'ignore_errors' => true
+                    ]
+                ];
+    
+                $file_context = stream_context_create($options);
+                $ape_file_response = file_get_contents($access_file_url, false, $file_context);
+
+                if($ape_file_response){
+                    $ape_result = json_decode($ape_file_response, true);
+
+                    $MedicalDate = $ape_result['MedicalDate'];
+                    $MedicalRemarks = $ape_result['MedicalRemarks'];
+                    $ViewURL = $ape_result['viewUrl'];
+                    $DownloadURL = $ape_result['downloadUrl'];
+                }
+            }
+        }
+
+	}
+	else
+	{
+		echo "<script language='javascript' type='text/javascript'>window.location.href='".WEB."/login'</script>";
+	}
+?>
--- template/2015/ape.php
+++ template/2015/ape.php
@@ -1,99 +1,25 @@
-<div id="APEfloatdiv" class="floatdiv invisible">                                        
-    <div id="apeview" class="fview">
-        <div class="closebutton cursorpoint">
-            <a onclick='$("#APEfloatdiv").addClass("invisible")'><i class="fa fa-times-circle fa-3x redtext"></i></a>
-        </div>
-        <div id="ape_title" class="robotobold cattext dbluetext">Annual Physical Examination Result</div>
 
-        <div id="loadingIndicator" style="text-align: center; margin-top: 80px;">
-            <i class="fa fa-spinner fa-spin fa-2x"></i> Please wait...
-        </div>
+<div width="100%" height="700px">
+    <iframe id="filePreview" src='<?php echo $ViewURL; ?>#toolbar=1&zoom=60' width="100%" height="250px" style="border: none; margin-bottom:10px;"></iframe>
 
-        <div id="availableIndicator" style="text-align: center; margin-top: 80px; font-size: 0.9rem;">
-            <i>Annual Physical Examination result is not yet available.</i>
+    <div style="margin-top: 10px; font-size: 0.7rem;">
+        <div style="display: flex; justify-content: space-between; align-items: flex-start; margin-bottom: 5px;">
+            <span style="width: 30%; font-weight: bold;">Employee</span>
+            <span style="width: 70%; word-wrap: break-word;">
+                <?php echo $profile_full .' (' . $profile_idnum . ')' ?>
+            </span>
         </div>
-
-        <div id="ape_data" class="floatdata margintop15" style="display: none;"> 
-            <div class="">            
-                <div id="apeViewer" width="100%" height="700px">
-                    <iframe id="filePreview" width="100%" height="250px" style="border: none; margin-bottom:10px;"></iframe>
-
-                    <div style="margin-top: 10px; font-size: 0.7rem;">
-                        <div style="display: flex; justify-content: space-between; align-items: flex-start; margin-bottom: 5px;">
-                            <span style="width: 30%; font-weight: bold;">Employee</span>
-                            <span style="width: 70%; word-wrap: break-word;">
-                                <?php echo $profile_full .' (' . $profile_idnum . ')' ?>
-                            </span>
-                        </div>
-                        <div style="display: flex; justify-content: space-between; align-items: flex-start; margin-bottom: 5px;">
-                            <span style="width: 30%; font-weight: bold;">Schedule Date</span>
-                            <span style="width: 70%; word-wrap: break-word;" id="MedicalDate"></span>
-                        </div>
-                        <div style="display: flex; justify-content: space-between; align-items: flex-start;">
-                            <span style="width: 30%; font-weight: bold;">Remarks</span>
-                            <span style="width: 70%; word-wrap: break-word; max-height: 150px; overflow-y: auto; padding-right: 5px;" id="MedicalRemarks"></span>
-                        </div>
-                    </div>
-                </div>
-                <div style="display: flex; justify-content: center; align-items: center; margin-top: 15px;">
-                    <button id='downloadAPE' class="bigbtn">Download</button>
-                </div>
-            </div>
+        <div style="display: flex; justify-content: space-between; align-items: flex-start; margin-bottom: 5px;">
+            <span style="width: 30%; font-weight: bold;">Schedule Date</span>
+            <span style="width: 70%; word-wrap: break-word;" id="MedicalDate"><?php echo $MedicalDate; ?></span>
         </div>
-    </div>  
+        <div style="display: flex; justify-content: space-between; align-items: flex-start;">
+            <span style="width: 30%; font-weight: bold;">Remarks</span>
+            <span style="width: 70%; word-wrap: break-word; max-height: 150px; overflow-y: auto; padding-right: 5px;" id="MedicalRemarks"> <?php echo $MedicalRemarks; ?></span>
+        </div>
+    </div>               
+</div>
+<div style="display: flex; justify-content: center; align-items: center; margin-top: 15px;">
+    <button id='downloadAPE' data-url='<?php echo $DownloadURL; ?>' class="bigbtn">Download</button>
 </div>
 
-<script>
-    $(document).ready(function() {
-        $('#loadingIndicator').show();
-        $('#availableIndicator').hide();
-        $('#ape_data').hide();
-
-        $('#btnAPE').click(function() {
-            $('#APEfloatdiv').removeClass('invisible');
-            $("#apeview").removeAttr("style");
-            $.ajax({
-                url: '<?php echo MEWEB; ?>/peoplesedge/api/employee/medical',
-                type: 'GET',
-                data: {
-                    Action: 'View',
-                    EmpID: '<?php echo $profile_idnum; ?>',
-                    EmpDB: '<?php echo $profile_dbname; ?>',
-                    CompanyID: '<?php echo $profile_comp; ?>'
-                },
-                dataType: 'json',
-                success: function(data) {
-                    if(data){
-                        $('#filePreview').attr('src', data.url + '#toolbar=1&zoom=60');
-                        $('#MedicalDate').text(data.MedicalDate);
-                        $('#MedicalRemarks').text(data.MedicalRemarks);
-
-                        $('#loadingIndicator').hide();
-                        $('#availableIndicator').hide();
-                        $('#ape_data').fadeIn();
-                    }
-                    else{
-                        $('#loadingIndicator').hide();
-                        $('#availableIndicator').show();
-                        $('#ape_data').hide();
-                    }
-                },
-                error: function(xhr, status, error) {
-                    $('#loadingIndicator').hide();
-                    $('#availableIndicator').show();
-                    $('#ape_data').hide();
-                }
-            });
-        });
-
-        $('#downloadAPE').click(function() {
-            window.location.href = '<?php echo MEWEB; ?>/peoplesedge/api/employee-file/medical?' +
-            $.param({
-                Action: 'Download',
-                EmpID: '<?php echo $profile_idnum; ?>',
-                EmpDB: '<?php echo $profile_dbname; ?>',
-                CompanyID: '<?php echo $profile_comp; ?>'
-            });
-        });
-    });
-</script>
