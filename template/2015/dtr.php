ea39652958307fc8319a1a732df834b49d309733
anna <anna@dev.megaworldcorp.com>
2025-06-18 14:58:13 +0800

    addition of 'last updated at' tagging under dtr

--- template/2015/dtr.php
+++ template/2015/dtr.php
@@ -1,53 +1,54 @@
-	<?php include(TEMP."/header.php"); ?>
+<?php include(TEMP."/header.php"); ?>
 
-    <!-- BODY -->
-                    
-                    <div id="mainsplashtext" class="mainsplashtext lefttalign">  
-                        <div class="topsplashtext lefttalign robotobold cattext whitetext"><?php echo WELCOME; ?></div>
-                        <div class="leftsplashtext lefttalign"><?php include(TEMP."/menu.php"); ?></div>
-                        <div class="rightsplashtext lefttalign">
-                            <div id="mainnotification" class="mainbody lefttalign whitetext">  
-                                <b class="mediumtext lorangetext">DAILY TIME RECORD</b><br><br>
-                                
-                                <table>
-                                    <tr>
-                                        <td>Year: 
-                                            <select id="dtr_year" name="dtr_year" class="smltxtbox">
-                                                <?php $yearend = strtotime(date('Y').'-12-15'); ?>
-                                                <?php 
-                                                    $yearval = date('U') > $yearend ? date("Y") + 1 : date("Y");
-                                                    if(isset($_GET['pryear']))
-                                                        $yearval = $_GET['pryear'];
-                                                    
-                                                ?>
-                                                <?php foreach ($dtr_year as $key => $value) : ?>
-                                                <option value="<?php echo $value['PRYear']; ?>"<?php echo $value['PRYear'] == $yearval ? ' selected' : ''; ?>><?php echo $value['PRYear']; ?></option>    
-                                                <?php endforeach; ?>
-                                            </select>
-                                        </td>
-                                        <td>DTR Cut-off: 
-                                            <select id="dtr_cover" name="dtr_cover" class="width250 smltxtbox">
-                                                <?php foreach ($dtr_period as $key => $value) : ?>
-                                                <option value="<?php echo date("Y-m-d", strtotime($value['PeriodFrom']))." ".date("Y-m-d", strtotime($value['PeriodTo'])); ?>" dfrom="<?php echo strtotime($value['PeriodFrom']); ?>" dto="<?php echo strtotime($value['PeriodTo']); ?>" posted="<?php echo $value['AttPost']; ?>" period="<?php echo $value['PeriodID']; ?>" <?php if(isset($_GET['period'])) echo $_GET['period'] == $value['PeriodID'] ? 'selected' : ''; ?>>
-                                                    <?php echo $value['PeriodID']." ".$value['PRYear']." ".date("m/d/Y", strtotime($value['PeriodFrom']))." to ".date("m/d/Y", strtotime($value['PeriodTo'])); ?>
-                                                </option>  
-                                                <?php endforeach; ?>
-                                            </select>&nbsp;&nbsp;
-                                            <?php $attpost = $dtr_period[0]['AttPost']; ?>
-                                            <span id="txtposted" class="lgreentext bold<?php echo $attpost == 1 ? "" : " invisible"; ?>"><i class="fa fa-check"></i> POSTED</span>
-                                        </td>
-                                        <?php if ($psblock && $profile_dbname != 'OJTPAY') : ?>
-                                        
-                                        <td>
-                                            <button class="smlbtn " id="viewPayslip">View Payslip</button>
-                                        </td>
+<!-- BODY -->
+                
+                <div id="mainsplashtext" class="mainsplashtext lefttalign">  
+                    <div class="topsplashtext lefttalign robotobold cattext whitetext"><?php echo WELCOME; ?></div>
+                    <div class="leftsplashtext lefttalign"><?php include(TEMP."/menu.php"); ?></div>
+                    <div class="rightsplashtext lefttalign">
+                        <div id="mainnotification" class="mainbody lefttalign whitetext">  
+                            <b class="mediumtext lorangetext">DAILY TIME RECORD</b><br><br>
+                            
+                            <table>
+                                <tr>
+                                    <td>Year: 
+                                        <select id="dtr_year" name="dtr_year" class="smltxtbox">
+                                            <?php $yearend = strtotime(date('Y').'-12-15'); ?>
+                                            <?php 
+                                                $yearval = date('U') > $yearend ? date("Y") + 1 : date("Y");
+                                                if(isset($_GET['pryear']))
+                                                    $yearval = $_GET['pryear'];
+                                                
+                                            ?>
+                                            <?php foreach ($dtr_year as $key => $value) : ?>
+                                            <option value="<?php echo $value['PRYear']; ?>"<?php echo $value['PRYear'] == $yearval ? ' selected' : ''; ?>><?php echo $value['PRYear']; ?></option>    
+                                            <?php endforeach; ?>
+                                        </select>
+                                    </td>
+                                    <td>DTR Cut-off: 
+                                        <select id="dtr_cover" name="dtr_cover" class="width250 smltxtbox">
+                                            <?php foreach ($dtr_period as $key => $value) : ?>
+                                            <option value="<?php echo date("Y-m-d", strtotime($value['PeriodFrom']))." ".date("Y-m-d", strtotime($value['PeriodTo'])); ?>" dfrom="<?php echo strtotime($value['PeriodFrom']); ?>" dto="<?php echo strtotime($value['PeriodTo']); ?>" posted="<?php echo $value['AttPost']; ?>" period="<?php echo $value['PeriodID']; ?>" <?php if(isset($_GET['period'])) echo $_GET['period'] == $value['PeriodID'] ? 'selected' : ''; ?>>
+                                                <?php echo $value['PeriodID']." ".$value['PRYear']." ".date("m/d/Y", strtotime($value['PeriodFrom']))." to ".date("m/d/Y", strtotime($value['PeriodTo'])); ?>
+                                            </option>  
+                                            <?php endforeach; ?>
+                                        </select>&nbsp;&nbsp;
+                                        <?php $attpost = $dtr_period[0]['AttPost']; ?>
+                                        <span id="txtposted" class="lgreentext bold<?php echo $attpost == 1 ? "" : " invisible"; ?>"><i class="fa fa-check"></i> POSTED</span>
+                                    </td>
+                                    <?php if ($psblock && $profile_dbname != 'OJTPAY') : ?>
+                                    
+                                    <td>
+                                        <button class="smlbtn " id="viewPayslip">View Payslip</button>
+                                    </td>
 
-                                        <?php endif; ?>
+                                    <?php endif; ?>
 
-                                    </tr>
-                                </table>
-                                
-                                <div id="dtrdata" class="innerdata">
+                                </tr>
+                            </table>
+                            
+                            <div id="dtrdata">
+                                <div class="innerdata">
                                 <table border="0" cellspacing="0" class="tdata" style="width:<?php echo $dtr_data ? ' 2400px' : ' 100%'; ?>">                                    
                                     <?php if($dtr_data) : ?>
                                         <tr>
@@ -125,10 +126,13 @@
                                             $total_ot21 = 0;
                                             $total_ot22 = 0;
 
-                                            $shiftsched = $mainsql->get_schedshift($profile_idnum);            
+                                            $shiftsched = $mainsql->get_schedshift($profile_idnum);      
+                                            
+                                            $dates_calculated = [];
                                         ?>
                                         <?php foreach ($dtr_data as $key => $value) : ?>
                                         <?php 
+                                            array_push($dates_calculated, $value['CreatedDate']);
 
                                             $numdays = intval(date("N", strtotime($value['DTRDATE'])));
 
@@ -344,20 +348,24 @@
                                     <?php endif; ?>
                                 </table>
                                 </div>
-                                
-                                <div class="margintop25">
-                                    <span class="bold">Note: </span><span class="italic">All value are based on hours for example 2.35 hours is equal to 2 hours and 21 minutes where 60 minutes x 0.35 = 21 minutes.</span>
+                                <div class="margintop10">
+                                    <span class="italic">Last updated at <?php echo date('Y-m-d H:i:s', strtotime(max($dates_calculated))); ?></span><br>
                                 </div>
-                                
                             </div>
+                            
+                            <div class="margintop15">
+                                <span class="bold">Note: </span><span class="italic">All value are based on hours for example 2.35 hours is equal to 2 hours and 21 minutes where 60 minutes x 0.35 = 21 minutes.</span>
+                            </div>
+                            
                         </div>
                     </div>
-                    <script>
-                        $('#viewPayslip').on('click', function(){
-                            var pryear = $('#dtr_year').val();
-                            var period = $('#dtr_cover option:selected').attr('period');
+                </div>
+                <script>
+                    $('#viewPayslip').on('click', function(){
+                        var pryear = $('#dtr_year').val();
+                        var period = $('#dtr_cover option:selected').attr('period');
 
-                            window.location.href = '<?php echo WEB; ?>/payslip?pryear=' + pryear + '&period=' + period;
-                        });
-                    </script>
-    <?php include(TEMP."/footer.php"); ?>
+                        window.location.href = '<?php echo WEB; ?>/payslip?pryear=' + pryear + '&period=' + period;
+                    });
+                </script>
+<?php include(TEMP."/footer.php"); ?>
