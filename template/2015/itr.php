e8453bca2443e7edcb5f7ace68806833ce3bab72
Kevs <kevinayag@users.noreply.github.com>
2026-01-28 09:50:16 +0800

    ASIAAPMI ITR 2025

--- template/2015/itr.php
+++ template/2015/itr.php
@@ -16,12 +16,11 @@ if (file_exists($itr_file)) {
         <div id="mainnotification" class="mainbody lefttalign whitetext">
             <b class="hugetext4 lorangetext">ITR not found</b><br><br>
             <br>
-            Your 2024 Income Tax Return Form (BIR 2316) was not found.
+            Your 2025 Income Tax Return Form (BIR 2316) was not found.
             <br><br>
-            *Available only for employee who received salary for the year 2024
+            *Available only for employee who received salary for the year 2025
             <br>
             * For further inquiries, contact Payroll Department
-            <!-- <br><b>For Global One Employees, your ITR will be available on 22 February 2021.</b> -->
         </div>
 
     </div>
