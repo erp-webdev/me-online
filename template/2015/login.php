a0053cba443b22f4528a45f412b4bbb60b404277
Anna <aeugenio.global@megaworldcorp.com>
2026-08-12 13:18:47 +0800

    fix(login page): remove ? to Forgot Password

--- template/2015/login.php
+++ template/2015/login.php
@@ -81,7 +81,7 @@
                         Sign in with Google
                     </a>*/
                     ?>
-	                <br><br><a href="<?php echo WEB; ?>/forgot_password" class="lgraytext">Forgot password?</a>
+	                <br><br><a href="<?php echo WEB; ?>/forgot_password" class="lgraytext">Forgot password</a>
 	                <br><span id="errortd" class="redtext"></span>
 	            </td>
 	        </tr>
